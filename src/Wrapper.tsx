2b07fd436847d27d77e598ef027e18f2cbe5b3bc
ARADDCC002 <ARADDCC002>
2022-02-01 11:11:04 +0000

    wip

--- src/Wrapper.tsx
+++ src/Wrapper.tsx
@@ -167,7 +167,7 @@ export default function Wrapper({ title, page, children }: { title: any; page: s
             >
               <MenuIcon />
             </IconButton>
-            <Box sx={{ flexGrow: 1, display: { xs: 'flex', cursor: 'pointer' } }}>
+            <Box sx={{ display: { xs: 'flex', cursor: 'pointer' } }}>
               <Link href='/' passHref>
                 <a>
                   <Image src='/Bailo-logo-reverse.png' alt='Logo' width={55} height={55} />
