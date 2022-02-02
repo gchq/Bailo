16e3d9d60792a156cf526bc1ae2749512052476d
ac83943 <89583054+ac83943@users.noreply.github.com>
2022-02-02 16:26:26 +0000

    Increase stall interval for queue uploads

--- server/utils/queues.ts
+++ server/utils/queues.ts
@@ -3,6 +3,10 @@ import config from 'config'
 
 export const uploadQueue = new Queue('UPLOAD_QUEUE', {
   redis: config.get('redis'),
+
+  // model building may take a minutes, especially when the cache is cold
+  stallInterval: 120000,
+  
 })
 
 export const deploymentQueue = new Queue('DEPLOYMENT_QUEUE', {
