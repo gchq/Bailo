b8b12f38d0074320a5492a1bd6511632fec8475c
ac83943 <89583054+ac83943@users.noreply.github.com>
2022-02-03 12:15:14 +0000

    Simplify approvals logic

--- src/common/ApprovalsChip.tsx
+++ src/common/ApprovalsChip.tsx
@@ -4,35 +4,28 @@ import DoneAll from '@mui/icons-material/DoneAll'
 import Chip from '@mui/material/Chip'
 import React from 'react'
 
-const ApprovalsChip = (props: any) => {
-  const { approvals } = props
-  const [approvedCount, setApprovedCount] = React.useState<number>(0)
-  const [styling, setStyling] = React.useState<object>({})
-  const [icon, setIcon] = React.useState<any>(undefined)
+export default function ApprovalsChip({ approvals }: { approvals: any }) {
+  const numApprovals = approvals.filter((e: string) => e === 'Accepted').length
+  const totalApprovals = approvals.length
 
-  React.useEffect(() => {
-    setApprovedCount(approvals.filter((e: string) => e === 'Accepted').length)
-    if (approvals.filter((e: string) => e === 'Accepted').length === approvals.length) {
-      setStyling({ borderRadius: 1, backgroundColor: '#4c8a4c', color: 'white', height: 'auto' })
-      setIcon(<DoneAll sx={{ color: 'white !important', pl: 1 }} />)
-    } else if (approvedCount < approvals.length && approvedCount !== 0) {
-      setStyling({ borderRadius: 1, backgroundColor: '#dc851b', color: 'white', height: 'auto' })
-      setIcon(<Done sx={{ color: 'white !important', pl: 1 }} />)
-    } else {
-      setStyling({ borderRadius: 1, backgroundColor: '#d63b3b', color: 'white', height: 'auto' })
-      setIcon(<Close sx={{ color: 'white !important', pl: 1 }} />)
-    }
-  }, [approvals, approvedCount])
+  let Icon
+  let backgroundColor
+  if (numApprovals === 0) {
+    Icon = Close
+    backgroundColor = '#d63b3b'
+  } else if (numApprovals < totalApprovals) {
+    Icon = Done
+    backgroundColor = '#dc851b'
+  } else {
+    Icon = DoneAll
+    backgroundColor = '#4c8a4c'
+  }
 
   return (
-    <>
-      <Chip 
-        icon={icon        }  
-        sx={styling} 
-        label={'Approvals ' + approvedCount + '/' + approvals.length} 
-      />
-    </>
+    <Chip 
+      icon={<Icon sx={{ color: 'white !important', pl: 1 }} />}  
+      sx={{ borderRadius: 1, color: 'white', height: 'auto', backgroundColor }} 
+      label={`Approvals ${numApprovals}/${totalApprovals}`} 
+    />
   )
 }
-
-export default ApprovalsChip
