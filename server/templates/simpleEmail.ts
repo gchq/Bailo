729d0738e43066ce2df88bd66b1e8b18d11a1973
ac83943 <89583054+ac83943@users.noreply.github.com>
2022-02-04 23:41:36 +0000

    Email on build status change

---
+++ server/templates/simpleEmail.ts
@@ -0,0 +1,77 @@
+import mjml2html from 'mjml'
+import { wrapper } from './partials'
+
+export interface Column {
+  header: string
+  value: string
+}
+
+export interface Button {
+  text: string
+  href: string
+}
+
+export interface SimpleEmail {
+  text: string
+  columns: Array<Column>
+  buttons: Array<Button>
+  subject: string
+}
+
+export function html({ text, columns, buttons } : SimpleEmail) {
+  return mjml2html(
+    wrapper(`
+    <mj-section background-color="#27598e" padding-bottom="5px" padding-top="20px">
+      <mj-column width="100%">
+        <mj-text align="center" color="#FFF" font-size="13px" font-family="Helvetica" padding-left="25px" padding-right="25px" padding-bottom="28px" padding-top="28px">
+          <span style="font-size:20px; font-weight:bold">${text}</span>
+        </mj-text>
+      </mj-column>
+    </mj-section>
+    <mj-section background-color="#356cc7" padding-bottom="15px">
+      ${columns.map(column => `
+        <mj-column>
+          <mj-text align="center" color="#FFF" font-size="15px" font-family="Ubuntu, Helvetica, Arial, sans-serif" padding-left="25px" padding-right="25px" padding-bottom="0px">
+            <strong>
+              ${column.header}
+            </strong>
+          </mj-text>
+          <mj-text align="center" color="#FFF" font-size="13px" font-family="Helvetica" padding-left="25px" padding-right="25px" padding-bottom="20px" padding-top="10px">
+            ${column.value}  
+          </mj-text>
+        </mj-column>
+      `).join('\n')}
+    </mj-section>
+    <mj-section background-color="#27598e" padding-bottom="20px" padding-top="20px">
+      ${buttons.map(button => `
+        <mj-column width="${100 / buttons.length}%">
+          <mj-button
+            background-color="#f37f58"
+            color="#FFF"
+            font-size="14px"
+            align="center"
+            font-weight="bold"
+            border="none"
+            padding="15px 30px"
+            border-radius="10px"
+            href="${button.href}"
+            font-family="Helvetica"
+            padding-left="25px"
+            padding-right="25px"
+            padding-bottom="10px">
+            ${button.text}
+          </mj-button>
+        </mj-column>      
+      `)}
+    </mj-section>
+  `)
+  ).html
+}
+
+export function simpleEmail(simpleEmail: SimpleEmail) {
+  return {
+    html: html(simpleEmail),
+    text: simpleEmail.text,
+    subject: simpleEmail.subject,
+  }
+}
--- server/utils/queues.ts
+++ server/utils/queues.ts
@@ -1,5 +1,9 @@
 import Queue from 'bee-queue'
 import config from 'config'
+import DeploymentModel from 'server/models/Deployment'
+import VersionModel from 'server/models/Version'
+import { simpleEmail } from 'server/templates/simpleEmail'
+import { sendEmail } from './smtp'
 
 export const uploadQueue = new Queue('UPLOAD_QUEUE', {
   redis: config.get('redis'),
@@ -10,4 +14,90 @@ export const uploadQueue = new Queue('UPLOAD_QUEUE', {
 
 export const deploymentQueue = new Queue('DEPLOYMENT_QUEUE', {
   redis: config.get('redis'),
+})
+
+async function sendUploadEmail(jobId: string, state: string) {
+  const job = await uploadQueue.getJob(jobId)
+  const version = await VersionModel.findById(job.data.versionId)
+    .populate({
+      path: 'model',
+      populate: { path: 'owner' },
+    })
+
+  if (!version.model.owner.email) {
+    return
+  }
+
+  const message = state === 'retrying' ? 'failed but retrying' : state
+  const base = `${config.get('app.protocol')}://${config.get('app.host')}:${config.get('app.port')}`
+
+  await sendEmail({
+    to: version.model.owner.email,
+    ...simpleEmail({
+      text: `Your model build for '${version.model.currentMetadata.highLevelDetails.name}' has ${message}`,
+      columns: [
+        { header: 'Model Name', value: version.model.currentMetadata.highLevelDetails.name },
+        { header: 'Build Type', value: 'Model' },
+        { header: 'Status', value: state.charAt(0).toUpperCase() + state.slice(1) }
+      ],
+      buttons: [
+        { text: 'Build Logs', href: `${base}/model/${version.model.uuid}` }
+      ],
+      subject: `Your model build for '${version.model.currentMetadata.highLevelDetails.name}' has ${message}`
+    }),
+  })
+}
+
+uploadQueue.on('job succeeded', async (jobId) => {
+  await sendUploadEmail(jobId, 'succeeded')
+})
+
+uploadQueue.on('job retrying', async (jobId) => {
+  await sendUploadEmail(jobId, 'retrying')
+})
+
+uploadQueue.on('job failed', async (jobId) => {
+  await sendUploadEmail(jobId, 'failed')
+})
+
+async function sendDeploymentEmail(jobId: string, state: string) {
+  const job = await deploymentQueue.getJob(jobId)
+  const deployment = await DeploymentModel.findById(job.data.deploymentId)
+    .populate('owner')
+    .populate('model')
+
+  if (!deployment.owner.email) {
+    return
+  }
+
+  const message = state === 'retrying' ? 'failed but retrying' : state
+  const base = `${config.get('app.protocol')}://${config.get('app.host')}:${config.get('app.port')}`
+
+  await sendEmail({
+    to: deployment.owner.email,
+    ...simpleEmail({
+      text: `Your deployment for '${deployment.model.currentMetadata.highLevelDetails.name}' has ${message}`,
+      columns: [
+        { header: 'Model Name', value: deployment.model.currentMetadata.highLevelDetails.name },
+        { header: 'Build Type', value: 'Deployment' },
+        { header: 'Status', value: state.charAt(0).toUpperCase() + state.slice(1) }
+      ],
+      buttons: [
+        { text: 'Build Logs', href: `${base}/deployment/${deployment.uuid}` }
+      ],
+      subject: `Your deployment for '${deployment.model.currentMetadata.highLevelDetails.name}' has ${message}`
+    }),
+  })
+}
+
+deploymentQueue.on('job succeeded', async (jobId) => {
+  await sendDeploymentEmail(jobId, 'succeeded')
+})
+
+deploymentQueue.on('job retrying', async (jobId) => {
+  await sendDeploymentEmail(jobId, 'retrying')
+})
+
+deploymentQueue.on('job failed', async (jobId) => {
+  await sendDeploymentEmail(jobId, 'failed')
 })
