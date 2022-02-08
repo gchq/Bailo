69443a1e14d0d02708d77b51bfea2594805dffe3
at55425 <>
2022-02-08 13:08:04 +0000

    move (optionally) adding trusted CA certs to app start up, not built into image

---
+++ run_app.sh
@@ -0,0 +1,10 @@
+# This is needed if we're using self-signed certs for the registry  
+if [[ -z "${ADD_CA_CERT}" ]]; then
+    echo "ADD_CA_CERT was not set; not adding anything to system CA"
+else
+    echo "Adding ${ADD_CA_CERT} to system CA"
+    cp ${ADD_CA_CERT} /usr/local/share/ca-certificates/bailo.pem
+    update-ca-certificates
+fi
+
+npm run dev
