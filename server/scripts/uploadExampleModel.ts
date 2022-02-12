0702076aa6b21bb22cea143a9efba6356cbd1b42
ac83943 <89583054+ac83943@users.noreply.github.com>
2022-02-12 18:42:01 +0000

    add upload / approval of requests to api

---
+++ server/scripts/uploadExampleModel.ts
@@ -0,0 +1,28 @@
+import { join } from 'path'
+import Bailo from '../../lib/node'
+import fs from 'fs'
+import { fileFromPath } from 'formdata-node/file-from-path'
+import logger from '../utils/logger'
+import open from 'open'
+
+async function uploadExampleModel() {
+  const api = new Bailo('http://localhost:8080/api/v1')
+
+  const defaultSchema = await api.getDefaultSchema('UPLOAD')
+  
+  const metadata = JSON.parse(fs.readFileSync(join(__dirname, '../../__tests__/example_models/minimal_model/minimal_metadata.json'), 'utf-8'))
+  metadata.schemaRef = defaultSchema.schema.reference
+
+  const { uuid } = await api.postModel(
+    await fileFromPath(join(__dirname, '../../__tests__/example_models/minimal_model/minimal_code.zip')),
+    await fileFromPath(join(__dirname, '../../__tests__/example_models/minimal_model/minimal_binary.zip')),
+    metadata
+  )
+
+  logger.info({ uuid }, `Successfully uploaded model '${uuid}'`)
+  await open(`http://localhost:8080/model/${uuid}`)
+  
+  return uuid
+}
+
+uploadExampleModel()
