c6d78bd5aa077031ed120dd122d79a48bf8dcf14
ac83943 <89583054+ac83943@users.noreply.github.com>
2022-02-12 18:43:02 +0000

    run styling

--- server/scripts/uploadExampleModel.ts
+++ server/scripts/uploadExampleModel.ts
@@ -9,8 +9,10 @@ async function uploadExampleModel() {
   const api = new Bailo('http://localhost:8080/api/v1')
 
   const defaultSchema = await api.getDefaultSchema('UPLOAD')
-  
-  const metadata = JSON.parse(fs.readFileSync(join(__dirname, '../../__tests__/example_models/minimal_model/minimal_metadata.json'), 'utf-8'))
+
+  const metadata = JSON.parse(
+    fs.readFileSync(join(__dirname, '../../__tests__/example_models/minimal_model/minimal_metadata.json'), 'utf-8')
+  )
   metadata.schemaRef = defaultSchema.schema.reference
 
   const { uuid } = await api.postModel(
@@ -21,7 +23,7 @@ async function uploadExampleModel() {
 
   logger.info({ uuid }, `Successfully uploaded model '${uuid}'`)
   await open(`http://localhost:8080/model/${uuid}`)
-  
+
   return uuid
 }
 
