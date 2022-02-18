65a6125a4f15eefd3b48ae36a6048923f5409607
ac83943 <89583054+ac83943@users.noreply.github.com>
2022-02-18 21:49:37 +0000

    fix code bugs

--- src/MuiForms/RangeWidget/RangeWidget.tsx
+++ src/MuiForms/RangeWidget/RangeWidget.tsx
@@ -22,7 +22,7 @@ const RangeWidget = ({
 }: WidgetProps) => {
   let sliderProps = { value, label, id, ...rangeSpec(schema) }
 
-  const _onChange = ({}, value: any) => onChange(value === '' ? options.emptyValue : value)
+  const _onChange = (_: any, value: any) => onChange(value === '' ? options.emptyValue : value)
   const _onBlur = ({ target: { value } }: React.FocusEvent<HTMLInputElement>) => onBlur(id, value)
   const _onFocus = ({ target: { value } }: React.FocusEvent<HTMLInputElement>) => onFocus(id, value)
 
