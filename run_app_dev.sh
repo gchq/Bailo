7b3e86cedd9635f0ccfb391d620b554797cc00f8
at55425 <>
2022-02-24 18:14:45 +0000

    add option to run schema setup on start

--- run_app_dev.sh
+++ run_app_dev.sh
@@ -1,13 +1,15 @@
 # This is needed if we're using self-signed certs for the registry  
-if [[ -z "${ADD_CA_CERT}" ]]; then
-    echo "ADD_CA_CERT was not set; not adding anything to system CA"
-else
-    echo "Adding ${ADD_CA_CERT} to system CA"
-    cp ${ADD_CA_CERT} /usr/local/share/ca-certificates/bailo.pem
-    update-ca-certificates
-fi
+# You must have mounted your additional CA certs into /usr/local/share/ca-certificates/
+update-ca-certificates
 
 echo "Running 'npm install'"
 npm install 
+
+if [[ -z "${SCHEMA_SETUP_SCRIPT}" ]]; then
+  echo "No schemas setup script set; You will have to add schemas after startup"
+else
+  echo "Running npx ts-node --project tsconfig.server.json $SCHEMA_SETUP_SCRIPT"
+  npx ts-node --project tsconfig.server.json $SCHEMA_SETUP_SCRIPT
+fi
 echo "Running 'npm run dev'"
 npm run dev
