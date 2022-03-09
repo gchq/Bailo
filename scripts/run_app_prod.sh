9f41064514f6dcd08a5dce4f4a65be7219a8ba5c
at55425 <at55425@protonmail.com>
2022-03-09 11:49:30 +0000

    pass in schema setup in default

--- scripts/run_app_prod.sh
+++ scripts/run_app_prod.sh
@@ -1,5 +1,12 @@
 # This is needed if we're using self-signed certs for the registry  
 update-ca-certificates
 
+if [[ -z "${SCHEMA_SETUP_SCRIPT}" ]]; then
+  echo "No schemas setup script set; You will have to add schemas after startup"
+else
+  echo "Running npx ts-node --project tsconfig.server.json $SCHEMA_SETUP_SCRIPT"
+  npx ts-node --project tsconfig.server.json $SCHEMA_SETUP_SCRIPT
+fi
+
 echo "Running 'npm run start'"
 npm run start
