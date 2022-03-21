4e3c91349d1071fa9ee9ab609bece209a5915d6f
ARADDCC002 <ARADDCC002>
2022-03-21 17:46:18 +0000

    changed logger import to req.log for schema request api

--- server/routes/v1/uiConfig.ts
+++ server/routes/v1/uiConfig.ts
@@ -2,18 +2,17 @@ import { ensureUserRole } from '../../utils/user'
 import config from 'config'
 import { Request, Response } from 'express'
 import { NotFound } from '../../utils/result'
-import logger from '../../utils/logger'
 
 export const getUiConfig = [
   ensureUserRole('user'),
-  async (_req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const uiConfig = await config.get('uiConfig')
 
     if (!uiConfig) {
       throw NotFound({}, `Unable to find UI Config`)
     }
 
-    logger.info({uiConfig: uiConfig}, 'User fetching UI config')
+    req.log.info({uiConfig: uiConfig}, 'User fetching UI config')
     return res.json(uiConfig)
   },
 ]
