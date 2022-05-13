4fa871b9f6daa3726ae513f925b4713d60c4c841
ARADDCC002 <ARADDCC002>
2022-05-13 10:45:23 +0000

    fixed incorrect import

--- server/services/version.ts
+++ server/services/version.ts
@@ -9,7 +9,7 @@ import { createSerializer, SerializerOptions } from '../utils/logger'
 import { serializedModelFields } from './model'
 import { UserDoc } from '../models/User'
 import { VersionDoc } from '../models/Version'
-import ModelModel from 'server/models/Model'
+import ModelModel from '../models/Model'
 
 const authorisation = new AuthorisationBase()
 
