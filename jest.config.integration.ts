fb2612ade4f34be5c7d661d3efc90460144393a0
a3957273 <89583054+a3957273@users.noreply.github.com>
2022-05-15 23:56:02 +0000

    update where page tests are located

--- jest.config.integration.ts
+++ jest.config.integration.ts
@@ -2,8 +2,7 @@ import type { Config } from '@jest/types'
 
 const customJestConfig: Config.InitialOptions = {
   bail: true,
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)'],
-  testPathIgnorePatterns: ['__tests__/__utils__', 'config'],
+  testMatch: ['**/__tests__/selenium_tests/*.[jt]s?(x)'],
 }
 
 module.exports = customJestConfig
