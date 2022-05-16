aa2ba9b2fc135fc1cfe5b7d07e4daeb003ef7baf
a3957273 <89583054+a3957273@users.noreply.github.com>
2022-05-16 00:05:24 +0000

    add next config back in

--- jest.config.integration.ts
+++ jest.config.integration.ts
@@ -1,8 +1,14 @@
+import nextJest from 'next/jest'
 import type { Config } from '@jest/types'
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+})
 
 const customJestConfig: Config.InitialOptions = {
   bail: true,
   testMatch: ['**/__tests__/selenium_tests/*.[jt]s?(x)'],
 }
 
-module.exports = customJestConfig
+module.exports = createJestConfig(customJestConfig)
