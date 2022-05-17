9794a07db96a9aad6128b9f59c240196376e7e7b
a3957273 <89583054+a3957273@users.noreply.github.com>
2022-05-17 22:54:43 +0000

    remove unnecessary optionals

--- pages/review.tsx
+++ pages/review.tsx
@@ -125,21 +125,21 @@ function ApprovalList({ type, category }: { type: RequestType; category: ReviewF
             <Grid item xs={12} sm={8}>
               {type === 'Upload' && (
                 <>
-                  <Link href={`/model/${requestObj?.version?.model?.uuid}`} passHref>
+                  <Link href={`/model/${requestObj.version?.model?.uuid}`} passHref>
                     <MuiLink variant='h5' sx={{ fontWeight: '500', textDecoration: 'none' }}>
-                      {requestObj?.version?.metadata?.highLevelDetails?.name}
+                      {requestObj.version?.metadata?.highLevelDetails?.name}
                     </MuiLink>
                   </Link>
                   <Stack direction='row' spacing={2}>
                     <Chip label={requestObj.approvalType} size='small' />
                     <Box sx={{ mt: 'auto !important', mb: 'auto !important' }}>
                       <Typography variant='body1'>
-                        {requestObj?.version?.metadata?.highLevelDetails?.modelInASentence}
+                        {requestObj.version?.metadata?.highLevelDetails?.modelInASentence}
                       </Typography>
                     </Box>
                   </Stack>
-                  {requestObj?.version === undefined ||
-                    (requestObj?.version === null && (
+                  {requestObj.version === undefined ||
+                    (requestObj.version === null && (
                       <Alert sx={{ mt: 2 }} severity='warning'>
                         This model appears to have data missing - check with the uploader to make sure it was uploaded
                         correctly
@@ -149,21 +149,21 @@ function ApprovalList({ type, category }: { type: RequestType; category: ReviewF
               )}
               {type === 'Deployment' && (
                 <>
-                  <Link href={`/deployment/${requestObj?.deployment?.uuid}`} passHref>
+                  <Link href={`/deployment/${requestObj.deployment?.uuid}`} passHref>
                     <MuiLink variant='h5' sx={{ fontWeight: '500', textDecoration: 'none' }}>
-                      {requestObj?.deployment?.metadata?.highLevelDetails?.name}
+                      {requestObj.deployment?.metadata?.highLevelDetails?.name}
                     </MuiLink>
                   </Link>
                   <Typography variant='body1'>
                     Requesting deployment of{' '}
-                    <Link href={`/model/${requestObj?.deployment?.model?.uuid}`} passHref>
+                    <Link href={`/model/${requestObj.deployment?.model?.uuid}`} passHref>
                       <MuiLink sx={{ fontWeight: '500', textDecoration: 'none' }}>
-                        {requestObj?.deployment?.model?.currentMetadata?.highLevelDetails?.name}
+                        {requestObj.deployment?.model?.currentMetadata?.highLevelDetails?.name}
                       </MuiLink>
                     </Link>
                   </Typography>
-                  {requestObj?.deployment === undefined ||
-                    (requestObj?.deployment === null && (
+                  {requestObj.deployment === undefined ||
+                    (requestObj.deployment === null && (
                       <Alert sx={{ mt: 2 }} severity='warning'>
                         This deployment appears to have data missing - check with the requester to make sure it was
                         requested correctly
