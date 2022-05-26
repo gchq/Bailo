38492e60f91926b3587e27091cfedd114c50ba73
ARADDCC002 <ARADDCC002>
2022-05-26 17:31:48 +0000

    working uiconfig routes test

--- server/routes/v1/uiConfig.spec.ts
+++ server/routes/v1/uiConfig.spec.ts
@@ -1,17 +1,23 @@
 import supertest from 'supertest'
 import { server } from '../../index'
+import mongoose from 'mongoose'
 
 const request = supertest.agent(server);
-//console.log(request)
 
 describe('test UI config routes', () => {
 
   test('that we can fetch the correct UI config', async () => {
-    /*const res = await request.get('/api/v1/config');
-    console.log(res)
-    expect(res.header['content-type']).toBe('text/html; charset=utf-8');
-    expect(res.statusCode).toBe(200);
-    expect(res.text).toEqual('hello world!');*/
+    const res = await request.get('/api/v1/config').set('x-userid', 'user').set('x-email', 'test')
+    const data = JSON.parse(res.text)
+    expect(res.header['content-type']).toBe('application/json; charset=utf-8')
+    expect(res.statusCode).toBe(200)
+    expect(data.banner).not.toBe(undefined)
+    expect(data.registry).not.toBe(undefined)
   });
+
+  afterAll(done => {
+    mongoose.connection.close()
+    done()
+  })
 
 })
