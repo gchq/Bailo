d6d6d161a92a02c185412c0cb4bc98148e1d3b50
EC2 Default User <jess77419@protonmail.com>
2022-08-17 10:03:53 +0000

    test minimal keys in dictionary

--- lib/python/tests/test_utils.py
+++ lib/python/tests/test_utils.py
@@ -13,3 +13,54 @@ def test_get_filename_and_mimetype_returns_mimetype_of_none_if_path_is_directory
 
     assert filename == "data"
     assert m_type is None
+
+
+def test_minimal_keys_in_dict_returns_valid_result_if_both_dictionaries_are_empty():
+    result = utils.minimal_keys_in_dictionary({}, {})
+
+    assert result == {"valid": True}
+
+
+def test_minimal_keys_in_dict_returns_error_if_dict2_does_not_include_key_from_minimal_dict():
+    result = utils.minimal_keys_in_dictionary({"key1": "value1"}, {})
+
+    assert not result["valid"]
+    assert result["error_message"] == "must contain 'key1'"
+
+
+def test_minimal_keys_in_dict_returns_error_if_dict2_has_empty_value_for_key_from_minimal_dict():
+    result = utils.minimal_keys_in_dictionary({"key1": "value1"}, {"key1": None})
+
+    assert not result["valid"]
+    assert result["error_message"] == "'key1' cannot be empty"
+
+
+def test_minimal_keys_in_dict_returns_error_if_dict2_is_missing_subkeys_from_minimal_dict():
+    result = utils.minimal_keys_in_dictionary(
+        {"key1": {"key2": "value1"}}, {"key1": "value1"}
+    )
+
+    assert not result["valid"]
+    assert result["error_message"] == "missing data under 'key1'"
+
+
+def test_minimal_keys_in_dict_validates_if_minimal_dict_is_empty():
+    result = utils.minimal_keys_in_dictionary({}, {"key1": "value1"})
+
+    assert result["valid"]
+
+
+def test_minimal_keys_in_dict_ignores_extra_keys_in_dict2():
+    result = utils.minimal_keys_in_dictionary(
+        {"key1": "value"}, {"key1": "value1", "key2": "value2"}
+    )
+
+    assert result["valid"]
+
+
+def test_minimal_keys_in_dict_validates_multilevel_dictionaries():
+    result = utils.minimal_keys_in_dictionary(
+        {"key1": {"key2": {"key3": "value"}}}, {"key1": {"key2": {"key3": "value"}}}
+    )
+
+    assert result["valid"]
