47688fc6de88b44517d9e8e23b7a04e1aef016ea
ARADDCC012 <ARADDCC012>
2022-08-18 09:46:18 +0000

    Updated next config to ESM and renamed others to cjs

--- next.config.cjs
+++
@@ -1,7 +0,0 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-
-module.exports = withBundleAnalyzer({
-  swcMinify: true,
-})
---
+++ next.config.js
@@ -0,0 +1,9 @@
+import bundleAnalyzer from '@next/bundle-analyzer'
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+export default withBundleAnalyzer({
+  swcMinify: true,
+})
