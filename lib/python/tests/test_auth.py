c29b530bea8cfff1cd460a7b4990b9ef0f315435
EC2 Default User <jess77419@protonmail.com>
2022-08-19 10:48:29 +0000

    remove url references and patch auth to remove requirement for actual auth

--- lib/python/tests/test_auth.py
+++ lib/python/tests/test_auth.py
@@ -1,82 +1,55 @@
-import os
+from unittest.mock import MagicMock
 
 import pytest
-from dotenv import load_dotenv
 
 from ..bailoclient.auth import CognitoSRPAuthenticator
 from ..bailoclient.config import APIConfig, BailoConfig, CognitoConfig
 from ..bailoclient.utils.exceptions import UnauthorizedException
 
-load_dotenv()
-
 
 @pytest.fixture()
-def authenticator():
-    return cognito_authenticator()
-
-
 def cognito_authenticator():
     cognito_config = CognitoConfig(
-        user_pool_id=os.environ["BAILO_USERPOOL"],
-        client_id=os.environ["BAILO_CLIENT_ID"],
-        client_secret=os.environ["BAILO_CLIENT_SECRET"],
-        region=os.environ["BAILO_REGION"],
+        user_pool_id="BAILO_USERPOOL",
+        client_id="BAILO_CLIENT_ID",
+        client_secret="BAILO_CLIENT_SECRET",
+        region="BAILO_REGION",
     )
 
     config = BailoConfig(
-        cognito=cognito_config, api=APIConfig(url="http://bailo.com", ca_verify=True)
+        cognito=cognito_config,
+        api=APIConfig(url="http://localhost:8080", ca_verify=True),
     )
 
-    return CognitoSRPAuthenticator(config)
-
-
-def user_authenticated_cognito(authenticator):
-    try:
-        return authenticator.authenticate_user(
-            username=os.environ["BAILO_USERNAME"], password=os.environ["BAILO_PASSWORD"]
-        )
-    except:
-        return False
-
-
-@pytest.mark.skipif(
-    user_authenticated_cognito(cognito_authenticator()) is False,
-    reason="requires user to be authenticated with cognito",
-)
-def test_authenticate_user(authenticator):
-    assert not authenticator.is_authenticated()
-
-
-@pytest.mark.skipif(
-    user_authenticated_cognito(cognito_authenticator()) is False,
-    reason="requires user to be authenticated with cognito",
-)
-def test_authenticate_user_true_after_authentication(authenticator):
-    authenticator.authenticate_user(
-        os.environ["BAILO_USERNAME"], os.environ["BAILO_PASSWORD"]
+    authenticator = CognitoSRPAuthenticator(config)
+    aws_auth_response = {"AuthenticationResult": {"AccessToken": "token"}}
+    authenticator._CognitoSRPAuthenticator__try_authorise = MagicMock(
+        return_value=aws_auth_response
     )
-    assert authenticator.is_authenticated()
+
+    return authenticator
 
 
-@pytest.mark.skipif(
-    user_authenticated_cognito(cognito_authenticator()) is False,
-    reason="requires user to be authenticated with cognito",
-)
-def test_auth_headers_raises_error_if_not_authenticated(authenticator):
+def test_authenticate_user(cognito_authenticator):
+    assert not cognito_authenticator.is_authenticated()
+
+
+def test_authenticate_user_true_after_authentication(cognito_authenticator):
+    cognito_authenticator.authenticate_user("BAILO_USERNAME", "BAILO_PASSWORD")
+
+    assert cognito_authenticator.is_authenticated()
+
+
+def test_auth_headers_raises_error_if_not_authenticated(cognito_authenticator):
     with pytest.raises(
         UnauthorizedException, match="Authenticator not yet authorised."
     ):
-        authenticator.get_authorisation_headers()
+        cognito_authenticator.get_authorisation_headers()
 
 
-@pytest.mark.skipif(
-    user_authenticated_cognito(cognito_authenticator()) is False,
-    reason="requires user to be authenticated with cognito",
-)
-def test_auth_headers_(authenticator):
-    authenticator.authenticate_user(
-        os.environ["BAILO_USERNAME"], os.environ["BAILO_PASSWORD"]
-    )
-    headers = authenticator.get_authorisation_headers()
+def test_auth_headers_(cognito_authenticator):
+    cognito_authenticator.authenticate_user("BAILO_USERNAME", "BAILO_PASSWORD")
+
+    headers = cognito_authenticator.get_authorisation_headers()
 
     assert headers.get("Authorization")
