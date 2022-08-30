ac25e922a938b14b974b934eafc80b96254f998d
ARADDCC012 <ARADDCC012>
2022-08-30 11:10:35 +0000

    BAI-205 Moved documentation into root docs directory

--- pages/docs/index.tsx
+++
@@ -1,6 +0,0 @@
-import React, { ReactElement } from 'react'
-import DocsWrapper from '@/src/DocsWrapper'
-
-export default function Docs(): ReactElement {
-  return <DocsWrapper />
-}
--- src/docs/IntroCard.tsx
+++ src/docs/IntroCard.tsx
@@ -1,5 +1,4 @@
-import * as React from 'react'
-import Box from '@mui/material/Box'
+import React, { ReactNode } from 'react'
 import Card from '@mui/material/Card'
 import CardActions from '@mui/material/CardActions'
 import CardContent from '@mui/material/CardContent'
@@ -10,24 +9,25 @@ import Link from 'next/link'
 interface IntroCardProps {
   title: string
   user: string
-  children: any
   url: string
+  children: ReactNode
 }
-
-export default function IntroCard({ title, user, children, url }: IntroCardProps) {
+export default function IntroCard({ title, user, url, children }: IntroCardProps) {
   return (
     <Card sx={{ minWidth: 275 }}>
       <CardContent>
         <Typography variant='h5' component='div'>
           {title}
         </Typography>
-        <Typography sx={{ mb: 1.5 }} color='text.secondary'>
+        <Typography sx={{ mb: 1.5 }} color='text.secondary' component='div'>
           {user}
         </Typography>
-        <Typography variant='body2'>{children}</Typography>
+        <Typography variant='body2' component='div'>
+          {children}
+        </Typography>
       </CardContent>
       <CardActions>
-        <Link href={url}>
+        <Link passHref href={url}>
           <Button size='small'>Learn More</Button>
         </Link>
       </CardActions>
