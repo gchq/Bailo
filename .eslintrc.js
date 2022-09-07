53dcb424dc724f251e178472dc02bcd214d42869
EC2 Default User <jess77419@protonmail.com>
2022-09-07 15:03:25 +0000

    fix no-shadow linting rule and fix linting errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,6 +33,7 @@ module.exports = {
     'import/no-cycle': [0, { ignoreExternal: true }],
     '@typescript-eslint/no-use-before-define': ['error', { functions: false, classes: false, variables: true }],
     '@typescript-eslint/no-namespace': ['error', { allowDeclarations: true }],
+    "@typescript-eslint/no-shadow": ["error"],
     // IGNORED RULES
     '@typescript-eslint/no-var-requires': 'off',
     'import/extensions': 'off',
@@ -51,6 +52,7 @@ module.exports = {
     'react/prop-types': 'off',
     'react/require-default-props': 'off',
     'react/react-in-jsx-scope': 'off',
+    'no-shadow': 'off',
   },
   settings: {
     'import/resolver': {
--- types/interfaces.ts
+++ types/interfaces.ts
@@ -3,7 +3,7 @@ import { Date, Types } from 'mongoose'
 import { Dispatch, SetStateAction } from 'react'
 import { UserDoc } from '../server/models/User'
 
-export type { DeploymentDoc as Deployment } from '../server/models/Deployment'
+// export type { DeploymentDoc as Deployment } from '../server/models/Deployment'
 export type { RequestDoc as Request } from '../server/models/Request'
 export type { UserDoc as User } from '../server/models/User'
 export type { VersionDoc as Version } from '../server/models/Version'
@@ -145,7 +145,6 @@ export interface RenderInterface {
   splitSchema: SplitSchema
   setSplitSchema: Dispatch<SetStateAction<SplitSchema>>
 }
-
 export const approvalStateOptions = ['Accepted', 'Declined', 'No Response']
 
 export enum ApprovalStates {
