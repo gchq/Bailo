e781a566dc5322d122677898447777e20ab1182a
EC2 Default User <jess77419@protonmail.com>
2022-09-08 14:54:40 +0000

    fix deployment data format and resolve import issues

--- server/models/Version.ts
+++ server/models/Version.ts
@@ -1,6 +1,7 @@
 import { Document, IndexOptions, model, Schema, Types } from 'mongoose'
 import logger from '../utils/logger'
-import { approvalStateOptions, ApprovalStates, LogStatement } from './Deployment'
+import LogStatement from './Deployment'
+import { approvalStateOptions, ApprovalStates } from '../../types/interfaces'
 import { ModelDoc } from './Model'
 
 interface FilePaths {
