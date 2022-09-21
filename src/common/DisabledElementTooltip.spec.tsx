9839a76f924fe5e162978220c6ba1247bc90c1a2
ARADDCC002 <ARADDCC002>
2022-09-21 12:19:43 +0000

    added file

---
+++ src/common/DisabledElementTooltip.spec.tsx
@@ -0,0 +1,25 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import Button from '@mui/material/Button'
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import DisabledElementTooltip from './DisabledElementTooltip'
+
+describe('DisabledElementTooltip', () => {
+  const testMessage = 'This ia test message'
+
+  it('renders an DisabledElementTooltip component when a button is hovered over', async () => {
+    render(
+      <DisabledElementTooltip conditions={[testMessage]}>
+        <Button data-testid='trigger'>Button</Button>
+      </DisabledElementTooltip>
+    )
+
+    await waitFor(async () => {
+      userEvent.hover(screen.getByTestId('trigger'))
+      expect(await screen.findByText(testMessage)).not.toBeUndefined()
+    })
+  })
+})
