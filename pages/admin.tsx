8b9eff38d4b253110b4bc543df1689036a89d42e
ARADDCC012 <110473008+ARADDCC012@users.noreply.github.com>
2022-11-17 14:16:48 +0000

    Fixed log list overflow bug

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement, useState } from 'react'
 import { useTheme } from '@mui/material/styles'
 import Box from '@mui/material/Box'
 import Divider from '@mui/material/Divider'
+import Stack from '@mui/material/Stack'
 import { LogLevel } from '@/types/interfaces'
 import Wrapper from '../src/Wrapper'
 import FilterMenu, { LogFilters } from '../src/FilterMenu/FilterMenu'
@@ -23,30 +24,29 @@ export default function Admin(): ReactElement {
 
   return (
     <Wrapper title='Admin' page='admin'>
-      <Box display='flex' width='100%' height='calc(100vh - 196px)'>
-        <Box flex={1} height='100%' overflow='auto'>
-          <Box
-            sx={{
-              p: 1,
-              height: '100%',
-              backgroundColor: theme.palette.mode === 'light' ? '#f3f1f1' : '#5a5a5a',
-              border: '1px solid rgba(0, 0, 0, 0.12)',
-              borderRadius: 1,
-            }}
-          >
-            <Box mx={1}>Logs</Box>
-            <Divider sx={{ my: 1 }} />
-            <Box mx={1} height='100%'>
-              <Box display='flex' mb={1} width='100%'>
-                <Box ml='auto'>
-                  <FilterMenu currentFilters={logFilters} onGetLogs={handleGetLogs} />
-                </Box>
-              </Box>
-              <Box mx={1} height='100%' overflow='auto'>
-                <LogTree query={logFilters} />
+      <Box display='flex' height='calc(100vh - 196px)'>
+        <Box
+          sx={{
+            p: 1,
+            backgroundColor: theme.palette.container.main,
+            border: '1px solid rgba(0, 0, 0, 0.12)',
+            borderRadius: 1,
+            flex: 1,
+            height: '100%',
+          }}
+        >
+          <Stack direction='column' spacing={1} width='100%' height='100%'>
+            <Box display='flex' mx={1}>
+              <Box my='auto'>Logs</Box>
+              <Box ml='auto'>
+                <FilterMenu currentFilters={logFilters} onGetLogs={handleGetLogs} />
               </Box>
             </Box>
-          </Box>
+            <Divider />
+            <Box mx={1} overflow='auto' height='100%'>
+              <LogTree query={logFilters} />
+            </Box>
+          </Stack>
         </Box>
       </Box>
     </Wrapper>
