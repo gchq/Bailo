ee1bbf30b8e76fe866476c5d87940ae2fe572e2d
ARADDCC002 <ARADDCC002>
2022-11-22 11:44:43 +0000

    increased default timeout for cypress commands

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,5 +5,6 @@ export default defineConfig({
   e2e: {
     baseUrl: `${config.get('app.protocol')}://${config.get('app.host')}:${config.get('app.port')}`,
     video: false,
+    defaultCommandTimeout: 10000
   },
 })
