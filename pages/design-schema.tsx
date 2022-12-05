d81b271689ae7ebf71808b12c2f3ba2d73842d02
ARADDCC002 <ARADDCC002>
2022-12-05 18:32:44 +0000

    form designer page rerenders the questions when they are reordered

--- pages/design-schema.tsx
+++ pages/design-schema.tsx
@@ -1,63 +1,18 @@
 import { useGetCurrentUser } from '@/data/user'
 import Wrapper from '@/src/Wrapper'
 import Box from '@mui/material/Box'
-import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
 import SchemaDesigner from '@/src/SchemaDesign/SchemaDesigner'
-import { useEffect, useState } from 'react'
-import { Schema, SplitSchema } from '@/types/interfaces'
-import { getStepsFromSchema } from '@/utils/formUtils'
-import FormDesigner from '@/src/Form/FormDesigner'
-import Paper from '@mui/material/Paper'
-import useNotification from '@/src/common/Snackbar'
 
 export default function DesignSchema() {
-  const [userSchema, setUserSchema] = useState<Schema | undefined>()
-  const [splitSchema, setSplitSchema] = useState<SplitSchema>({ reference: '', steps: [] })
-
   const { currentUser } = useGetCurrentUser()
-  const sendNotification = useNotification()
-
-  useEffect(() => {
-    if (!userSchema) return
-    const { reference } = userSchema
-    const steps = getStepsFromSchema(userSchema, {}, [])
-    setSplitSchema({ reference, steps })
-  }, [userSchema])
-
-  useEffect(() => {
-    console.log(splitSchema)
-  }, [splitSchema])
-
-  const onSubmit = () => {
-    sendNotification({ variant: 'success', msg: 'Model submitted!' })
-  }
 
   return (
     <Wrapper title='Design Schema' page='design'>
       <Box display='flex'>
         {currentUser && currentUser.roles.includes('admin') ? (
           <Box>
-            <Stack direction='row' spacing={2}>
-              <Box>
-                <SchemaDesigner setUserSchema={setUserSchema} />
-              </Box>
-              <Box>
-                <Paper
-                  sx={{
-                    p: 2,
-                    width: '100%',
-                  }}
-                >
-                  <FormDesigner
-                    splitSchema={splitSchema}
-                    setSplitSchema={setSplitSchema}
-                    onSubmit={onSubmit}
-                    modelUploading={false}
-                  />
-                </Paper>
-              </Box>
-            </Stack>
+            <SchemaDesigner />
           </Box>
         ) : (
           <Typography variant='h5' component='p'>
--- src/SchemaDesign/SchemaDesigner.tsx
+++ src/SchemaDesign/SchemaDesigner.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import { Schema, SchemaQuestion } from '@/types/interfaces'
+import { Schema, SchemaQuestion, SplitSchema } from '@/types/interfaces'
+import { withTheme } from '@rjsf/core'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import Divider from '@mui/material/Divider'
@@ -9,26 +10,30 @@ import Tab from '@mui/material/Tab'
 import Tabs from '@mui/material/Tabs'
 import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
-import React, { Dispatch, SetStateAction, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
+import { getStepsFromSchema } from '@/utils/formUtils'
 import QuestionPicker from './QuestionPicker'
+import { Theme as MaterialUITheme } from '../MuiForms'
 
-export default function SchemaDesigner({
-  setUserSchema,
-}: {
-  setUserSchema: Dispatch<SetStateAction<Schema | undefined>>
-}) {
+const SchemaForm = withTheme(MaterialUITheme)
+
+export default function SchemaDesigner() {
   const [questionPickerOpen, setQuestionPickerOpen] = React.useState(false)
   const [pages, setPages] = React.useState<any[]>([])
   const [pageName, setPageName] = React.useState('')
   const [pageReference, setPageReference] = React.useState('')
   const [selectedPage, setSelectedPage] = React.useState('')
+  const [splitSchema, setSplitSchema] = React.useState<SplitSchema>({ reference: '', steps: [] })
   const [schemaName, setSchemaName] = React.useState('')
+  const [showForm, setShowForm] = React.useState(true)
   const [schemaReference, setSchemaReference] = React.useState('')
   const [schemaDetails, setSchemaDetails] = React.useState({
     name: '',
     reference: '',
   })
+
+  const pageIndex = pages.findIndex((page) => page.reference === selectedPage)
 
   useEffect(() => {
     if (pages) {
@@ -43,16 +48,17 @@ export default function SchemaDesigner({
         }
         if (page.questions && page.questions.length > 0) {
           page.questions.forEach((question) => {
+            const questionReference = question.reference
             const questionToAdd = {
-              [question.reference]: { ...question },
+              [questionReference]: { ...question },
             }
-            delete questionToAdd[question.reference].reference
-            Object.assign(pageToAdd[page.reference].properties, questionToAdd)
+            delete questionToAdd[questionReference].reference
+            pageToAdd[page.reference].properties = { ...pageToAdd[page.reference].properties, ...questionToAdd }
           })
+          Object.assign(userPages, pageToAdd)
         }
-        Object.assign(userPages, pageToAdd)
       })
-      setUserSchema({
+      const userSchema: Schema = {
         name: schemaDetails.name,
         reference: schemaDetails.reference,
         schema: {
@@ -74,9 +80,18 @@ export default function SchemaDesigner({
           },
         },
         use: 'UPLOAD',
-      })
+      }
+      const { reference } = userSchema
+      const steps = getStepsFromSchema(userSchema, {}, [])
+      setSplitSchema({ reference, steps })
+      setShowForm(false)
     }
-  }, [schemaDetails, pages, setUserSchema])
+  }, [schemaDetails, pages, setSplitSchema])
+
+  // This is the only way we can find to force the form to rerender
+  useEffect(() => {
+    if (!showForm) setShowForm(true)
+  }, [showForm])
 
   const handlePageChange = (_event, newValue) => {
     setSelectedPage(newValue)
@@ -109,8 +124,10 @@ export default function SchemaDesigner({
   }
 
   const addQuestion = (data: SchemaQuestion) => {
-    const pageToAmmend = pages.find((page) => page.reference === selectedPage)
+    const updatedPages = pages
+    const pageToAmmend = updatedPages.find((page) => page.reference === selectedPage)
     pageToAmmend.questions.push(data)
+    setPages(updatedPages)
   }
 
   const reorder = (list, startIndex, endIndex) => {
@@ -144,97 +161,118 @@ export default function SchemaDesigner({
 
   return (
     <Box>
-      <Stack spacing={4}>
-        <Paper
-          sx={{
-            p: 2,
-          }}
-        >
-          <Box component='form' onSubmit={updateSchemaName}>
-            <Stack direction='row' spacing={1}>
-              <TextField
-                required
-                label='Schema reference'
-                onChange={(event): void => setSchemaReference(event.target.value)}
-                value={schemaReference}
-              />
-              <TextField
-                required
-                label='Schema name'
-                onChange={(event): void => setSchemaName(event.target.value)}
-                value={schemaName}
-              />
-              <Button type='submit'>Update schema details</Button>
-            </Stack>
-          </Box>
-        </Paper>
-        <Divider flexItem />
-        <Paper
-          sx={{
-            p: 2,
-          }}
-        >
-          <Box component='form' onSubmit={addNewPage}>
-            <Stack direction='row' spacing={1}>
-              <TextField
-                required
-                label='Page reference'
-                onChange={(event): void => setPageReference(event.target.value)}
-                value={pageReference}
-              />
-              <TextField
-                required
-                label='Page name'
-                onChange={(event): void => setPageName(event.target.value)}
-                value={pageName}
-              />
-              <Button type='submit'>Add new page</Button>
-            </Stack>
-          </Box>
-        </Paper>
-        <Paper
-          sx={{
-            p: 2,
-          }}
-        >
-          <Tabs value={selectedPage} onChange={handlePageChange}>
-            {pages.map((page) => (
-              <Tab key={page.reference} value={page.reference} label={page.title} />
-            ))}
-          </Tabs>
+      <Stack direction='row' spacing={4}>
+        <Box>
+          <Stack spacing={4}>
+            <Paper
+              sx={{
+                p: 2,
+              }}
+            >
+              <Box component='form' onSubmit={updateSchemaName}>
+                <Stack direction='row' spacing={1}>
+                  <TextField
+                    required
+                    label='Schema reference'
+                    onChange={(event): void => setSchemaReference(event.target.value)}
+                    value={schemaReference}
+                  />
+                  <TextField
+                    required
+                    label='Schema name'
+                    onChange={(event): void => setSchemaName(event.target.value)}
+                    value={schemaName}
+                  />
+                  <Button type='submit'>Update schema details</Button>
+                </Stack>
+              </Box>
+            </Paper>
+            <Divider flexItem />
+            <Paper
+              sx={{
+                p: 2,
+              }}
+            >
+              <Box component='form' onSubmit={addNewPage}>
+                <Stack direction='row' spacing={1}>
+                  <TextField
+                    required
+                    label='Page reference'
+                    onChange={(event): void => setPageReference(event.target.value)}
+                    value={pageReference}
+                  />
+                  <TextField
+                    required
+                    label='Page name'
+                    onChange={(event): void => setPageName(event.target.value)}
+                    value={pageName}
+                  />
+                  <Button type='submit'>Add new page</Button>
+                </Stack>
+              </Box>
+            </Paper>
+            <Paper
+              sx={{
+                p: 2,
+              }}
+            >
+              <Tabs value={selectedPage} onChange={handlePageChange}>
+                {pages.map((page) => (
+                  <Tab key={page.reference} value={page.reference} label={page.title} />
+                ))}
+              </Tabs>
 
-          {pages.map((page) => (
-            <Box key={page.reference}>
-              {page.reference === selectedPage && (
-                <Box sx={{ pt: 2 }}>
-                  <DragDropContext onDragEnd={onDragEnd}>
-                    <Droppable droppableId='list'>
-                      {(droppableProvided) => (
-                        <div {...droppableProvided.droppableProps} ref={droppableProvided.innerRef}>
-                          {page.questions.map((question, index) => (
-                            <Draggable key={question.title} draggableId={question.title} index={index}>
-                              {(draggableProvided) => (
-                                <div
-                                  ref={draggableProvided.innerRef}
-                                  {...draggableProvided.draggableProps}
-                                  {...draggableProvided.dragHandleProps}
-                                >
-                                  <Typography key={question.title}>{question.title}</Typography>
-                                </div>
-                              )}
-                            </Draggable>
-                          ))}
-                          {droppableProvided.placeholder}
-                        </div>
-                      )}
-                    </Droppable>
-                  </DragDropContext>
-                  <Button onClick={handleClickOpen}>Add new question</Button>
+              {pages.map((page) => (
+                <Box key={page.reference}>
+                  {page.reference === selectedPage && (
+                    <Box sx={{ pt: 2 }}>
+                      <DragDropContext onDragEnd={onDragEnd}>
+                        <Droppable droppableId='list'>
+                          {(droppableProvided) => (
+                            <div {...droppableProvided.droppableProps} ref={droppableProvided.innerRef}>
+                              {page.questions.map((question, index) => (
+                                <Draggable key={question.title} draggableId={question.title} index={index}>
+                                  {(draggableProvided) => (
+                                    <div
+                                      ref={draggableProvided.innerRef}
+                                      {...draggableProvided.draggableProps}
+                                      {...draggableProvided.dragHandleProps}
+                                    >
+                                      <Typography key={question.title}>{question.title}</Typography>
+                                    </div>
+                                  )}
+                                </Draggable>
+                              ))}
+                              {droppableProvided.placeholder}
+                            </div>
+                          )}
+                        </Droppable>
+                      </DragDropContext>
+                      <Button onClick={handleClickOpen}>Add new question</Button>
+                    </Box>
+                  )}
                 </Box>
-              )}
-            </Box>
-          ))}
-          {selectedPage === '' && <Typography>Add a new page to begin designing your schema</Typography>}
+              ))}
+              {selectedPage === '' && <Typography>Add a new page to begin designing your schema</Typography>}
+            </Paper>
+          </Stack>
+        </Box>
+        <Paper
+          sx={{
+            p: 2,
+            width: '100%',
+          }}
+        >
+          {showForm && splitSchema?.steps[pageIndex]?.schema !== undefined && (
+            <SchemaForm
+              schema={splitSchema.steps[pageIndex].schema}
+              formData={splitSchema.steps[pageIndex].state}
+              uiSchema={splitSchema.steps[pageIndex].uiSchema}
+            >
+              {/* eslint-disable-next-line react/jsx-no-useless-fragment */}
+              <></>
+            </SchemaForm>
+          )}
         </Paper>
       </Stack>
       <QuestionPicker onSubmit={addQuestion} handleClose={handleClose} questionPickerOpen={questionPickerOpen} />
