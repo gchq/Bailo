3a601b0b85c16cc1c8a1f5ccafa53d1b3af1e1cb
ARADDCC002 <ARADDCC002>
2022-12-13 11:47:41 +0000

    undid a change for the documentation link in schemas apge

--- src/SchemaDesign/SchemaList.tsx
+++ src/SchemaDesign/SchemaList.tsx
@@ -12,42 +12,48 @@ export default function SchemaList() {
   const { schemas: deploymentSchemas, isSchemasLoading: isDeploymentSchemasLoading } = useGetSchemas('DEPLOYMENT')
 
   return (
-    !isUploadSchemasLoading &&
-    !isDeploymentSchemasLoading && (
-      <Grid container spacing={2}>
-        <Grid item sm={12} md={6} sx={{ width: '100%' }}>
-          <Paper sx={{ my: 2, p: 2 }}>
-            <Typography variant='h4'>Schema List</Typography>
-            <Box sx={{ my: 2 }}>
-              <Typography variant='h6'>Upload Schemas</Typography>
-              {uploadSchemas &&
-                uploadSchemas.map((schema) => <Typography key={schema.reference}>{schema.name}</Typography>)}
-            </Box>
-            <Box sx={{ my: 2 }}>
-              <Typography variant='h6'>Deployment Schemas</Typography>
-              {deploymentSchemas &&
-                deploymentSchemas.map((schema) => <Typography key={schema.reference}>{schema.name}</Typography>)}
-            </Box>
-          </Paper>
-        </Grid>
-        <Grid item sm={12} md={6} sx={{ width: '100%' }}>
-          <Paper sx={{ my: 2, p: 2 }}>
-            <Stack spacing={2} alignItems='center' sx={{ textAlign: 'center' }}>
-              <Typography>Have an existing schema? Upload it directly below!</Typography>
-              <Typography variant='caption'>
-                {`For more information on creating a schema yourself, please see ${(
-                  <Link target='_blank' href='/docs/administration/getting-started/configuration/making-a-schema'>
+    <>
+      {!isUploadSchemasLoading && !isDeploymentSchemasLoading && (
+        <Grid container spacing={2}>
+          <Grid item sm={12} md={6} sx={{ width: '100%' }}>
+            <Paper sx={{ my: 2, p: 2 }}>
+              <Typography variant='h4'>Schema List</Typography>
+              <Box sx={{ my: 2 }}>
+                <Typography variant='h6'>Upload Schemas</Typography>
+                {uploadSchemas &&
+                  uploadSchemas.map((schema) => <Typography key={schema.reference}>{schema.name}</Typography>)}
+              </Box>
+              <Box sx={{ my: 2 }}>
+                <Typography variant='h6'>Deployment Schemas</Typography>
+                {deploymentSchemas &&
+                  deploymentSchemas.map((schema) => <Typography key={schema.reference}>{schema.name}</Typography>)}
+              </Box>
+            </Paper>
+          </Grid>
+          <Grid item sm={12} md={6} sx={{ width: '100%' }}>
+            <Paper sx={{ my: 2, p: 2 }}>
+              <Stack spacing={2} alignItems='center' sx={{ textAlign: 'center' }}>
+                <Typography>Have an existing schema? Upload it directly below!</Typography>
+                <Typography variant='caption'>
+                  For more information on creating a schema yourself, please see
+                  <Link
+                    sx={{ pl: 0.5 }}
+                    target='_blank'
+                    href='/docs/administration/getting-started/configuration/making-a-schema'
+                  >
                     the documentation
                   </Link>
-                )}.`}
-              </Typography>
-              <Button sx={{ maxWidth: '250px' }} variant='contained'>
-                Upload Schema
-              </Button>
-            </Stack>
-          </Paper>
+                  .
+                </Typography>
+                <Button sx={{ maxWidth: '250px' }} variant='contained'>
+                  Upload Schema
+                </Button>
+              </Stack>
+            </Paper>
+          </Grid>
         </Grid>
-      </Grid>
-    )
+      )}
+      {(isUploadSchemasLoading || isDeploymentSchemasLoading) && <Typography>Loading...</Typography>}
+    </>
   )
 }
