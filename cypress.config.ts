53ecccbeafd3873cfd4d7269fbc61c73e07dc41b
a3957273 <89583054+a3957273@users.noreply.github.com>
2022-12-24 11:51:59 +0000

    disable videos

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,7 @@ import config from 'config'
 export default defineConfig({
   e2e: {
     baseUrl: `${config.get('app.protocol')}://${config.get('app.host')}:${config.get('app.port')}`,
+    video: false,
     defaultCommandTimeout: 10000,
   },
 })
