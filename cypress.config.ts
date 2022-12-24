30b9107d25b4270d93ca600c7631585c7c475930
a3957273 <89583054+a3957273@users.noreply.github.com>
2022-12-24 11:20:59 +0000

    enable video

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,6 @@ import config from 'config'
 export default defineConfig({
   e2e: {
     baseUrl: `${config.get('app.protocol')}://${config.get('app.host')}:${config.get('app.port')}`,
-    video: false,
     defaultCommandTimeout: 10000,
   },
 })
