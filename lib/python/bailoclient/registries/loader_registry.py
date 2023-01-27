51acfef0f8ee084cf677b788e8831fafaebbeba4
jo77419 <jess77419@protonmail.com>
2023-01-27 08:36:16 +0000

    registry docstrings

--- lib/python/bailoclient/registries/loader_registry.py
+++ lib/python/bailoclient/registries/loader_registry.py
@@ -3,7 +3,18 @@ from ..loader import Loader
 
 
 def loader(flavour: ModelFlavour):
+    """Loader decorator for registering a model loader
+
+    Args:
+        flavour (ModelFlavour): Model flavour
+    """
+
     def register_loader(func):
+        """Register the model loader function to the loader class
+
+        Args:
+            func (Callable): Function to register
+        """
         Loader.model_loaders[flavour.value] = func
 
     return register_loader
--- lib/python/bailoclient/registries/template_registry.py
+++ lib/python/bailoclient/registries/template_registry.py
@@ -3,7 +3,18 @@ from ..bundler import Bundler
 
 
 def template(flavour: ModelFlavour):
+    """Template decorator for registering a model template
+
+    Args:
+        flavour (ModelFlavour): Model flavour
+    """
+
     def register_template(func):
+        """Register the model.py template code to the bundler class
+
+        Args:
+            func (Callable): Function to register (function should return template path)
+        """
         Bundler.model_py_templates[flavour.value] = func()
 
     return register_template
