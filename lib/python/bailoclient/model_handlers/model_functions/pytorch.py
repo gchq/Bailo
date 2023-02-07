1f7cee5f649f602bb94a408d40333a211df3c7c6
jo77419 <jess77419@protonmail.com>
2023-02-07 14:05:39 +0000

    update pytorch loader to be via pytorch or mlflow

--- lib/python/bailoclient/model_handlers/model_functions/pytorch.py
+++ lib/python/bailoclient/model_handlers/model_functions/pytorch.py
@@ -1,3 +1,5 @@
+import os
+from typing import List
 from pkg_resources import resource_filename
 
 from bailoclient.utils.enums import ModelFlavour
@@ -5,19 +7,50 @@ from bailoclient.model_handlers import bundler, loader, template
 
 
 @bundler(flavour=ModelFlavour.PYTORCH)
-def pytorch_bundler(model, path, code_paths, pip_requirements):
+def pytorch_bundler(model, path: str, code_paths: List[str], pip_requirements: str):
+    """Bundle a Pytorch model with MLflow
+
+    Args:
+        model (Pytorch model): The Pytorch model
+        path (str): Path to export the model to
+        code_paths (List[str]): List of additional code paths
+        pip_requirements (str): Path to requirements.txt file
+    """
     from mlflow.pytorch import save_model
 
-    save_model(model, path, code_paths, pip_requirements)
+    save_model(
+        model, path=path, code_paths=code_paths, pip_requirements=pip_requirements
+    )
 
 
 @loader(flavour=ModelFlavour.PYTORCH)
-def pytorch_loader(model_path):
-    import torch
+def pytorch_loader(model_path: str):
+    """Load model with Pytorch
 
-    return torch.load(model_path)
+    Args:
+        model_path (str): Path to either the model.pth file
+                            or the MLflow model directory
+
+    Returns:
+        Pytorch model: Loaded Pytorch model
+    """
+
+    if os.path.isfile(model_path):
+        import torch
+
+        return torch.load(model_path)
+
+    if os.path.isdir(model_path):
+        from mlflow.pytorch import load_model
+
+        return load_model(model_path)
 
 
 @template(flavour=ModelFlavour.PYTORCH)
 def pytorch_template():
+    """Get the Pytorch model.py template
+
+    Returns:
+        str: Path to Pytorch model template
+    """
     return resource_filename("bailoclient", "resources/templates/pytorch.py")
