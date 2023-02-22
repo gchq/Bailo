8c787fc3222e60ad607a3cced8e3e00ee88e3422
ARADDCC002 <ARADDCC002>
2023-02-22 13:40:45 +0000

    fixed version routing unit tests

--- server/routes/v1/version.spec.ts
+++ server/routes/v1/version.spec.ts
@@ -1,9 +1,10 @@
 import mongoose from 'mongoose'
+import ModelModel from '../../models/Model.js'
 import UserModel from '../../models/User.js'
 import VersionModel from '../../models/Version.js'
-import * as approvalService from '../../services/approval.js'
+import { createVersionApprovals } from '../../services/approval.js'
 import '../../utils/mockMongo'
-import { testUser, testManager, testReviewer, testVersion } from '../../utils/test/testModels.js'
+import { testUser, testManager, testReviewer, testVersion, testModel } from '../../utils/test/testModels.js'
 import {
   authenticatedGetRequest,
   authenticatedPostRequest,
@@ -11,12 +12,17 @@ import {
   validateTestRequest,
 } from '../../utils/test/testUtils.js'
 
+jest.mock('../../services/approval.js', () => ({
+  createVersionApprovals: jest.fn(),
+}))
+
 describe('test version routes', () => {
   beforeEach(async () => {
     await UserModel.create(testUser)
     await UserModel.create(testManager)
     await UserModel.create(testReviewer)
 
+    await ModelModel.create(testModel)
     const versionDoc = await VersionModel.create(testVersion)
     testVersion._id = versionDoc._id
   })
@@ -28,16 +34,16 @@ describe('test version routes', () => {
   })
 
   test('that we can edit a version', async () => {
-    const mock: any = jest.spyOn(approvalService, 'createVersionApprovals')
+    ;(createVersionApprovals as unknown as jest.Mock).mockImplementation()
     const editedVersion = testVersion
     editedVersion.metadata.highLevelDetails.name = 'test2'
-    mock.mockReturnValue({})
     const res = await authenticatedPutRequest(`/api/v1/version/${testVersion._id}`).send(editedVersion.metadata)
     validateTestRequest(res)
     expect(res.body.metadata.highLevelDetails.name).toBe('test2')
   })
 
   test('that we can reset approvals for a version', async () => {
+    ;(createVersionApprovals as unknown as jest.Mock).mockImplementation()
     const res = await authenticatedPostRequest(`/api/v1/version/${testVersion._id}/reset-approvals`)
     validateTestRequest(res)
     expect(res.body.managerApproved).toBe('No Response')
