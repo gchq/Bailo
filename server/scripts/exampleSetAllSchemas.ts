66a1f3199934bce11ab5edd1bb255c680cc744cc
a3957273 <89583054+a3957273@users.noreply.github.com>
2023-02-23 18:52:24 +0000

    change module resolution

--- server/scripts/exampleSetAllSchemas.ts
+++ server/scripts/exampleSetAllSchemas.ts
@@ -1,8 +1,8 @@
 /* eslint-disable import/newline-after-import */
 import { createSchema } from '../services/schema.js'
 import { connectToMongoose, disconnectFromMongoose } from '../utils/database.js'
-import deploySchema from './example_schemas/minimal_deployment_schema.json' assert { type: 'json' }
-import modelSchema from './example_schemas/minimal_upload_schema.json' assert { type: 'json' }
+import deploySchema from './example_schemas/minimal_deployment_schema.json'
+import modelSchema from './example_schemas/minimal_upload_schema.json'
 ;(async () => {
   await connectToMongoose()
 
