050b91f77ea2e60e842fea7d614c511855832447
jo77419 <jess77419@protonmail.com>
2023-03-01 17:09:13 +0000

    update minimal model code paths

--- lib/python/tests/conftest.py
+++ lib/python/tests/conftest.py
@@ -1,4 +1,4 @@
 import os
 
 os.environ["BAILO_URL"] = "http://bailo.com/test/api"
-os.environ["MINIMAL_MODEL_PATH"] = "../../__tests__/example_models/minimal_model"
+os.environ["MINIMAL_MODEL_PATH"] = "../../cypress/fixtures"
