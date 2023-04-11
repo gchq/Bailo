5a3c47d6ad0df803734b5c1f456991ed11360351
a3957273 <89583054+a3957273@users.noreply.github.com>
2023-04-11 22:10:16 +0000

    Deep clone deployment data so deployment can be submitted twice

--- frontend/pages/model/[uuid]/deploy.tsx
+++ frontend/pages/model/[uuid]/deploy.tsx
@@ -3,6 +3,7 @@ import Grid from '@mui/material/Grid'
 import Paper from '@mui/material/Paper'
 import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
+import { cloneDeep } from 'lodash-es'
 import { postEndpoint } from '../../../data/api'
 import { useGetModel } from '../../../data/model'
 import { useGetDefaultSchema, useGetSchemas } from '../../../data/schema'
@@ -84,7 +85,7 @@ export default function Deploy() {
   const onSubmit = async () => {
     setError(undefined)
 
-    const data = getStepsData(splitSchema)
+    const data = cloneDeep(getStepsData(splitSchema))
 
     data.highLevelDetails.modelID = modelUuid
     data.schemaRef = currentSchema?.reference
