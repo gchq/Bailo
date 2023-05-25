8dcc649ff54d64b9104d03261555b1e2818bc579
K25831 <K25831@protonmail.com>
2023-05-25 14:10:05 +0000

    BAI-599 cyress tests for Import model started

--- frontend/cypress/e2e/pages/upload.cy.ts
+++ frontend/cypress/e2e/pages/upload.cy.ts
@@ -6,6 +6,19 @@ describe('Upload page', () => {
   it('Renders a heading', () => {
     cy.get('[data-test=headerTitle]').should('contain.text', 'Upload Model')
   })
+
+  it('Renders a tab header', () => {
+    cy.get('.MuiTabs-flexContainer > :nth-child(3)').should('contain.text', 'Import Model')
+  })
+
+  it('Should open \'Open File\' when SELECT MODEL is clicked', () => {
+    cy.get('.MuiTabs-flexContainer > :nth-child(3)')
+    cy.get('button').should('contains.text', 'SELECT MODEL')
+    cy.get('[data-test=SELECTMODELButton]').click()
+    // cy.get('[data-test=headerTitle]').should('contain.text', 'Open File')
+  })
+
+
 })
 
 export {}
