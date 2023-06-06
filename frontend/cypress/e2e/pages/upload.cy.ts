64b2dd3247ad45cd660556c78e0cabb357dc7117
K25831 <K25831@protonmail.com>
2023-06-06 12:32:03 +0000

    BAI-559 upload zip file continued in cypress tests

--- frontend/cypress/e2e/pages/upload.cy.ts
+++ frontend/cypress/e2e/pages/upload.cy.ts
@@ -17,10 +17,10 @@ describe('Upload page', () => {
   it('uploads zip file', () => {
     cy.get('[data-test=importModel]').click({ force: true })
     cy.get('[data-test=selectModel]').click()
-
-  //model.cy.ts example
-    // cy.log('Selecting code and binary files')
-  //   cy.get('[for=select-code-file]').selectFile('cypress/fixtures/minimal_code.zip', { force: true })
+    //model.cy.ts example
+    cy.log('Selecting minimal_code.zip file')
+    cy.get('[for=select-code-file]').selectFile('cypress/fixtures/minimal_code.zip', { force: true })
+  })
 })
 
 export {}
