b165b212d58d0c4bf45ea200dc492538eec4c573
K25831 <K25831@protonmail.com>
2023-06-08 15:42:33 +0000

    BAI-559 cypress import model, json file upload fail and error, test written and passing

--- frontend/cypress/e2e/pages/upload.cy.ts
+++ frontend/cypress/e2e/pages/upload.cy.ts
@@ -3,16 +3,9 @@ describe('Upload page', () => {
     cy.visit('/upload')
   })
 
-  // it('Renders a heading', () => {
-  //   cy.get('[data-test=headerTitle]').should('contain.text', 'Upload Model')
-  // })
-
-  // //Import Model tab cypress tests
-  // it('Renders Import Model, tests SELECT MODEL and tick box', () => {
-  //   cy.get('[data-test=importModel]').click({ force: true })
-  //   cy.get('[data-test=selectModel]').click()
-  //   cy.get('.PrivateSwitchBase-input').click()
-  // })
+  it('Renders a heading', () => {
+    cy.get('[data-test=headerTitle]').should('contain.text', 'Upload Model')
+  })
 
   it('uploads zip file', () => {
     cy.get('[data-test=importModel]').click({ force: true })
@@ -22,6 +15,15 @@ describe('Upload page', () => {
     cy.get('input[type="file"]').selectFile('cypress/fixtures/minimal_code.zip', { force: true })
     cy.get('.PrivateSwitchBase-input').click()
     cy.get('[data-test=submitButton]').click()
+  })
+
+  it('does not upload a json file', () => {
+    cy.get('[data-test=importModel]').click({ force: true })
+    cy.get('[data-test=selectModel]').click()
+    //model.cy.ts example
+    cy.log('Selecting minimal_code.zip file')
+    cy.get('input[type="file"]').selectFile('cypress/fixtures/minimal_metadata.json', { force: true })
+    cy.get('.MuiAlert-standardError')
   })
 })
 
