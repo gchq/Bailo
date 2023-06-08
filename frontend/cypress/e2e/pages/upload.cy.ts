a7a18a1fbbfa64897723bee236056d0371d2440f
K25831 <K25831@protonmail.com>
2023-06-08 15:50:16 +0000

    BAI-559 cypress test to ensure error message goes after failed upload, when a zip file is then selected for upload - written and passing

--- frontend/cypress/e2e/pages/upload.cy.ts
+++ frontend/cypress/e2e/pages/upload.cy.ts
@@ -17,13 +17,16 @@ describe('Upload page', () => {
     cy.get('[data-test=submitButton]').click()
   })
 
-  it('does not upload a json file', () => {
+  it('does not upload a json file, but error message disappears when a zip file is uploaded', () => {
     cy.get('[data-test=importModel]').click({ force: true })
     cy.get('[data-test=selectModel]').click()
     //model.cy.ts example
     cy.log('Selecting minimal_code.zip file')
     cy.get('input[type="file"]').selectFile('cypress/fixtures/minimal_metadata.json', { force: true })
     cy.get('.MuiAlert-standardError')
+    cy.get('[data-test=selectModel]').click()
+    cy.get('input[type="file"]').selectFile('cypress/fixtures/minimal_code.zip', { force: true })
+    cy.get('.MuiAlert-standardError').should('not.exist')
   })
 })
 
