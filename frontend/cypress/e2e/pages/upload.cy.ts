6a4df2fd049c74bab0678b865f714bc11124c67c
K25831 <K25831@protonmail.com>
2023-06-14 10:40:27 +0000

    BAI-556 test updated to new naming conventions. Tests all passing

--- frontend/cypress/e2e/pages/upload.cy.ts
+++ frontend/cypress/e2e/pages/upload.cy.ts
@@ -8,7 +8,7 @@ describe('Upload page', () => {
   })
 
   it('uploads zip file', () => {
-    cy.get('[data-test=importModel]').click({ force: true })
+    cy.get('[data-test=importModelTab]').click({ force: true })
     cy.get('[data-test=selectModel]').click()
     //model.cy.ts example
     cy.log('Selecting minimal_code.zip file')
@@ -18,7 +18,7 @@ describe('Upload page', () => {
   })
 
   it('does not upload a json file, but error message disappears when a zip file is uploaded', () => {
-    cy.get('[data-test=importModel]').click({ force: true })
+    cy.get('[data-test=importModelTab]').click({ force: true })
     cy.get('[data-test=selectModel]').click()
     //model.cy.ts example
     cy.log('Selecting minimal_code.zip file')
