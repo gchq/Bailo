855bc3de102e49674c201c0edbc6072a48e0238a
JR40159 <126243293+JR40159@users.noreply.github.com>
2023-07-04 16:15:23 +0000

    Add minimal mocked config for route tests

--- backend/src/utils/test/testUtils.ts
+++ backend/src/utils/test/testUtils.ts
@@ -19,6 +19,48 @@ vi.mock('../../utils/user.js', () => {
   }
 })
 
+vi.mock('../../utils/config.js', () => {
+  return {
+    __esModule: true,
+    default: {
+      app: {
+        app: {
+          protocol: '',
+        },
+      },
+      logging: {
+        stroom: {
+          enabled: false,
+        },
+        file: {
+          enabled: false,
+        },
+      },
+      minio: {
+        connection: {
+          endPoint: 'fake',
+        },
+        buckets: {
+          uploads: 'uploads',
+        },
+      },
+      oauth: {
+        enabled: false,
+      },
+      ui: {
+        seldonVersions: [
+          {
+            name: 'seldonio - 1.10.0',
+            image: 'seldonio/seldon-core-s2i-python37:1.10.0',
+          },
+        ],
+        banner: '',
+        registry: '',
+      },
+    },
+  }
+})
+
 export function authenticatedGetRequest(path: string) {
   const request = supertest(server)
   return request.get(path).set('Content-Type', 'application/json').set('Accept', 'application/json')
