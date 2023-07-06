ee552c23db125a9ddceaa73775d439a61a3071c1
ARADDCC002 <89992537+ARADDCC002@users.noreply.github.com>
2023-07-06 12:28:36 +0000

    fixed index page e2e test

--- frontend/cypress/e2e/pages/index.cy.ts
+++ frontend/cypress/e2e/pages/index.cy.ts
@@ -9,11 +9,19 @@ describe('Home page', () => {
 
   it('Should toggle dark mode when switch is toggled', () => {
     cy.get('[data-test=userMenuButton]').click()
-    cy.get('[data-test=appBar]').should('have.css', 'background-color', 'rgb(39, 89, 142)')
+    cy.get('[data-test=appBar]').should(
+      'have.css',
+      'background',
+      'rgba(0, 0, 0, 0) linear-gradient(276deg, rgb(214, 37, 96) 0%, rgb(84, 39, 142) 100%) repeat scroll 0% 0%'
+    )
     cy.get('[data-test=toggleDarkMode]').click()
     cy.get('[data-test=appBar]').should('have.css', 'background-color', 'rgb(36, 36, 36)')
     cy.get('[data-test=toggleDarkMode]').click()
-    cy.get('[data-test=appBar]').should('have.css', 'background-color', 'rgb(39, 89, 142)')
+    cy.get('[data-test=appBar]').should(
+      'have.css',
+      'background',
+      'rgba(0, 0, 0, 0) linear-gradient(276deg, rgb(214, 37, 96) 0%, rgb(84, 39, 142) 100%) repeat scroll 0% 0%'
+    )
   })
 })
 
