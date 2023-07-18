f659b55dd161f1131ebf687e350965f7bb45950b
K25831 <K25831@protonmail.com>
2023-07-18 11:06:19 +0000

    BAI-735 selector file made and team and model name selection code transferred.

--- frontend/pages/beta/model/new/model.tsx
+++ frontend/pages/beta/model/new/model.tsx
@@ -1,12 +1,12 @@
 import { Lock, LockOpen } from '@mui/icons-material'
 import {
-  Autocomplete,
+  // Autocomplete,
   Box,
   Button,
   Card,
   Divider,
   FormControlLabel,
-  MenuItem,
+  // MenuItem,
   Radio,
   RadioGroup,
   Stack,
@@ -17,11 +17,12 @@ import {
 import { useState } from 'react'
 import { NewModelData } from 'types/types'
 
+import Selector from '../../../../src/common/Selector'
 import Wrapper from '../../../../src/Wrapper.beta'
 
 export default function NewModel() {
-  const [teamName, setTeamName] = useState('')
-  const [modelName, setModelName] = useState('')
+  // const [teamName, setTeamName] = useState('')
+  // const [modelName, setModelName] = useState('')
   const [description, setDescription] = useState('')
   const [accessibility, setAccessibility] = useState<NewModelData['accessibility']>('public')
 
@@ -63,32 +64,6 @@ export default function NewModel() {
     )
   }
 
-  const team = [
-    { value: 'teamOne', label: 'team 1' },
-    { value: 'teamTwo', label: 'team 2' },
-    { value: 'teamThree', label: 'team 3' },
-    { value: 'teamFour', label: 'team 4' },
-    { value: 'teamFive', label: 'team 5' },
-    { value: 'teamSix', label: 'team 6' },
-    { value: 'teamSeven', label: 'team 7' },
-    { value: 'teamEight', label: 'team 8' },
-    { value: 'teamNine', label: 'team 9' },
-    { value: 'teamTen', label: 'team 10' },
-    { value: 'teamEleven', label: 'team 11' },
-    { value: 'teamTwelve', label: 'team 12' },
-    { value: 'teamThirteen', label: 'team 13' },
-    { value: 'teamFourteen', label: 'team 14' },
-    { value: 'teamFifteen', label: 'team 15' },
-    { value: 'teamSixteen', label: 'team 16' },
-  ]
-
-  const modelNames = [
-    { value: 'modelOne', label: 'model 1' },
-    { value: 'modelTwo', label: 'model 2' },
-    { value: 'modelThree', label: 'model 3' },
-    { value: 'modelFour', label: 'model 4' },
-  ]
-
   return (
     <Wrapper title='Create a new Model' page='marketplace'>
       <Card sx={{ p: 4, maxWidth: 500, m: 'auto' }}>
@@ -105,41 +80,13 @@ export default function NewModel() {
                   <Typography sx={{ fontWeight: 'bold' }}>
                     Team <span style={{ color: 'red' }}>*</span>
                   </Typography>
-                  <Stack spacing={2} sx={{ width: 200 }}>
-                    <Autocomplete
-                      freeSolo
-                      options={team.map((option) => option.label)}
-                      renderInput={(params) => (
-                        <TextField
-                          {...params}
-                          required
-                          size='small'
-                          value={teamName}
-                          onChange={(e) => setTeamName(e.target.value)}
-                        />
-                      )}
-                    />
-                  </Stack>
+                  //team bits here
                 </Stack>
                 <Stack>
                   <Typography sx={{ fontWeight: 'bold' }}>
                     Model name <span style={{ color: 'red' }}>*</span>
                   </Typography>
-                  <Stack spacing={2} sx={{ width: 200 }}>
-                    <Autocomplete
-                      freeSolo
-                      options={modelNames.map((option) => option.label)}
-                      renderInput={(params) => (
-                        <TextField
-                          {...params}
-                          required
-                          size='small'
-                          value={modelName}
-                          onChange={(e) => setModelName(e.target.value)}
-                        />
-                      )}
-                    />
-                  </Stack>
+                  //model name bits here
                 </Stack>
               </Stack>
               <Stack>
---
+++ frontend/src/common/Selector.tsx
@@ -0,0 +1,73 @@
+import { Autocomplete, Stack, TextField } from '@mui/material'
+import { useState } from 'react'
+
+export default function Selector() {
+  const [teamName, setTeamName] = useState('')
+  const [modelName, setModelName] = useState('')
+
+  const team = [
+    { value: 'teamOne', label: 'team 1' },
+    { value: 'teamTwo', label: 'team 2' },
+    { value: 'teamThree', label: 'team 3' },
+    { value: 'teamFour', label: 'team 4' },
+    { value: 'teamFive', label: 'team 5' },
+    { value: 'teamSix', label: 'team 6' },
+    { value: 'teamSeven', label: 'team 7' },
+    { value: 'teamEight', label: 'team 8' },
+    { value: 'teamNine', label: 'team 9' },
+    { value: 'teamTen', label: 'team 10' },
+    { value: 'teamEleven', label: 'team 11' },
+    { value: 'teamTwelve', label: 'team 12' },
+    { value: 'teamThirteen', label: 'team 13' },
+    { value: 'teamFourteen', label: 'team 14' },
+    { value: 'teamFifteen', label: 'team 15' },
+    { value: 'teamSixteen', label: 'team 16' },
+  ]
+
+  const modelNames = [
+    { value: 'modelOne', label: 'model 1' },
+    { value: 'modelTwo', label: 'model 2' },
+    { value: 'modelThree', label: 'model 3' },
+    { value: 'modelFour', label: 'model 4' },
+  ]
+
+  const teamNameSelector = () => {
+    return (
+      <Stack spacing={2} sx={{ width: 200 }}>
+        <Autocomplete
+          freeSolo
+          options={team.map((option) => option.label)}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              required
+              size='small'
+              value={teamName}
+              onChange={(e) => setTeamName(e.target.value)}
+            />
+          )}
+        />
+      </Stack>
+    )
+  }
+
+  const modelNameSelector = () => {
+    return (
+      <Stack spacing={2} sx={{ width: 200 }}>
+        <Autocomplete
+          freeSolo
+          options={modelNames.map((option) => option.label)}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              required
+              size='small'
+              value={modelName}
+              onChange={(e) => setModelName(e.target.value)}
+            />
+          )}
+        />
+      </Stack>
+    )
+  }
+}
