1de88f1e6e95aeef956f185a2aa19a95ebfa5373
a3957273 <89583054+a3957273@users.noreply.github.com>
2023-07-28 09:29:36 +0000

    Add unit tests for model

--- backend/test/services/model.spec.ts
+++ backend/test/services/model.spec.ts
@@ -1,35 +1,50 @@
 import { describe, expect, test, vi } from 'vitest'
 
-import { createModel } from '../../src/services/v2/model.js'
+import { createModel, getModelById } from '../../src/services/v2/model.js'
+
+const mocks = vi.hoisted(() => {
+  const save = vi.fn()
+  const findOne = vi.fn()
+
+  const Model: any = vi.fn(() => ({
+    save,
+  }))
+
+  Model.findOne = findOne
+
+  return {
+    save,
+    findOne,
+    convertStringToId: vi.fn(() => 'model-id'),
+    Model,
+  }
+})
+
+vi.mock('../../src/utils/v2/id.js', () => ({
+  convertStringToId: mocks.convertStringToId,
+}))
+
+vi.mock('../../src/models/v2/Model.js', () => ({ default: mocks.Model }))
+
+vi.mock('../../src/external/v2/authorisation/index.js', async () => ({
+  ...((await vi.importActual('../../src/external/v2/authorisation/index.js')) as object),
+  default: { userModelAction: vi.fn(() => true) },
+}))
 
 describe('services > model', () => {
   test('createModel > good', async () => {
-    const mocks = vi.hoisted(() => {
-      const save = vi.fn()
-
-      return {
-        save,
-        convertStringToId: vi.fn(() => 'model-id'),
-        Model: vi.fn(() => ({
-          save,
-        })),
-      }
-    })
-
-    vi.mock('../../src/utils/v2/id.js', () => ({
-      convertStringToId: mocks.convertStringToId,
-    }))
-
-    vi.mock('../../src/models/v2/Model.js', () => ({ default: mocks.Model }))
-
-    vi.mock('../../src/external/v2/authorisation/index.js', async () => ({
-      ...((await vi.importActual('../../src/external/v2/authorisation/index.js')) as object),
-      default: { userModelAction: vi.fn(() => true) },
-    }))
-
     await createModel({} as any, {} as any)
 
     expect(mocks.save).toBeCalled()
     expect(mocks.Model).toBeCalled()
+  })
+
+  test('getModelById > good', async () => {
+    mocks.findOne.mockResolvedValueOnce('mocked')
+
+    const model = await getModelById({} as any, {} as any)
+
+    expect(mocks.findOne).toBeCalled()
+    expect(model).toBe('mocked')
   })
 })
