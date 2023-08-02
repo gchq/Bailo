9b6fcfd65351733246cd4bd1d5df6e2ae6d77beb
a3957273 <89583054+a3957273@users.noreply.github.com>
2023-08-02 10:09:40 +0000

    Unit tests

--- backend/test/routes/model/getModel.spec.ts
+++ backend/test/routes/model/getModel.spec.ts
@@ -6,6 +6,13 @@ import { describe, expect, test, vi } from 'vitest'
 import { getModelSchema } from '../../../src/routes/v2/model/getModel.js'
 import { createFixture, testGet } from '../../../src/utils/v2/test/routes.js'
 
+const mockValidate = vi.hoisted(() => {
+  return {
+    parse: vi.fn(() => ({})),
+  }
+})
+vi.mock('../../../utils/v2/validate.js', () => mockValidate)
+
 describe('routes > model > getModel', () => {
   test('200 > ok', async () => {
     vi.mock('../../../src/services/v2/model.js', () => ({
@@ -13,6 +20,22 @@ describe('routes > model > getModel', () => {
     }))
 
     const fixture = createFixture(getModelSchema)
+    const res = await testGet(`/api/v2/model/${fixture.params.modelId}`)
+
+    console.log(res.body)
+
+    expect(res.statusCode).toBe(200)
+    expect(res.body).matchSnapshot()
+  })
+
+  test('200 > ok > unit', async () => {
+    vi.mock('../../../src/services/v2/model.js', () => ({
+      getModelById: vi.fn(() => ({ _id: 'test' })),
+    }))
+
+    const fixture = createFixture(getModelSchema)
+    mockValidate.parse.mockReturnValueOnce(fixture)
+
     const res = await testGet(`/api/v2/model/${fixture.params.modelId}`)
 
     expect(res.statusCode).toBe(200)
--- backend/test/services/model.spec.ts
+++ backend/test/services/model.spec.ts
@@ -2,7 +2,12 @@ import { describe, expect, test, vi } from 'vitest'
 
 import { createModel, getModelById } from '../../src/services/v2/model.js'
 
-const mocks = vi.hoisted(() => {
+const idMocks = vi.hoisted(() => ({ convertStringToId: vi.fn(() => 'model-id') }))
+vi.mock('../../src/utils/v2/id.js', () => ({
+  convertStringToId: idMocks.convertStringToId,
+}))
+
+const modelMocks = vi.hoisted(() => {
   const save = vi.fn()
   const findOne = vi.fn()
 
@@ -15,16 +20,10 @@ const mocks = vi.hoisted(() => {
   return {
     save,
     findOne,
-    convertStringToId: vi.fn(() => 'model-id'),
     Model,
   }
 })
-
-vi.mock('../../src/utils/v2/id.js', () => ({
-  convertStringToId: mocks.convertStringToId,
-}))
-
-vi.mock('../../src/models/v2/Model.js', () => ({ default: mocks.Model }))
+vi.mock('../../src/models/v2/Model.js', () => ({ default: modelMocks.Model }))
 
 vi.mock('../../src/external/v2/authorisation/index.js', async () => ({
   ...((await vi.importActual('../../src/external/v2/authorisation/index.js')) as object),
@@ -35,16 +34,16 @@ describe('services > model', () => {
   test('createModel > good', async () => {
     await createModel({} as any, {} as any)
 
-    expect(mocks.save).toBeCalled()
-    expect(mocks.Model).toBeCalled()
+    expect(modelMocks.save).toBeCalled()
+    expect(modelMocks.Model).toBeCalled()
   })
 
   test('getModelById > good', async () => {
-    mocks.findOne.mockResolvedValueOnce('mocked')
+    modelMocks.findOne.mockResolvedValueOnce('mocked')
 
     const model = await getModelById({} as any, {} as any)
 
-    expect(mocks.findOne).toBeCalled()
+    expect(modelMocks.findOne).toBeCalled()
     expect(model).toBe('mocked')
   })
 })
