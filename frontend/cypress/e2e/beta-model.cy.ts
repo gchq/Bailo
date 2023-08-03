385937c3ae5ea54589f403fecfc184f707f5f414
K25831 <K25831@protonmail.com>
2023-08-03 09:29:02 +0000

    BAI-740 beta create model cypress tests - page loading passes, started the create model test.

--- frontend/cypress/e2e/beta-model.cy.ts
+++ frontend/cypress/e2e/beta-model.cy.ts
@@ -1,29 +1,20 @@
 const betaModelUrl = 'localhost:8080/beta/model/new/model'
 
 describe('Beta create new model', () => {
-  before(() => {
-    cy.log('Navigate to beta create model page')
+  it('loads the Create a new model Page', () => {
     cy.visit(betaModelUrl)
+    cy.get('[data-test=createModelPageTitle]').contains('Create a new model')
   })
 
-  // it('loads the Create a new model Page', () => {
-  //   cy.get('[data-test=createModelPageTitle]').contains('Create a new model')
-  // })
-
   it('creates a new model', () => {
-    //select team
-    //select model
-    // write description
-    //click 'create model'
-    // access control public v private
+    cy.visit(betaModelUrl)
+    cy.get('[data-test=teamSelector]').click()
+    // cy.get('[data-test=modelSelector]').select()
+    // cy.get('[data-test=modelDescription]').type()
 
-    cy.get('[data-test=teamSelector]').select()
-    cy.get('[data-test=modelSelector]').select()
-    cy.get('[data-test=modelDescription]').type()
-
-    cy.get('[data-test=publicButtonSelector]').click()
+    // cy.get('[data-test=publicButtonSelector]').click()
     //   cy.get('[data-test=privateButtonSelector]').click()
 
-    cy.get('[data-test=createModelButton]').click()
+    // cy.get('[data-test=createModelButton]').click()
   })
 })
