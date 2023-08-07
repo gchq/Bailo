f1a2ee500441716fd49be391d7431ad8a6b815e0
JR40159 <126243293+JR40159@users.noreply.github.com>
2023-08-07 09:23:55 +0000

    Update logs

--- backend/src/utils/signals.ts
+++ backend/src/utils/signals.ts
@@ -1,12 +1,14 @@
+import logger from "./logger.js"
+
 process.on('SIGINT', () => {
-  console.info('SIGINT signal received: Interrupted')
+  logger.info('SIGINT signal received: Interrupted')
   process.exit(0)
 })
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function registerSigTerminate(httpServer: any) {
   process.on('SIGTERM', () => {
-    console.info('SIGTERM signal received: closing HTTP server')
+    logger.info('SIGTERM signal received: closing HTTP server')
     httpServer.close(() => {
       process.exit(0)
     })
