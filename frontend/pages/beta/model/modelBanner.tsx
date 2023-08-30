4918e6143573ad153ccd5cd8ee3ff304c1fafed8
tt38665 <tt38665>
2023-08-30 10:37:56 +0000

    work in progress for banner

---
+++ frontend/pages/beta/model/modelBanner.tsx
@@ -0,0 +1,38 @@
+import Container from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Grid'
+import Paper from '@mui/material/Paper'
+import * as React from 'react'
+
+export default function modelPageBanner() {
+  return (
+    <Paper
+      sx={{
+        backgroundColor: 'secondary',
+        color: 'black',
+        paddingTop: '0.25rem',
+        paddingBottom: '0.25rem',
+        display: 'flex',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+      }}
+    >
+      <Container maxWidth='xl'>
+        <Typography>Message goes here</Typography>
+      </Container>
+      <Button
+        variant='contained'
+        style={{
+          maxWidth: '600px',
+          maxHeight: '20px',
+          minWidth: '60px',
+          minHeight: '20px',
+        }}
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        onClick={() => {}}
+      >
+        Review
+      </Button>
+    </Paper>
+  )
+}
