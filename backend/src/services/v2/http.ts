9deb60154397be93fe9b20ed8a2e4da8fcdffae1
a3957273 <89583054+a3957273@users.noreply.github.com>
2023-09-03 13:32:08 +0000

    Add test for https agent

--- backend/src/services/v2/http.ts
+++ backend/src/services/v2/http.ts
@@ -2,6 +2,6 @@ import https from 'node:https'
 
 // This function has the same syntax as 'https.Agent', but is centralised throughout
 // the application in case it needs to be altered.
-export function getHttpsAgent(config: https.AgentOptions | undefined) {
+export function getHttpsAgent(config?: https.AgentOptions) {
   return new https.Agent({ ...config })
 }
