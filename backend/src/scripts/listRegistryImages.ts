ec06ab24c7115c0d35b0088f8f373d7058829633
a3957273 <89583054+a3957273@users.noreply.github.com>
2023-09-03 13:28:40 +0000

    Create a service for a common http interface

--- backend/src/scripts/listRegistryImages.ts
+++ backend/src/scripts/listRegistryImages.ts
@@ -1,12 +1,12 @@
 import axios from 'axios'
-import https from 'https'
 
 import { getAccessToken } from '../routes/v1/registryAuth.js'
+import { getHttpsAgent } from '../services/v2/http.js'
 import config from '../utils/config.js'
 import { connectToMongoose, disconnectFromMongoose } from '../utils/database.js'
 import logger from '../utils/logger.js'
 
-const httpsAgent = new https.Agent({
+const httpsAgent = getHttpsAgent({
   rejectUnauthorized: !config.registry.insecure,
 })
 
