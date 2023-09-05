d6173cad08338433faf7a75a6a950b9241bc3909
a3957273 <89583054+a3957273@users.noreply.github.com>
2023-09-05 11:21:48 +0000

    Only instantiate one https agent

--- backend/src/services/v2/http.ts
+++ backend/src/services/v2/http.ts
@@ -2,6 +2,11 @@ import https from 'node:https'
 
 // This function has the same syntax as 'https.Agent', but is centralised throughout
 // the application in case it needs to be altered.
+let httpsAgent: https.Agent | undefined
 export function getHttpsAgent(config?: https.AgentOptions) {
-  return new https.Agent({ ...config })
+  if (!httpsAgent) {
+    httpsAgent = new https.Agent({ ...config })
+  }
+
+  return httpsAgent
 }
