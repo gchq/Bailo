051e2a8ba2f5c0e1f9b7bba483609f3307012234
a3957273 <89583054+a3957273@users.noreply.github.com>
2023-09-05 11:28:42 +0000

    Revert "Only instantiate one https agent"
    This reverts commit d6173cad08338433faf7a75a6a950b9241bc3909.

--- backend/src/services/v2/http.ts
+++ backend/src/services/v2/http.ts
@@ -2,11 +2,6 @@ import https from 'node:https'
 
 // This function has the same syntax as 'https.Agent', but is centralised throughout
 // the application in case it needs to be altered.
-let httpsAgent: https.Agent | undefined
 export function getHttpsAgent(config?: https.AgentOptions) {
-  if (!httpsAgent) {
-    httpsAgent = new https.Agent({ ...config })
-  }
-
-  return httpsAgent
+  return new https.Agent({ ...config })
 }
