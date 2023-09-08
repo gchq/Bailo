1ff9f862af8dc3016782846deeef70cde9094304
ARADDCC002 <ARADDCC002>
2023-09-08 10:42:55 +0000

    removed unused import

--- frontend/src/model/beta/Releases.tsx
+++ frontend/src/model/beta/Releases.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Stack, Tooltip } from '@mui/material'
+import { Box, Button, Stack } from '@mui/material'
 import { ReactElement, useEffect, useMemo, useState } from 'react'
 
 import { useGetReleasesForModelId } from '../../../actions/release'
