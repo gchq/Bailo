612bd485f80e8cb388a770be095af14affe1f6e0
araddcc002 <araddcc002@euro.ngc.com>
2023-10-18 15:57:50 +0000

    updated colours for beta email

--- backend/src/services/v2/smtp/templates/releaseReview.ts
+++ backend/src/services/v2/smtp/templates/releaseReview.ts
@@ -32,13 +32,13 @@ export class ReleaseReviewEmail extends BaseEmailTemplate {
   setHtml(releaseName: string, modelId: string, baseUrl: string, author: string) {
     this.html = mjml2html(
       super.wrapper(`
-    <mj-section background-color="#27598e" padding-bottom="5px" padding-top="20px">
+    <mj-section background-color="#54278e" padding-bottom="5px" padding-top="20px">
       <mj-column width="100%">
         <mj-text align="center" color="#FFF" font-size="13px" font-family="Helvetica" padding-left="25px" padding-right="25px" padding-bottom="28px" padding-top="28px"><span style="font-size:20px; font-weight:bold">You have been requested to review a ${ReviewKind.Release.toLowerCase()}.</span>
         </mj-text>
       </mj-column>
     </mj-section>
-    <mj-section background-color="#356cc7" padding-bottom="15px">
+    <mj-section background-color="#9e60f2" padding-bottom="15px">
       <mj-column>
         <mj-text align="center" color="#FFF" font-size="15px" font-family="Ubuntu, Helvetica, Arial, sans-serif" padding-left="25px" padding-right="25px" padding-bottom="0px"><strong>Model Name</strong></mj-text>
         <mj-text align="center" color="#FFF" font-size="13px" font-family="Helvetica" padding-left="25px" padding-right="25px" padding-bottom="20px" padding-top="10px">${releaseName}</mj-text>
@@ -54,14 +54,14 @@ export class ReleaseReviewEmail extends BaseEmailTemplate {
         <mj-text align="center" color="#FFF" font-size="13px" font-family="Helvetica" padding-left="25px" padding-right="25px" padding-bottom="20px" padding-top="10px">${author}</mj-text>
       </mj-column>
     </mj-section>
-    <mj-section background-color="#27598e" padding-bottom="20px" padding-top="20px">
+    <mj-section background-color="#54278e" padding-bottom="20px" padding-top="20px">
       <mj-column width="50%">
-        <mj-button background-color="#f37f58" color="#FFF" font-size="14px" align="center" font-weight="bold" border="none" padding="15px 30px" border-radius="10px" href="${baseUrl}/model/${modelId}" font-family="Helvetica" padding-left="25px" padding-right="25px" padding-bottom="10px">Open ${
+        <mj-button background-color="#d62560" color="#FFF" font-size="14px" align="center" font-weight="bold" border="none" padding="15px 30px" border-radius="10px" href="${baseUrl}/model/${modelId}" font-family="Helvetica" padding-left="25px" padding-right="25px" padding-bottom="10px">Open ${
           ReviewKind.Release
         }</mj-button>
       </mj-column>
       <mj-column width="50%">
-        <mj-button background-color="#f37f58" color="#FFF" font-size="14px" align="center" font-weight="bold" border="none" padding="15px 30px" border-radius="10px" href="${baseUrl}/review" font-family="Helvetica" padding-left="25px" padding-right="25px" padding-bottom="12px">See Reviews</mj-button>
+        <mj-button background-color="#d62560" color="#FFF" font-size="14px" align="center" font-weight="bold" border="none" padding="15px 30px" border-radius="10px" href="${baseUrl}/review" font-family="Helvetica" padding-left="25px" padding-right="25px" padding-bottom="12px">See Reviews</mj-button>
       </mj-column>
     </mj-section>
   `),
