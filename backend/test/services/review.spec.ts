1db7b966dfa87ad8ff0faaec2f78306ea617f933
a3957273 <89583054+a3957273@users.noreply.github.com>
2023-11-04 23:54:27 +0000

    Add review interface

--- backend/test/services/review.spec.ts
+++ backend/test/services/review.spec.ts
@@ -2,7 +2,7 @@ import { describe, expect, test, vi } from 'vitest'
 
 import Model from '../../src/models/v2/Model.js'
 import Release from '../../src/models/v2/Release.js'
-import { Decision } from '../../src/models/v2/Review.js'
+import { Decision, ReviewInterface } from '../../src/models/v2/Review.js'
 import { createReleaseReviews, findReviews, respondToReview } from '../../src/services/v2/review.js'
 import { ReviewKind } from '../../src/types/v2/enums.js'
 
