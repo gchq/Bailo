deb257c0036258bd86a279aaa6c8053588c06322
a3957273 <89583054+a3957273@users.noreply.github.com>
2023-11-16 11:04:00 +0000

    bug authorisation

--- backend/src/connectors/Authorisation/AuthorisationDefault.ts
+++ backend/src/connectors/Authorisation/AuthorisationDefault.ts
@@ -5,8 +5,20 @@ import { Model } from '../../types/types.js'
 
 export default class AuthorisationDefault {
   async getUserFromReq(req: Request) {
+    console.log('We got a request coming in on URL', req.originalUrl)
+    console.log('The user object is', req.get('x-user'))
+    console.log('The roles object is', req.get('x-roles'))
+
+    console.log(JSON.stringify(req.get('x-user')))
+
+    console.log('value here is', req.get('x-user') ?? '{}')
+    console.log('value of roles is', req.get('x-roles') ?? '["user", "admin"]')
+
     const userId = 'user'
     const email = 'user@example.com'
+
+    console.log('We successfully got the user and email address')
+
     const data = JSON.parse(req.get('x-user') ?? '{}')
     const roles = JSON.parse(req.get('x-roles') ?? '["user", "admin"]')
 
