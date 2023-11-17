98f53ae8b249d575ee7868a0cc639ce28c28ffd9
tt38665 <125564578+TT38665@users.noreply.github.com>
2023-11-17 15:53:41 +0000

    implemented error message

--- frontend/src/model/beta/overview/TemplatePage.tsx
+++ frontend/src/model/beta/overview/TemplatePage.tsx
@@ -2,7 +2,10 @@ import { PostAdd } from '@mui/icons-material'
 import { Box, Button, Divider, Stack, Typography } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
 import { useRouter } from 'next/router'
+import Loading from 'src/common/Loading'
+import MessageAlert from 'src/MessageAlert'
 
+import { useGetSchemas } from '../../../../actions/schema'
 import { ModelInterface } from '../../../../types/v2/types'
 
 type TemplatePageProps = {
@@ -12,69 +15,79 @@ type TemplatePageProps = {
 export default function TemplatePage({ model }: TemplatePageProps) {
   const theme = useTheme()
   const router = useRouter()
+  const { schemas, isSchemasLoading, isSchemasError } = useGetSchemas(model.card.schemaId)
 
   function handleCreateFromScratchClick() {
     router.push(`/beta/model/${model.id}/schema`)
   }
 
+  if (isSchemasError) {
+    return <MessageAlert message={isSchemasError.info.message} severity='error' />
+  }
+
   return (
-    <Box sx={{ maxWidth: '900px', mx: 'auto', my: 4 }}>
-      <Stack spacing={4} justifyContent='center' alignItems='center'>
-        <Typography component='h2' variant='h6' color='primary' data-test='createModelCardOverview'>
-          Create a model card
-        </Typography>
-        <PostAdd fontSize='large' color='primary' />
-        <Typography variant='body1'>
-          Model cards are required to ensure that our models are ethical, secure and effective. A model card is a living
-          document, it lives with your code and will evolve over time.
-        </Typography>
-        <Stack
-          direction={{ sm: 'column', md: 'row' }}
-          spacing={4}
-          justifyContent='center'
-          alignItems='center'
-          divider={<Divider orientation='vertical' flexItem />}
-        >
-          <Box
-            sx={{
-              border: 'solid 2px',
-              borderColor: theme.palette.primary.main,
-              p: 4,
-              borderRadius: 2,
-              width: '300px',
-            }}
-          >
-            <Stack spacing={2}>
-              <Typography component='h3' variant='h6'>
-                Create from a template
-              </Typography>
-              <Typography variant='body1'>Create a model using an existing model as a template.</Typography>
-              <Button variant='contained' disabled>
-                Create
-              </Button>
+    <>
+      {isSchemasLoading && <Loading />}
+      {schemas && !isSchemasLoading && (
+        <Box sx={{ maxWidth: '900px', mx: 'auto', my: 4 }}>
+          <Stack spacing={4} justifyContent='center' alignItems='center'>
+            <Typography component='h2' variant='h6' color='primary' data-test='createModelCardOverview'>
+              Create a model card
+            </Typography>
+            <PostAdd fontSize='large' color='primary' />
+            <Typography variant='body1'>
+              Model cards are required to ensure that our models are ethical, secure and effective. A model card is a
+              living document, it lives with your code and will evolve over time.
+            </Typography>
+            <Stack
+              direction={{ sm: 'column', md: 'row' }}
+              spacing={4}
+              justifyContent='center'
+              alignItems='center'
+              divider={<Divider orientation='vertical' flexItem />}
+            >
+              <Box
+                sx={{
+                  border: 'solid 2px',
+                  borderColor: theme.palette.primary.main,
+                  p: 4,
+                  borderRadius: 2,
+                  width: '300px',
+                }}
+              >
+                <Stack spacing={2}>
+                  <Typography component='h3' variant='h6'>
+                    Create from a template
+                  </Typography>
+                  <Typography variant='body1'>Create a model using an existing model as a template.</Typography>
+                  <Button variant='contained' disabled>
+                    Create
+                  </Button>
+                </Stack>
+              </Box>
+              <Box
+                sx={{
+                  border: 'solid 2px',
+                  borderColor: theme.palette.primary.main,
+                  p: 4,
+                  borderRadius: 2,
+                  width: '300px',
+                }}
+              >
+                <Stack spacing={2}>
+                  <Typography component='h3' variant='h6'>
+                    Create from scratch
+                  </Typography>
+                  <Typography variant='body1'>Create a model from scratch using a predefined schema.</Typography>
+                  <Button variant='contained' onClick={handleCreateFromScratchClick}>
+                    Create
+                  </Button>
+                </Stack>
+              </Box>
             </Stack>
-          </Box>
-          <Box
-            sx={{
-              border: 'solid 2px',
-              borderColor: theme.palette.primary.main,
-              p: 4,
-              borderRadius: 2,
-              width: '300px',
-            }}
-          >
-            <Stack spacing={2}>
-              <Typography component='h3' variant='h6'>
-                Create from scratch
-              </Typography>
-              <Typography variant='body1'>Create a model from scratch using a predefined schema.</Typography>
-              <Button variant='contained' onClick={handleCreateFromScratchClick}>
-                Create
-              </Button>
-            </Stack>
-          </Box>
-        </Stack>
-      </Stack>
-    </Box>
+          </Stack>
+        </Box>
+      )}
+    </>
   )
 }
