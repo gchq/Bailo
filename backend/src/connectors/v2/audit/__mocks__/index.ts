7b2c525eec9cbf9f62c109e1c74b0ca8b4521381
JR40159 <126243293+JR40159@users.noreply.github.com>
2023-11-17 13:59:47 +0000

    Add tests for audit model

--- backend/src/connectors/v2/audit/__mocks__/index.ts
+++ backend/src/connectors/v2/audit/__mocks__/index.ts
@@ -1,6 +1,11 @@
 import { vi } from 'vitest'
 
 const audit = {
+  onCreateModel: vi.fn(),
+  onViewModel: vi.fn(),
+  onUpdateModel: vi.fn(),
+  onSearchModel: vi.fn(),
+
   onCreateAccessRequest: vi.fn(),
   onViewAccessRequest: vi.fn(),
   onUpdateAccessRequest: vi.fn(),
