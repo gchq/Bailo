c1bb1d241a8111e0b26eace1b8358195bd608964
araddcc002 <araddcc002@euro.ngc.com>
2023-11-27 16:56:41 +0000

    added a custom 404 page and improved styling of forbidden component

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -1,25 +1,20 @@
-import { GppBad } from '@mui/icons-material'
+import { SentimentVeryDissatisfied } from '@mui/icons-material'
 import { Button, Stack, Typography } from '@mui/material'
 import Link from 'src/Link'
 
-interface ForbiddenProps {
-  errorMessage?: string
-}
-
-export default function Forbidden({ errorMessage }: ForbiddenProps) {
+export default function NotFound() {
   return (
     <>
       <Stack spacing={2} alignItems='center' sx={{ pt: 2 }}>
         <Stack direction='row' justifyContent='center' alignItems='center' spacing={2}>
-          <GppBad color='secondary' sx={{ fontSize: 75 }} />
+          <SentimentVeryDissatisfied color='secondary' sx={{ fontSize: 75 }} />
           <Typography color='secondary' sx={{ fontSize: 75 }}>
-            403
+            404
           </Typography>
         </Stack>
         <Typography component='h3' variant='h5' color='primary' fontWeight='bold'>
-          Uh oh! Looks like you don&apos;t have permission to view this content.
+          Hmm... We&apos;re having trouble finding this page.
         </Typography>
-        <Typography>{errorMessage}</Typography>
         <Link href='/'>
           <Button>Click here to go back to the main page</Button>
         </Link>
