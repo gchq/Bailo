4e286380f5baf24f016a74d4a338a7ee0c95f62c
GB27247 <148772006+GB27247@users.noreply.github.com>
2023-11-30 10:00:59 +0000

    Set verify to True

--- lib/python-beta/tests/conftest.py
+++ lib/python-beta/tests/conftest.py
@@ -18,7 +18,7 @@ from bailo.helper.model import Model
 
 @pytest.fixture
 def integration_client():
-    return Client("http://localhost:8080")
+    return Client("http://localhost:8080", verify=True)
 
 
 @pytest.fixture
