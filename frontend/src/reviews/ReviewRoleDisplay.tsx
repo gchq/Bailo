8d66d66e88132734130b51a9dc9ee6f82c6337a4
tt38665 <125564578+TT38665@users.noreply.github.com>
2024-01-12 16:33:09 +0000

    Made adjustments code

--- frontend/src/reviews/ReviewRoleDisplay.tsx
+++ frontend/src/reviews/ReviewRoleDisplay.tsx
@@ -1,52 +1,32 @@
 import { Typography } from '@mui/material'
 
 import { useGetModelRoles } from '../../actions/model'
-import { DecisionKeys, ReviewRequestInterface } from '../../types/interfaces'
+import { ReviewRequestInterface } from '../../types/interfaces'
+import { getRoleDisplay } from '../../utils/beta/roles'
 import Loading from '../common/Loading'
-//import { getRoleDisplay } from '../../utils/beta/roles'
 import MessageAlert from '../MessageAlert'
-
-//get roles
-//get response types
-//need to do a condition if mtr or msro
-//string statement to say who needs to review release
-// place in map function
-
-// does decisionKeys need to be imported instead
-
-// make message dissappear once task is complete
-// do I need to use a useState for the message so that it is not shown when release/acces has been reviewed?
-
-// export const ResponseTypes = {
-//   Approve: 'approve',
-//   RequestChanges: 'request_changes',
-// } as const
-
-// export type ResponseTypeKeys = (typeof ResponseTypes)[keyof typeof ResponseTypes]
 
 type DisplayRoleProps = {
   review: ReviewRequestInterface
-  kind?: DecisionKeys
 }
 
-export default function ReviewRoleDisplay({ review, kind }: DisplayRoleProps) {
-  const { _modelRoles, isModelRolesLoading, isModelRolesError } = useGetModelRoles(review.model.id)
-
-  const showRole = () => {
-    if (review.kind === 'release' || (review.kind === 'access' && kind !== 'approve') || kind !== 'request_changes') {
-      return `This ${review.kind} needs to be reviewed by the ${review.role} `
-    }
-    //need to do error/bad request
-  }
+export default function ReviewRoleDisplay({ review }: DisplayRoleProps) {
+  const { modelRoles, isModelRolesLoading, isModelRolesError } = useGetModelRoles(review.model.id)
 
   if (isModelRolesError) {
     return <MessageAlert message={isModelRolesError.info.message} severity='error' />
   }
 
+  if (review.responses.length > 0) {
+    return <></>
+  }
+
   return (
     <>
       {isModelRolesLoading && <Loading />}
-      <Typography>{showRole()}</Typography>
+      <Typography variant='subtitle2' sx={{ fontStyle: 'italic' }}>
+        {`This ${review.kind} needs to be reviewed by the ${getRoleDisplay(review.role, modelRoles)}.`}
+      </Typography>
     </>
   )
 }
--- frontend/src/reviews/ReviewsList.tsx
+++ frontend/src/reviews/ReviewsList.tsx
@@ -1,11 +1,10 @@
 import { List } from '@mui/material'
 import { useGetReviewRequestsForUser } from 'actions/review'
-import { useEffect, useState } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 import EmptyBlob from 'src/common/EmptyBlob'
 import Loading from 'src/common/Loading'
 import MessageAlert from 'src/MessageAlert'
 import ReviewItem from 'src/reviews/ReviewItem'
-import ReviewRoleDisplay from 'src/reviews/ReviewRoleDisplay'
 import { ReviewRequestInterface } from 'types/interfaces'
 
 type ReviewsListProps = {
@@ -35,13 +34,10 @@ export default function ReviewsList({ isActive = true, kind = 'all' }: ReviewsLi
       {filteredReviews.length === 0 && <EmptyBlob text='No reviews found' />}
       <List>
         {filteredReviews.map((review) => (
-          <>
-            <ReviewItem
-              review={review}
-              key={`${review.model.id}-${review.semver || review.accessRequestId}-${review.role}`}
-            />
-            <ReviewRoleDisplay review={review} />
-          </>
+          <ReviewItem
+            review={review}
+            key={`${review.model.id}-${review.semver || review.accessRequestId}-${review.role}`}
+          />
         ))}
       </List>
     </>
