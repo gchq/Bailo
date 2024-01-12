0a596396e58aa9bcfbc82d9984ef4edf5d3056b9
ARADDCC012 <110473008+ARADDCC012@users.noreply.github.com>
2024-01-12 15:30:26 +0000

    Fixed MessageAlert bug

--- frontend/src/settings/beta/authentication/AuthenticationTab.tsx
+++ frontend/src/settings/beta/authentication/AuthenticationTab.tsx
@@ -72,6 +72,7 @@ export default function AuthenticationTab() {
   }
 
   const handleCloseDialog = () => {
+    setDeleteTokenErrorMessage('')
     setTokenToDelete('')
     setIsConfirmationDialogOpen(false)
   }
