62f670d4a02dcc5056c42d466911831ca239050b
araddcc002 <araddcc002@euro.ngc.com>
2024-01-16 16:31:13 +0000

    updated tests again

--- backend/test/routes/model/accessRequest/postAccessRequestComment.spec.ts
+++ backend/test/routes/model/accessRequest/postAccessRequestComment.spec.ts
@@ -1,24 +1,17 @@
 import { describe, expect, test, vi } from 'vitest'
 
 import audit from '../../../../src/connectors/v2/audit/__mocks__/index.js'
-import AccessRequest from '../../../../src/models/v2/AccessRequest.js'
 import { postAccessRequestCommentSchema } from '../../../../src/routes/v2/model/accessRequest/postAccessRequestComment.js'
 import { createFixture, testPost } from '../../../testUtils/routes.js'
 
+vi.mock('../../../../src/utils/config.js')
+vi.mock('../../../../src/utils/v2/config.js')
 vi.mock('../../../../src/utils/user.js')
 vi.mock('../../../../src/connectors/v2/audit/index.js')
 vi.mock('../../../../src/connectors/v2/authorisation/index.js')
 
 vi.mock('../../../../src/services/v2/accessRequest.js', () => ({
-  newAccessRequestComment: vi.fn(
-    () =>
-      new AccessRequest({
-        id: 'test',
-        createdBy: 'user',
-        modelId: 'test',
-        comments: [],
-      }),
-  ),
+  newAccessRequestComment: vi.fn(() => ({ message: 'test' })),
 }))
 
 describe('routes > release > postReleaseComment', () => {
--- backend/test/routes/model/release/postReleaseComment.spec.ts
+++ backend/test/routes/model/release/postReleaseComment.spec.ts
@@ -4,6 +4,8 @@ import audit from '../../../../src/connectors/v2/audit/__mocks__/index.js'
 import { postReleaseCommentSchema } from '../../../../src/routes/v2/release/postReleaseComment.js'
 import { createFixture, testPost } from '../../../testUtils/routes.js'
 
+vi.mock('../../../../src/utils/config.js')
+vi.mock('../../../../src/utils/v2/config.js')
 vi.mock('../../../../src/utils/user.js')
 vi.mock('../../../../src/connectors/v2/audit/index.js')
 vi.mock('../../../../src/connectors/v2/authorisation/index.js')
