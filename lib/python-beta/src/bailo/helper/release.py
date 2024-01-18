08d3dfb41a225439b2f40b19976472c3ffbf13f8
GB27247 <148772006+GB27247@users.noreply.github.com>
2024-01-18 10:12:47 +0000

    Add download by filename endpoint

--- lib/python-beta/src/bailo/helper/release.py
+++ lib/python-beta/src/bailo/helper/release.py
@@ -132,16 +132,16 @@ class Release:
             draft,
         )
 
-    def download(self, file_id: str, buffer: BytesIO) -> str:
+    def download(self, filename: str, buffer: BytesIO) -> str:
         """Give returns a Reading object given the file id.
 
-        :param file_name: The name of the file to retrieve
+        :param filename: The name of the file to retrieve
         :param buffer: A BytesIO object
         :return: A JSON response object
         """
-        return self.client.get_download_file(self.model_id, file_id, buffer)
+        return self.client.get_download_by_filename(self.model_id, str(self.version), filename, buffer)
 
-    def upload(self, name: str, file: BytesIO) -> Any:
+    def upload(self, name: str, file: BytesIO) -> str:
         """Upload files in a given directory to the release.
 
         :param name: The name of the file to upload to bailo
