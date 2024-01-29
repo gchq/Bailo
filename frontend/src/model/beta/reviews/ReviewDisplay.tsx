4ee90f71faac9e5100dfb027022a932adfb27b79
araddcc002 <araddcc002@euro.ngc.com>
2024-01-29 11:56:56 +0000

    resolved various pr comments

--- frontend/src/model/beta/reviews/ReviewDisplay.tsx
+++ frontend/src/model/beta/reviews/ReviewDisplay.tsx
@@ -1,82 +1,40 @@
-import Done from '@mui/icons-material/Done'
 import HourglassEmpty from '@mui/icons-material/HourglassEmpty'
 import { Stack, Tooltip, Typography } from '@mui/material'
 import { useEffect, useState } from 'react'
+import ApprovalsDisplay from 'src/model/beta/reviews/ApprovalsDisplay'
+import { plural } from 'utils/stringUtils'
 
-import { useGetModelRoles } from '../../../../actions/model'
-import { ReviewRequestInterface, ReviewResponse } from '../../../../types/interfaces'
-import Loading from '../../../common/Loading'
-import MessageAlert from '../../../MessageAlert'
+import { ReviewRequestInterface, ReviewResponseWithRole } from '../../../../types/interfaces'
 
 interface ReviewDisplayProps {
   reviews: ReviewRequestInterface[]
 }
 
-interface ReviewResponseWithRole extends ReviewResponse {
-  role: string
-}
-
 export default function ReviewDisplay({ reviews }: ReviewDisplayProps) {
-  const { modelRoles, isModelRolesLoading, isModelRolesError } = useGetModelRoles(
-    reviews[0] ? reviews[0].model.id : undefined,
-  )
-
   const [acceptedReviewResponses, setAcceptedReviewResponses] = useState<ReviewResponseWithRole[]>([])
   const [changesRequestedReviewResponses, setChangesRequestedReviewResponses] = useState<ReviewResponseWithRole[]>([])
 
   useEffect(() => {
     const updatedApprovals: ReviewResponseWithRole[] = []
     const updatedRequests: ReviewResponseWithRole[] = []
-    if (reviews) {
-      reviews.forEach((review) =>
-        review.responses.forEach((reviewResponse) => {
-          reviewResponse.decision === 'approve'
-            ? updatedApprovals.push({ ...reviewResponse, role: review.role })
-            : updatedRequests.push({ ...reviewResponse, role: review.role })
-        }),
-      )
-      setAcceptedReviewResponses(updatedApprovals)
-      setChangesRequestedReviewResponses(updatedRequests)
-    }
+    reviews.forEach((review) =>
+      review.responses.forEach((reviewResponse) => {
+        reviewResponse.decision === 'approve'
+          ? updatedApprovals.push({ ...reviewResponse, role: review.role })
+          : updatedRequests.push({ ...reviewResponse, role: review.role })
+      }),
+    )
+    setAcceptedReviewResponses(updatedApprovals)
+    setChangesRequestedReviewResponses(updatedRequests)
   }, [reviews])
-
-  const approvalsDisplay = () => {
-    const staticRoles = ['owner', 'contributor', 'consumer']
-    const dynamicRoles = modelRoles.filter((role) => !staticRoles.includes(role.id)).map((role) => role)
-    const roleApprovals: string[] = []
-    dynamicRoles.forEach((dynamicRole) => {
-      const acceptedResponsesForRole = acceptedReviewResponses.filter(
-        (acceptedResponse) => acceptedResponse.role === dynamicRole.id,
-      )
-      if (acceptedResponsesForRole.length > 0) roleApprovals.push(dynamicRole.name)
-    })
-    return roleApprovals.map((role) => (
-      <Tooltip
-        title={`${acceptedReviewResponses.length} approval${acceptedReviewResponses.length > 1 ? 's' : ''}`}
-        key={role}
-      >
-        <Stack direction='row'>
-          <Done color='success' fontSize='small' />
-          <Typography variant='caption'>{`Approved by ${role}`}</Typography>
-        </Stack>
-      </Tooltip>
-    ))
-  }
-
-  if (isModelRolesError) {
-    return <MessageAlert message={isModelRolesError.info.message} severity='error' />
-  }
 
   return (
     <>
-      {isModelRolesLoading && <Loading />}
-      {changesRequestedReviewResponses.length === 0 && acceptedReviewResponses.length > 0 && approvalsDisplay()}
+      {changesRequestedReviewResponses.length === 0 && acceptedReviewResponses.length > 0 && (
+        <ApprovalsDisplay modelId={reviews[0].model.id} acceptedReviewResponses={acceptedReviewResponses} />
+      )}
       {changesRequestedReviewResponses.length > 0 && (
-        <Tooltip
-          title={`${changesRequestedReviewResponses.length} ${
-            changesRequestedReviewResponses.length > 1 ? 'reviews' : 'review'
-          } requesting changes`}
-        >
+        <Tooltip title={`${plural(changesRequestedReviewResponses.length, 'review')}`}>
           <Stack direction='row'>
             <HourglassEmpty color='warning' fontSize='small' />
             <Typography variant='caption'>Changes requested</Typography>
