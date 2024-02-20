3dc4d0236ee6d1606db1f8a2d86a15d034f40aa3
tt38665 <125564578+TT38665@users.noreply.github.com>
2024-02-20 11:51:16 +0000

    refactored some of the code for tab component. Still in progress.

--- frontend/src/settings/authentication/TokenTabs.tsx
+++ frontend/src/settings/authentication/TokenTabs.tsx
@@ -1,5 +1,5 @@
-import { LoadingButton } from '@mui/lab'
-import { Box, Dialog, DialogActions, DialogTitle, Tab, Tabs, Typography } from '@mui/material'
+import { LoadingButton, TabContext, TabList, TabPanel } from '@mui/lab'
+import { Box, Dialog, DialogActions, DialogTitle, Tab } from '@mui/material'
 import { useRouter } from 'next/router'
 import React from 'react'
 import { useEffect, useState } from 'react'
@@ -8,16 +8,12 @@ import { TokenInterface } from 'types/v2/types'
 type TokenTabProps = {
   token?: TokenInterface
 }
-interface TabPanelProps {
-  children?: React.ReactNode
-  index: number
-  value: number
-}
+
 export default function TokenTabs({ token }: TokenTabProps) {
   const [open, setOpen] = useState(false)
   const router = useRouter()
   const [isLoading, setIsLoading] = useState(false)
-  const [showValue, setShowValue] = React.useState(0)
+  const [showValue, setShowValue] = useState('1')
   // const [showAccessKey, setShowAccessKey] = useState(false)
   // const [showSecretKey, setShowSecretKey] = useState(false)
 
@@ -30,7 +26,7 @@ export default function TokenTabs({ token }: TokenTabProps) {
     router.push('/settings?tab=authentication&category=personal')
   }
 
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+  const handleChange = (event, newValue) => {
     setShowValue(newValue)
   }
 
@@ -38,64 +34,28 @@ export default function TokenTabs({ token }: TokenTabProps) {
   // have an icon for each tab
   // make sure tabbing is implemented for accessibility
 
-  function TabPanel(props: TabPanelProps) {
-    const { children, value, index, ...other } = props
-
-    return (
-      <div
-        role='tabpanel'
-        hidden={value !== index}
-        id={`vertical-tabpanel-${index}`}
-        aria-labelledby={`vertical-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box sx={{ p: 3 }}>
-            <Typography>{children}</Typography>
-          </Box>
-        )}
-      </div>
-    )
-  }
-
-  function a11yProps(index: number) {
-    return {
-      id: `vertical-tab-${index}`,
-      'aria-controls': `vertical-tabpanel-${index}`,
-    }
-  }
-
   return (
     <Dialog open={open} onClose={handleClose} fullWidth maxWidth='sm'>
       <DialogTitle>Token Created</DialogTitle>
-      <Box sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: 224 }}>
-        <Tabs
-          orientation='vertical'
-          variant='scrollable'
-          value={showValue}
-          onChange={handleChange}
-          aria-label='Vertical tabs example'
-          sx={{ borderRight: 1, borderColor: 'divider' }}
-          selectionFollowsFocus
-        >
-          <Tab label='Item One' {...a11yProps(0)} />
-          <Tab label='Item Two' {...a11yProps(1)} />
-          <Tab label='Item Three' {...a11yProps(2)} />
-          <Tab label='Item Four' {...a11yProps(3)} />
-        </Tabs>
-        <TabPanel value={showValue} index={0}>
-          Item One
-        </TabPanel>
-        <TabPanel value={showValue} index={1}>
-          Item Two
-        </TabPanel>
-        <TabPanel value={showValue} index={2}>
-          Item Three
-        </TabPanel>
-        <TabPanel value={showValue} index={3}>
-          Item Four
-        </TabPanel>
-      </Box>
+      <TabContext value={showValue}>
+        <Box sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: 224 }}>
+          <TabList
+            orientation='vertical'
+            value={showValue}
+            onChange={handleChange}
+            aria-label='Vertical tabs example'
+            sx={{ borderRight: 1, borderColor: 'divider' }}
+            selectionFollowsFocus
+          >
+            <Tab label='Item One' value='1' />
+            <Tab label='Item Two' value='2' />
+            <Tab label='Item Three' value='3' />
+          </TabList>
+          <TabPanel value='1'>Item One </TabPanel>
+          <TabPanel value='2'>Item Two </TabPanel>
+          <TabPanel value='3'>Item Three </TabPanel>
+        </Box>
+      </TabContext>
       <DialogActions>
         <LoadingButton variant='contained' loading={isLoading} onClick={handleClose}>
           Continue
