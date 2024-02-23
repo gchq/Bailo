7094226ad1d30a54e477ff85d29718e505cab40b
araddcc002 <araddcc002@euro.ngc.com>
2024-02-23 11:26:52 +0000

    added new custom loading spinner

--- frontend/src/common/Loading.tsx
+++ frontend/src/common/Loading.tsx
@@ -1,14 +1,38 @@
-import { Box, CircularProgress } from '@mui/material'
+import { Box } from '@mui/material'
+import { styled } from '@mui/material/styles'
+import Image from 'next/image'
+import BailoLogo from 'public/favicon-colour-transparent.png'
 import { ReactElement } from 'react'
 
 type LoadingProps = {
   size?: number
 }
 
-export default function Loading({ size = 40 }: LoadingProps): ReactElement {
+export default function Loading({ size = 80 }: LoadingProps): ReactElement {
+  const StyledImage = styled(Image)({
+    animation: `nfLoaderSpin infinite 1.5s linear`,
+    animationTimingFunction: ' cubic-bezier(.17,.67,.83,.67)',
+    transformBox: 'fill-box',
+
+    '@keyframes nfLoaderSpin': {
+      from: {
+        transform: 'rotate(0deg)',
+      },
+      to: {
+        transform: 'rotate(360deg)',
+      },
+    },
+  })
+
   return (
     <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-      <CircularProgress size={size} />
+      <StyledImage
+        src={BailoLogo}
+        alt='loading spinner'
+        width={size}
+        height={size}
+        className={'rotate linear infinite'}
+      />
     </Box>
   )
 }
