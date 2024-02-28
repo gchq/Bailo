b937eca898ede5ca51477450b3a05acc8e39dfd0
araddcc002 <araddcc002@euro.ngc.com>
2024-02-28 11:38:29 +0000

    wip

--- frontend/src/MessageAlert.tsx
+++ frontend/src/MessageAlert.tsx
@@ -59,7 +59,7 @@ export default function MessageAlert({
   return (
     <Alert
       severity={severity}
-      sx={{ mb: 2, mt: 2, maxHeight: slimView ? '70px' : 'none' }}
+      sx={{ mb: 2, mt: 2, maxHeight: slimView ? '70px' : 'none', maxWidth: slimView ? '250px' : 'none' }}
       ref={alertRef}
       data-test={dataTest}
     >
