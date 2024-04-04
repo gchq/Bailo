fc987cd9fbc41872a4a4468e05f48266866e5c6f
araddcc002 <araddcc002@euro.ngc.com>
2024-04-04 16:09:16 +0000

    updated styling of expandable button to match model search box
    Former-commit-id: 1f33d1d33b808cc2a3db51ca6d31c27645e58364

--- frontend/src/common/ExpandableButton.tsx
+++ frontend/src/common/ExpandableButton.tsx
@@ -1,39 +1,6 @@
-import { Box, Button } from '@mui/material'
-import { styled, useTheme } from '@mui/material/styles'
-import { CSSProperties, ReactElement, useState } from 'react'
-
-const StyledHoverSpan = styled('span')({
-  animationName: 'button-expand',
-  animationDuration: '.5s',
-  animationDirection: 'forwards',
-  overflow: 'hidden',
-  whiteSpace: 'nowrap',
-  '@keyframes button-expand': {
-    '0%': {
-      maxWidth: '0px',
-    },
-    '100%': {
-      maxWidth: '400px',
-    },
-  },
-})
-
-const StyledSpan = styled('span')({
-  animationName: 'button-shrink',
-  animationDuration: '.5s',
-  animationDirection: 'backwards',
-  animationFillMode: 'forwards',
-  overflow: 'hidden',
-  whiteSpace: 'nowrap',
-  '@keyframes button-shrink': {
-    '0%': {
-      maxWidth: '400px',
-    },
-    '100%': {
-      maxWidth: '0px',
-    },
-  },
-})
+import { Stack, Typography } from '@mui/material'
+import { alpha, styled } from '@mui/material/styles'
+import { CSSProperties, ReactElement } from 'react'
 
 interface ExpandableButtonProps {
   label: string
@@ -41,32 +8,45 @@ interface ExpandableButtonProps {
   onClick: () => void
   ariaLabel: string
   height?: CSSProperties['height']
+  initialMaxWidth?: CSSProperties['maxWidth']
+  onHoverMaxWidth?: CSSProperties['maxWidth']
 }
 
-export default function ExpandableButton({ label, icon, onClick, ariaLabel, height = 'unset' }: ExpandableButtonProps) {
-  const [hover, setHover] = useState(false)
-  const theme = useTheme()
+export default function ExpandableButton({
+  label,
+  icon,
+  onClick,
+  ariaLabel,
+  height = 'unset',
+  initialMaxWidth = '50px',
+  onHoverMaxWidth = '350px',
+}: ExpandableButtonProps) {
+  const StyledButton = styled('button')(({ theme }) => ({
+    maxWidth: initialMaxWidth,
+    webkitTransition: 'max-width 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+    transition: 'max-width 0.5s',
+    alignItems: 'center',
+    overflow: 'hidden',
+    color: theme.palette.common.white,
+    borderStyle: 'none',
+    borderRadius: 3,
+    cursor: 'pointer',
+    height: height,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    '&&:hover': {
+      maxWidth: onHoverMaxWidth,
+    },
+  }))
 
   return (
-    <Box sx={{ display: 'flex', overflow: 'hidden' }}>
-      <Button
-        onMouseEnter={() => setHover(!hover)}
-        onMouseLeave={() => setHover(!hover)}
-        onClick={() => onClick()}
-        variant='outlined'
-        data-test='expandableButton'
-        aria-label={ariaLabel}
-        sx={{ color: theme.palette.common.white, borderColor: `${theme.palette.common.white} !important`, height }}
-      >
-        {icon}
-        {hover ? (
-          <StyledHoverSpan className='test-open' data-test='expandedButtonContent'>
-            {label}
-          </StyledHoverSpan>
-        ) : (
-          <StyledSpan className='test-closed' data-test='collapsedButtonContent' />
-        )}
-      </Button>
-    </Box>
+    <StyledButton data-test='expandableButton' onClick={() => onClick()} aria-label={ariaLabel}>
+      <Stack direction='row' alignItems='center' spacing={2}>
+        <div style={{ paddingLeft: 10, paddingTop: 2 }}>{icon}</div>
+        <Typography sx={{ whiteSpace: 'nowrap', pr: 1 }}>{label}</Typography>
+      </Stack>
+    </StyledButton>
   )
 }
