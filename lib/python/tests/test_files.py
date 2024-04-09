154aa66ac10a031da60b4b4bd016cbf3efb0db24
BW27492 <BW27492@euro.ngc.com>
2024-04-09 16:13:50 +0000

    Tests for multiple file download and filter.

--- lib/python/tests/test_files.py
+++ lib/python/tests/test_files.py
@@ -4,6 +4,7 @@ from io import BytesIO
 
 import pytest
 from bailo.core.exceptions import BailoException
+import os
 
 
 @pytest.mark.integration
@@ -16,11 +17,36 @@ def test_file_upload(example_model):
     with file as file:
         example_release.upload("test", file)
 
-    download_file = example_release.download("test")
+    download_file = example_release.download("test", write=False)
 
     # Check that file uploaded has the same contents as the one downloaded
     assert download_file.content == byte_obj
 
+
+@pytest.mark.integration
+def test_file_download_all(example_model, downloads_path):
+    byte_obj = b"Test Binary"
+    file = BytesIO(byte_obj)
+
+    example_release = example_model.create_release("0.1.0", "test")
+
+    for filename in ["test.json", "test2.txt", "to_exclude.txt"]:
+        example_release.upload(filename, file)
+        file.seek(0)
+
+    example_release.download_all(path=downloads_path, include=["*.txt"], exclude=["to_exclude.txt"])
+
+    assert os.listdir(downloads_path) == ["test2.txt"]
+
+    with open(str(downloads_path.join("test2.txt")), "rb") as f:
+        assert f.read() == byte_obj
+
+@pytest.mark.integration
+def test_file_download_all_no_files(example_model):
+    example_release = example_model.create_release("0.1.0", "test")
+
+    with pytest.raises(BailoException):
+        example_release.download_all()
 
 @pytest.mark.integration
 def test_source_target_doesnt_exist(example_model):
