80eaff1e451fc4f67950730cbc241771c3ce7bd5
GB27247 <148772006+GB27247@users.noreply.github.com>
2024-04-10 16:01:34 +0000

    Remove axios requests

--- frontend/src/model/releases/FileDownload.tsx
+++ frontend/src/model/releases/FileDownload.tsx
@@ -1,9 +1,5 @@
-import FileDownloadIcon from '@mui/icons-material/FileDownload'
-import { LoadingButton } from '@mui/lab'
-import { Grid, Typography } from '@mui/material'
-import axios from 'axios'
+import { Grid, Link, Tooltip, Typography } from '@mui/material'
 import prettyBytes from 'pretty-bytes'
-import useNotification from 'src/hooks/useNotification'
 import { FileInterface, isFileInterface } from 'types/types'
 
 type FileDownloadProps = {
@@ -12,56 +8,22 @@ type FileDownloadProps = {
 }
 
 export default function FileDownload({ modelId, file }: FileDownloadProps) {
-  const sendNotification = useNotification()
-
-  const handleFileDownload = async () => {
-    await axios
-      .get(`/api/v2/model/${modelId}/file/${file._id}/download`)
-      .then((response) => {
-        const url = window.URL.createObjectURL(new Blob([response.data]))
-        const link = document.createElement('a')
-        link.href = url
-        link.setAttribute('download', file.name)
-        document.body.appendChild(link)
-        link.click()
-      })
-      .catch(function (error) {
-        if (error.response) {
-          sendNotification({
-            variant: 'error',
-            msg: `Error code ${error.response.status} recieved from server whilst attemping to download file ${file.name}`,
-          })
-        } else if (error.request) {
-          sendNotification({
-            variant: 'error',
-            msg: `There was a problem with the request whilst attemping to download file ${file.name}`,
-          })
-        } else {
-          sendNotification({ variant: 'error', msg: `Unknown error whilst attemping to download file ${file.name}` })
-        }
-      })
-  }
   return (
     <>
       {isFileInterface(file) && (
-        <Grid container columns={20} key={file.name}>
-          <Grid item xs={1}>
-            <LoadingButton
-              color='primary'
-              sx={{ minWidth: 0, height: 25, width: 25 }}
-              size='small'
-              variant='contained'
-              onClick={handleFileDownload}
-            >
-              <FileDownloadIcon fontSize='small' />
-            </LoadingButton>
+        <Grid container alignItems='center' key={file.name}>
+          <Grid item xs={11}>
+            {isFileInterface(file) && (
+              <Tooltip title={file.name}>
+                <Link href={`/api/v2/model/${modelId}/file/${file._id}/download`} data-test={`fileLink-${file.name}`}>
+                  <Typography noWrap textOverflow='ellipsis'>
+                    {file.name}
+                  </Typography>
+                </Link>
+              </Tooltip>
+            )}
           </Grid>
-          <Grid item xs={15}>
-            <Typography noWrap textOverflow='ellipsis'>
-              {file.name}
-            </Typography>
-          </Grid>
-          <Grid item xs={4} textAlign='right'>
+          <Grid item xs={1} textAlign='right'>
             <Typography variant='caption'>{prettyBytes(file.size)}</Typography>
           </Grid>
         </Grid>
