d8102e9ff2bf14707bcd130d35b0e45e12f9687f
JR40159 <126243293+JR40159@users.noreply.github.com>
2024-04-19 12:13:51 +0000

    Fix existing tests

--- backend/test/services/mirroredModel.spec.ts
+++ backend/test/services/mirroredModel.spec.ts
@@ -2,7 +2,7 @@ import { describe, expect, test, vi } from 'vitest'
 
 import authorisation from '../../src/connectors/authorisation/index.js'
 import { UserInterface } from '../../src/models/User.js'
-import { exportModelCardRevisions } from '../../src/services/mirroredModel.js'
+import { exportModel } from '../../src/services/mirroredModel.js'
 
 vi.mock('../../src/connectors/authorisation/index.js')
 
@@ -28,10 +28,22 @@ vi.mock('../../src/services/log.js', async () => ({
 }))
 
 const modelMocks = vi.hoisted(() => ({
-  getModelById: vi.fn(),
+  getModelById: vi.fn(() => ({ settings: { mirroredModelId: 'abc' } })),
   getModelCardRevisions: vi.fn(() => [{ toJSON: vi.fn(), version: 123 }]),
 }))
 vi.mock('../../src/services/model.js', () => modelMocks)
+
+const releaseMocks = vi.hoisted(() => ({
+  getModelById: vi.fn(),
+  getModelCardRevisions: vi.fn(() => [{ toJSON: vi.fn(), version: 123 }]),
+}))
+vi.mock('../../src/services/release.js', () => releaseMocks)
+
+const fileMocks = vi.hoisted(() => ({
+  getModelById: vi.fn(),
+  getModelCardRevisions: vi.fn(() => [{ toJSON: vi.fn(), version: 123 }]),
+}))
+vi.mock('../../src/services/release.js', () => fileMocks)
 
 const archiverMocks = vi.hoisted(() => ({
   append: vi.fn(),
@@ -41,9 +53,9 @@ const archiverMocks = vi.hoisted(() => ({
 vi.mock('archiver', () => ({ default: vi.fn(() => archiverMocks) }))
 
 describe('services > mirroredModel', () => {
-  test('exportModelCardRevisions > not enabled', async () => {
+  test('exportModel > not enabled', async () => {
     vi.spyOn(configMock, 'modelMirror', 'get').mockReturnValue({ enabled: false })
-    const response = exportModelCardRevisions({} as UserInterface, 'modelId', true)
+    const response = exportModel({} as UserInterface, 'modelId', true)
 
     expect(response).rejects.toThrowError('Model mirroring has not been enabled.')
   })
@@ -51,21 +63,23 @@ describe('services > mirroredModel', () => {
   test('exportModelCardRevisions > bad authorisation', async () => {
     vi.mocked(authorisation.model).mockResolvedValueOnce({ info: 'You do not have permission', success: false, id: '' })
 
-    const response = exportModelCardRevisions({} as UserInterface, 'modelId', true)
+    const response = exportModel({} as UserInterface, 'modelId', true)
     expect(response).rejects.toThrowError(/^You do not have permission/)
   })
 
   test('exportModelCardRevisions > missing disclaimer agreement', async () => {
-    const response = exportModelCardRevisions({} as UserInterface, 'modelId', false)
-    expect(response).rejects.toThrowError(/^Unable to export model card./)
+    const response = exportModel({} as UserInterface, 'modelId', false)
+    expect(response).rejects.toThrowError(
+      /^You must agree to the disclaimer agreement before being able to export a model./,
+    )
   })
 
-  test('exportModelCardRevisions > unable to create zip file', async () => {
+  test('exportModelCardRevisions > unable to create model card zip file', async () => {
     archiverMocks.append.mockImplementationOnce(() => {
       throw Error('Error making zip file')
     })
-    const response = exportModelCardRevisions({} as UserInterface, 'modelId', true)
-    expect(response).rejects.toThrowError(/^Error when generating the zip file./)
+    const response = exportModel({} as UserInterface, 'modelId', true)
+    expect(response).rejects.toThrowError(/^Error when generating the model card revisions zip file./)
   })
 
   /*
