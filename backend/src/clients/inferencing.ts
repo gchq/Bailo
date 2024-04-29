00865dc0cd1baedf51cfa9eca27c855f5d7909dc
GB27247 <148772006+GB27247@users.noreply.github.com>
2024-04-29 16:18:20 +0000

    Add testing for the endpoints

--- backend/src/clients/inferencing.ts
+++ backend/src/clients/inferencing.ts
@@ -1,5 +1,7 @@
+import fetch from 'node-fetch'
+
 import config from '../utils/config.js'
-import { InternalError } from '../utils/error.js'
+import { BadReq, InternalError } from '../utils/error.js'
 
 interface InferenceService {
   modelId: string
@@ -8,9 +10,7 @@ interface InferenceService {
   port: number
 }
 
-type CreateInferenceService = Pick<InferenceService, 'modelId' | 'image' | 'tag' | 'port'>
-
-export async function createInferenceService(inferenceServiceParams: CreateInferenceService) {
+export async function createInferenceService(inferenceServiceParams: InferenceService) {
   let res
   try {
     res = await fetch(`${config.inference.connection.host}/api/deploy`, {
@@ -21,58 +21,29 @@ export async function createInferenceService(inferenceServiceParams: CreateInfer
   } catch (err) {
     throw InternalError('Unable to communicate with the inferencing service.', { err })
   }
+  const body = await res.json()
   if (!res.ok) {
-    const context = {
-      url: res.url,
-      status: res.status,
-      statusText: res.statusText,
-    }
-    throw InternalError('Unrecoginsed response returned by inferencing service.', context)
+    throw BadReq('Unrecognised response returned by the inferencing service.')
   }
+
+  return body
 }
 
-type UpdateInferenceService = Pick<InferenceService, 'modelId' | 'image' | 'tag' | 'port'>
-
-export async function updateInferenceService(inferenceServiceParams: UpdateInferenceService) {
+export async function updateInferenceService(inferenceServiceParams: InferenceService) {
   let res
   try {
     res = await fetch(`${config.inference.connection.host}/api/update`, {
-      method: 'UPDATE',
+      method: 'PATCH',
       headers: new Headers({ 'content-type': 'application/json' }),
       body: JSON.stringify(inferenceServiceParams),
     })
   } catch (err) {
     throw InternalError('Unable to communicate with the inferencing service.', { err })
   }
+  const body = await res.json()
   if (!res.ok) {
-    const context = {
-      url: res.url,
-      status: res.status,
-      statusText: res.statusText,
-    }
-    throw InternalError('Unrecoginsed response returned by inferencing service.', context)
+    throw BadReq('Unrecognised response returned by the inferencing service.')
   }
-}
 
-type DeleteInferenceService = Pick<InferenceService, 'modelId' | 'image' | 'tag' | 'port'>
-
-export async function DeleteInferenceService(inferenceServiceParams: DeleteInferenceService) {
-  let res
-  try {
-    res = await fetch(`${config.inference.connection.host}/api/delete`, {
-      method: 'DELETE',
-      headers: new Headers({ 'content-type': 'application/json' }),
-      body: JSON.stringify(inferenceServiceParams),
-    })
-  } catch (err) {
-    throw InternalError('Unable to communicate with the inferencing service.', { err })
-  }
-  if (!res.ok) {
-    const context = {
-      url: res.url,
-      status: res.status,
-      statusText: res.statusText,
-    }
-    throw InternalError('Unrecoginsed response returned by inferencing service.', context)
-  }
+  return body
 }
