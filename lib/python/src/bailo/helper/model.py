92387e686f6c61094d3d072cdcb4acc68eb01a0b
BW27492 <BW27492@euro.ngc.com>
2024-05-14 15:43:47 +0000

    Mvednon-unique model class methods to entry class

--- lib/python/src/bailo/helper/model.py
+++ lib/python/src/bailo/helper/model.py
@@ -9,6 +9,7 @@ from bailo.core.client import Client
 from bailo.core.enums import EntryKind, ModelVisibility
 from bailo.core.exceptions import BailoException
 from bailo.core.utils import NestedDict
+from bailo.helper.entry import Entry
 from bailo.helper.release import Release
 from semantic_version import Version
 
@@ -20,13 +21,12 @@ except ImportError:
     ml_flow = False
 
 
-class Model:
+class Model(Entry):
     """Represent a model within Bailo.
 
     :param client: A client object used to interact with Bailo
     :param model_id: A unique ID for the model
     :param name: Name of model
-    :param kind: Either a Model or a Datacard
     :param description: Description of model
     :param visibility: Visibility of model, using ModelVisibility enum (e.g Public or Private), defaults to None
     """
@@ -36,53 +36,45 @@ class Model:
         client: Client,
         model_id: str,
         name: str,
-        kind: EntryKind,
         description: str,
         visibility: ModelVisibility | None = None,
     ) -> None:
-        self.client = client
+        super().__init__(
+            client=client, id=model_id, name=name, description=description, kind=EntryKind.MODEL, visibility=visibility
+        )
 
         self.model_id = model_id
-        self.name = name
-        self.kind = kind
-        self.description = description
-        self.visibility = visibility
-
-        self.model_card = None
-        self.model_card_version = None
-        self.model_card_schema = None
 
     @classmethod
     def create(
         cls,
         client: Client,
         name: str,
-        kind: EntryKind,
         description: str,
         team_id: str,
         visibility: ModelVisibility | None = None,
     ) -> Model:
-        """Build a model from Bailo and uploads it.
+        """Build a model from Bailo and upload it.
 
         :param client: A client object used to interact with Bailo
         :param name: Name of model
-        :param kind: Either a Model or a Datacard
         :param description: Description of model
         :param team_id: A unique team ID
         :param visibility: Visibility of model, using ModelVisibility enum (e.g Public or Private), defaults to None
         :return: Model object
         """
-        res = client.post_model(name=name, kind=kind, description=description, team_id=team_id, visibility=visibility)
+        res = client.post_model(
+            name=name, kind=EntryKind.MODEL, description=description, team_id=team_id, visibility=visibility
+        )
         model = cls(
             client=client,
             model_id=res["model"]["id"],
             name=name,
-            kind=kind,
             description=description,
             visibility=visibility,
         )
 
-        model.__unpack(res["model"])
+        model._Entry__unpack(res["model"])
 
         return model
 
@@ -98,80 +90,31 @@ class Model:
         model = cls(
             client=client,
             model_id=model_id,
-            kind=res["kind"],
             name=res["name"],
             description=res["description"],
         )
-        model.__unpack(res)
 
+        model._Entry__unpack(res)
         model.get_card_latest()
 
         return model
 
-    def update(self) -> None:
-        """Upload and retrieves any changes to the model summary on Bailo."""
-        res = self.client.patch_model(
-            model_id=self.model_id,
-            name=self.name,
-            kind=self.kind,
-            description=self.description,
-            visibility=self.visibility,
-        )
-        self.__unpack(res["model"])
-
     def update_model_card(self, model_card: dict[str, Any] | None = None) -> None:
-        """Upload and retrieves any changes to the model card on Bailo.
+        """Upload and retrieve any changes to the model card on Bailo.
 
         :param model_card: Model card dictionary, defaults to None
 
         ..note:: If a model card is not provided, the current model card attribute value is used
         """
-        if model_card is None:
-            model_card = self.model_card
-        res = self.client.put_model_card(model_id=self.model_id, metadata=model_card)
-        self.__unpack_mc(res["card"])
-
-    def card_from_schema(self, schema_id: str) -> None:
-        """Create a model card using a schema on Bailo.
-
-        :param schema_id: A unique schema ID
-        """
-        res = self.client.model_card_from_schema(model_id=self.model_id, schema_id=schema_id)
-        self.__unpack_mc(res["card"])
-
-    def card_from_model(self):
-        """Copy a model card from a different model (not yet implemented).
-
-        :raises NotImplementedError: Not implemented error
-        """
-        raise NotImplementedError
-
-    def card_from_template(self):
-        """Create a model card using a template (not yet implemented).
-
-        :raises NotImplementedError: Not implemented error
-        """
-        raise NotImplementedError
-
-    def get_card_latest(self) -> None:
-        """Get the latest model card from Bailo."""
-        res = self.client.get_model(model_id=self.model_id)
-        if "card" in res["model"]:
-            self.__unpack_mc(res["model"]["card"])
-        else:
-            raise BailoException(f"A model card doesn't exist for model {self.model_id}")
-
-    def get_card_revision(self, version: str) -> None:
-        """Get a specific model card revision from Bailo.
-
-        :param version: Model card version
-        """
-        res = self.client.get_model_card(model_id=self.model_id, version=version)
-        self.__unpack_mc(res["modelCard"])
+        self._Entry__update_card(card=model_card)
 
     def create_experiment(
         self,
     ) -> Experiment:
+        """Create an experiment locally
+
+        :return: An experiment object
+        """
         return Experiment.create(model=self)
 
     def create_release(
@@ -254,42 +197,29 @@ class Model:
         """
         raise NotImplementedError
 
-    def get_roles(self):
-        """Get all roles for the model.
+    @property
+    def model_card(self):
+        return self.card
 
-        :return: List of roles
-        """
-        res = self.client.get_model_roles(model_id=self.model_id)
+    @model_card.setter
+    def model_card(self, value):
+        self.card = value
 
-        return res["roles"]
+    @property
+    def model_card_version(self):
+        return self.card_version
 
-    def get_user_roles(self):
-        """Get all user roles for the model.
+    @model_card_version.setter
+    def model_card_version(self, value):
+        self.card_version = value
 
-        :return: List of user roles
-        """
-        res = self.client.get_model_user_roles(model_id=self.model_id)
+    @property
+    def model_card_schema(self):
+        return self.card_schema
 
-        return res["roles"]
-
-    def __unpack(self, res):
-        self.model_id = res["id"]
-        self.name = res["name"]
-        self.description = res["description"]
-
-        if res["visibility"] == "private":
-            self.visibility = ModelVisibility.PRIVATE
-        else:
-            self.visibility = ModelVisibility.PUBLIC
-
-    def __unpack_mc(self, res):
-        self.model_card_version = res["version"]
-        self.model_card_schema = res["schemaId"]
-
-        try:
-            self.model_card = res["metadata"]
-        except KeyError:
-            self.model_card = None
+    @model_card_schema.setter
+    def model_card_schema(self, value):
+        self.card_schema = value
 
 
 class Experiment:
