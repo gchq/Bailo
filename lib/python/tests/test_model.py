e525edb509ad7f68a1ac21b290442dcc4ece69cd
BW27492 <BW27492@euro.ngc.com>
2024-05-14 15:45:44 +0000

    New tests for datacard class

--- lib/python/tests/test_model.py
+++ lib/python/tests/test_model.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import pytest
 from bailo import Client, Experiment, Model, ModelVisibility
-from bailo.core.enums import EntryKind
 from bailo.core.exceptions import BailoException
 from bailo.core.utils import NestedDict
 
@@ -19,17 +18,16 @@ def test_create_experiment_from_model(local_model):
 
 @pytest.mark.integration
 @pytest.mark.parametrize(
-    ("name", "kind", "description", "team_id", "visibility"),
+    ("name", "description", "team_id", "visibility"),
     [
-        ("test-model", EntryKind.MODEL, "test", "Uncategorised", ModelVisibility.PUBLIC),
-        ("test-model", EntryKind.MODEL, "test", "Uncategorised", None),
+        ("test-model", "test", "Uncategorised", ModelVisibility.PUBLIC),
+        ("test-model", "test", "Uncategorised", None),
     ],
 )
-def test_create_get_from_version_and_update(
+def test_create_get_from_id_and_update(
     name: str,
     description: str,
     team_id: str,
-    kind: EntryKind,
     visibility: ModelVisibility | None,
     integration_client: Client,
 ):
@@ -37,7 +35,6 @@ def test_create_get_from_version_and_update(
     model = Model.create(
         client=integration_client,
         name=name,
-        kind=kind,
         description=description,
         team_id=team_id,
         visibility=visibility,
@@ -61,7 +58,6 @@ def test_get_and_update_latest_model_card(integration_client):
     model = Model.create(
         client=integration_client,
         name="test-model",
-        kind=EntryKind.MODEL,
         description="test",
         team_id="Uncategorised",
         visibility=ModelVisibility.PUBLIC,
@@ -79,7 +75,6 @@ def get_model_card_without_creation(integration_client):
     model = Model.create(
         client=integration_client,
         name="test-model",
-        kind=EntryKind.MODEL,
         description="test",
         team_id="Uncategorised",
         visibility=ModelVisibility.PUBLIC,
@@ -95,7 +90,6 @@ def test_get_releases(integration_client):
     model = Model.create(
         client=integration_client,
         name="test-model",
-        kind=EntryKind.MODEL,
         description="test",
         team_id="Uncategorised",
         visibility=ModelVisibility.PUBLIC,
@@ -119,7 +113,6 @@ def test_create_release_without_model_card(integration_client):
     model = Model.create(
         client=integration_client,
         name="test-model",
-        kind=EntryKind.MODEL,
         description="test",
         team_id="Uncategorised",
         visibility=ModelVisibility.PUBLIC,
