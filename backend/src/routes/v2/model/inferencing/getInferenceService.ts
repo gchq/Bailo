cebf73610329f8b4db842c948d264a5a7ab4396a
GB27247 <148772006+GB27247@users.noreply.github.com>
2024-06-03 15:34:42 +0000

    Fix tests

--- backend/src/routes/v2/model/inferencing/getInferenceService.ts
+++ backend/src/routes/v2/model/inferencing/getInferenceService.ts
@@ -18,7 +18,7 @@ export const getInferenceSchema = z.object({
   }),
 })
 
-if (config.ui.inference?.enabled) {
+if (config.ui?.inference?.enabled) {
   registerPath({
     method: 'get',
     path: '/api/v2/model/{modelId}/inference/{image}/{tag}',
