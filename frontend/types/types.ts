d5daf7bd7169f732fc84f3353cea51c2bf4959b0
GCHQDeveloper36 <73173408+GCHQDeveloper36@users.noreply.github.com>
2024-07-05 17:42:27 +0000

    Migrate access request schema select to main SchemaSelect component

--- frontend/types/types.ts
+++ frontend/types/types.ts
@@ -229,6 +229,13 @@ export interface Role {
   description?: string
 }
 
+export const SchemaKindLabel = {
+  model: 'model',
+  accessRequest: 'access request',
+  dataCard: 'data card',
+}
+export type SchemaKindLabelKeys = (typeof SchemaKindLabel)[keyof typeof SchemaKindLabel]
+
 export const SchemaKind = {
   MODEL: 'model',
   ACCESS_REQUEST: 'accessRequest',
