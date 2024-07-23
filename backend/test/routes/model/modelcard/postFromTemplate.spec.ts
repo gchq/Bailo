16fe5097352745f69c91b8e952585d4ae548b73d
araddcc002 <araddcc002@euro.ngc.com>
2024-07-23 11:51:06 +0000

    added unit test

--- backend/test/routes/model/modelcard/postFromTemplate.spec.ts
+++ backend/test/routes/model/modelcard/postFromTemplate.spec.ts
@@ -1,7 +1,7 @@
 import { describe, expect, test, vi } from 'vitest'
 
 import audit from '../../../../src/connectors/audit/__mocks__/index.js'
-import { postFromSchemaSchema } from '../../../../src/routes/v2/model/modelcard/postFromSchema.js'
+import { postFromTemplateSchema } from '../../../../src/routes/v2/model/modelcard/postFromTemplate.js'
 import { createFixture, testPost } from '../../../testUtils/routes.js'
 
 vi.mock('../../../../src/connectors/authorisation/index.js')
@@ -14,22 +14,22 @@ vi.mock('../../../../src/services/model.js', async () => {
   const actual = (await vi.importActual('../../../../src/services/model.js')) as object
   return {
     ...actual,
-    createModelCardFromSchema: vi.fn(() => ({ _id: 'test' })),
+    createModelCardFromTemplate: vi.fn(() => ({ _id: 'test' })),
   }
 })
 
-describe('routes > model > postModel > schema', () => {
+describe('routes > model > postModel > template', () => {
   test('200 > ok', async () => {
-    const fixture = createFixture(postFromSchemaSchema)
-    const res = await testPost('/api/v2/model/example/setup/from-schema', fixture)
+    const fixture = createFixture(postFromTemplateSchema)
+    const res = await testPost('/api/v2/model/example/setup/from-template', fixture)
 
     expect(res.statusCode).toBe(200)
     expect(res.body).matchSnapshot()
   })
 
   test('audit > expected call', async () => {
-    const fixture = createFixture(postFromSchemaSchema)
-    const res = await testPost('/api/v2/model/example/setup/from-schema', fixture)
+    const fixture = createFixture(postFromTemplateSchema)
+    const res = await testPost('/api/v2/model/example/setup/from-template', fixture)
 
     expect(res.statusCode).toBe(200)
     expect(audit.onCreateModelCard).toBeCalled()
@@ -37,13 +37,12 @@ describe('routes > model > postModel > schema', () => {
     expect(audit.onCreateModelCard.mock.calls.at(0)?.at(2)).toMatchSnapshot()
   })
 
-  test('400 > no description', async () => {
-    const fixture = createFixture(postFromSchemaSchema) as any
+  test('400 > no model template ID provided', async () => {
+    const fixture = createFixture(postFromTemplateSchema) as any
 
-    // This model does not include a description.
-    delete fixture.body.schemaId
+    delete fixture.body.templateId
 
-    const res = await testPost('/api/v2/model/example/setup/from-schema', fixture)
+    const res = await testPost('/api/v2/model/example/setup/from-template', fixture)
 
     expect(res.statusCode).toEqual(400)
     expect(res.body).matchSnapshot()
