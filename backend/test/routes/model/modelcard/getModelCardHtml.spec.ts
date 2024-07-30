cc6a170282ece5af2c46423ac892718345cea242
ARADDCC012 <110473008+ARADDCC012@users.noreply.github.com>
2024-07-30 13:10:27 +0000

    Fixed getModelCardHtml test

--- backend/test/routes/model/modelcard/getModelCardHtml.spec.ts
+++ backend/test/routes/model/modelcard/getModelCardHtml.spec.ts
@@ -1,6 +1,7 @@
 import { describe, expect, test, vi } from 'vitest'
 
 import audit from '../../../../src/connectors/audit/index.js'
+import { UserInterface } from '../../../../src/models/User.js'
 import { getModelCardHtmlSchema } from '../../../../src/routes/v2/model/modelcard/getModelCardHtml.js'
 import { renderToHtml } from '../../../../src/services/modelCardExport.js'
 import { createFixture, testGet } from '../../../testUtils/routes.js'
@@ -10,17 +11,24 @@ vi.mock('../../../../src/utils/user.js')
 vi.mock('../../../../src/utils/config.js')
 vi.mock('../../../../src/connectors/audit/index.js')
 
+const mockModelCardExportService = vi.hoisted(() => {
+  return {
+    renderToHtml: vi.fn(() => ({ html: 'test', card: 'card' })),
+  }
+})
+vi.mock('../../../../src/services/modelCardExport.js', () => mockModelCardExportService)
+
 describe('routes > model > modelcard > getModelCardHtml', () => {
   test('should return HTML and call audit', async () => {
-    const html = { html: 'test', card: 'card' }
-    vi.mock('../../../src/services/modelCardExport.js', () => ({
-      renderToHtml: vi.fn(() => html),
-    }))
+    const testUser = { dn: 'user' } as UserInterface
+    mockModelCardExportService.renderToHtml.mockResolvedValueOnce({ html: 'test', card: 'card' })
 
     const fixture = createFixture(getModelCardHtmlSchema)
-    await testGet(`/api/v2/model/${fixture.params.modelId}/model-card/${fixture.params.version}/html`)
+    const res = await testGet(`/api/v2/model/${fixture.params.modelId}/model-card/${fixture.params.version}/html`)
 
-    expect(renderToHtml).toHaveBeenCalledWith(undefined, fixture.params.modelId, fixture.params.version)
+    expect(renderToHtml).toHaveBeenCalledWith(testUser, fixture.params.modelId, fixture.params.version)
+    expect(res.statusCode).toBe(200)
+    expect(res.text).toBe('test')
     expect(audit.onViewModelCard).toHaveBeenCalled()
   })
 })
