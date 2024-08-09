f14d682c4a3c8c966bb731e22d5c9db97ab6f87b
araddcc002 <araddcc002@euro.ngc.com>
2024-08-09 15:58:58 +0000

    updated the selection menu for themes

--- frontend/src/settings/display/DisplayTab.tsx
+++ frontend/src/settings/display/DisplayTab.tsx
@@ -1,13 +1,10 @@
-import { Container, Divider, Grid, Stack } from '@mui/material'
-import { useTheme } from '@mui/material/styles'
+import { Container, Grid, Stack } from '@mui/material'
 import Typography from '@mui/material/Typography'
 import { useMemo } from 'react'
-import ThemeCircle from 'src/settings/display/ThemeCircle'
 import ThemeSelectButton from 'src/settings/display/ThemeSelectButton'
 import { themeList } from 'src/theme'
 
 export default function DisplayTab() {
-  const theme = useTheme()
   const themes = useMemo(() => {
     return themeList.map((theme) => (
       <Grid item xs={12} sm={6} key={theme.key}>
@@ -20,13 +17,6 @@ export default function DisplayTab() {
     <Container maxWidth='md'>
       <Stack spacing={2}>
         <Typography fontWeight='bold'>Theme</Typography>
-        <Stack direction='row' spacing={2}>
-          <ThemeCircle colour={theme.palette.primary.main} />
-          <ThemeCircle colour={theme.palette.secondary.main} />
-          <ThemeCircle colour={theme.palette.text.primary} />
-          <ThemeCircle colour={theme.palette.container.main} />
-        </Stack>
-        <Divider />
         <Stack direction='row' spacing={2}>
           {themes}
         </Stack>
