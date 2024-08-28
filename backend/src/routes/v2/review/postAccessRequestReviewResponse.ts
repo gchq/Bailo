58653002620576a91a70191ccac9426856f85522
araddcc002 <araddcc002@euro.ngc.com>
2024-08-28 13:14:19 +0000

    updated review zod validation for access request

--- backend/src/routes/v2/review/postAccessRequestReviewResponse.ts
+++ backend/src/routes/v2/review/postAccessRequestReviewResponse.ts
@@ -6,38 +6,55 @@ import { AuditInfo } from '../../../connectors/audit/Base.js'
 import audit from '../../../connectors/audit/index.js'
 import { Decision, ResponseInterface } from '../../../models/Response.js'
 import { respondToReview } from '../../../services/response.js'
+import { registerPath, reviewInterfaceSchema } from '../../../services/specification.js'
 import { ReviewKind } from '../../../types/enums.js'
 import { getEnumValues } from '../../../utils/enum.js'
 import { parse } from '../../../utils/validate.js'
 
-const nonConditionalFields = z.object({
+const staticProperties = z.object({
+  role: z.string(),
+})
+
+const optionalComment = z.object({
+  comment: z.string().optional(),
+  decision: z.enum(getEnumValues(Decision)).exclude([Decision.RequestChanges]),
+})
+
+const mandatoryComment = z.object({
+  comment: z.string().min(1, 'A comment must be supplied when requesting changes'),
+  decision: z.literal(Decision.RequestChanges),
+})
+
+export const postAccessRequestReviewResponseSchema = z.object({
   params: z.object({
     modelId: z.string(),
     accessRequestId: z.string(),
   }),
-  body: z.object({
-    role: z.string(),
-  }),
+  body: z.discriminatedUnion('decision', [
+    staticProperties.merge(optionalComment),
+    staticProperties.merge(mandatoryComment),
+  ]),
 })
 
-const optionalComment = z.object({
-  body: z.object({
-    comment: z.string().optional(),
-    decision: z.enum(getEnumValues(Decision)).exclude([Decision.RequestChanges]),
-  }),
+registerPath({
+  method: 'post',
+  path: '/api/v2/model/{modelId}/access-request/{acessRequestId}/review',
+  tags: ['accessRequest', 'review'],
+  description: 'Send a review for an access request.',
+  schema: postAccessRequestReviewResponseSchema,
+  responses: {
+    200: {
+      description: 'The created review instance.',
+      content: {
+        'application/json': {
+          schema: z.object({
+            review: reviewInterfaceSchema,
+          }),
+        },
+      },
+    },
+  },
 })
-
-const mandatoryComment = z.object({
-  body: z.object({
-    comment: z.string().min(1, 'A comment must be supplied when requesting changes'),
-    decision: z.literal(Decision.RequestChanges),
-  }),
-})
-
-export const postAccessRequestReviewResponseSchema = z.intersection(
-  nonConditionalFields,
-  z.union([optionalComment, mandatoryComment]),
-)
 
 interface PostAccessRequestReviewResponse {
   response: ResponseInterface
