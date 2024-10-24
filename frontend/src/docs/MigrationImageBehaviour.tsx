55a7d3b6ab3ec40a0508ac3e1d9d4b2e33048cf3
KW86022 <184099420+KW86022@users.noreply.github.com>
2024-10-24 14:44:56 +0000

    Remove legacy next image behaviour from documentation

---
+++ frontend/src/docs/MigrationImageBehaviour.tsx
@@ -0,0 +1,22 @@
+//Script to migrate from next/legacy/image to next/image
+//paddingBottom is controlled by a width to height ratio
+
+import Image from 'next/image'
+
+function ResponsiveImage({ src, width = 1, height = 1, scaling = 50, alt }) {
+  return (
+    <div
+      style={{
+        position: 'relative',
+        width: 'auto',
+        height: 'auto',
+        marginLeft: 'auto',
+        paddingBottom: `max(350px, ${(width / height) * scaling}%)`,
+      }}
+    >
+      <Image className='next-image' src={src} layout='fill' objectFit='contain' alt={alt} />
+    </div>
+  )
+}
+
+export default ResponsiveImage
