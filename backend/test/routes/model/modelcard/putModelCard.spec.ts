52f33ac4077ea339931a762396c3eed3ab4bf218
araddcc002 <araddcc002@euro.ngc.com>
2025-01-02 13:13:19 +0000

    updated unit test for putmodelcard

--- backend/test/routes/model/modelcard/putModelCard.spec.ts
+++ backend/test/routes/model/modelcard/putModelCard.spec.ts
@@ -1,7 +1,8 @@
+import { ObjectId } from 'mongodb'
 import { describe, expect, test, vi } from 'vitest'
 
 import audit from '../../../../src/connectors/audit/__mocks__/index.js'
-import ModelCardRevisionModel, { ModelCardRevisionDoc } from '../../../../src/models/ModelCardRevision.js'
+import ModelCardRevisionModel from '../../../../src/models/ModelCardRevision.js'
 import { putModelCardSchema } from '../../../../src/routes/v2/model/modelcard/putModelCard.js'
 import { createFixture, testPut } from '../../../testUtils/routes.js'
 
@@ -9,16 +10,23 @@ vi.mock('../../../../src/connectors/authorisation/index.js')
 vi.mock('../../../../src/utils/user.js')
 vi.mock('../../../../src/connectors/audit/index.js')
 
-const mockModelService = vi.hoisted(() => {
+const modelServiceMock = vi.hoisted(() => {
   return {
-    updateModelCard: vi.fn(() => new ModelCardRevisionModel({ _id: 'test' }) as ModelCardRevisionDoc),
+    updateModelCard: vi.fn(() => undefined as any),
   }
 })
-vi.mock('../../../../src/services/model.js', () => mockModelService)
+vi.mock('../../../../src/services/model.js', () => modelServiceMock)
 
-describe('routes > model > putModelCardu', () => {
+describe('routes > model > putModelCard', () => {
   test('200 > ok', async () => {
     const fixture = createFixture(putModelCardSchema)
+    modelServiceMock.updateModelCard.mockResolvedValueOnce(
+      new ModelCardRevisionModel({
+        _id: new ObjectId('6776901b879d08e34b599d7e'),
+        id: new ObjectId('6776901b879d08e34b599d7e'),
+        modelId: 'test',
+      }),
+    )
     const res = await testPut(`/api/v2/model/${fixture.params.modelId}/model-cards`, fixture)
     expect(res.statusCode).toBe(200)
     expect(res.body).matchSnapshot()
@@ -26,6 +34,13 @@ describe('routes > model > putModelCardu', () => {
 
   test('audit > expected call', async () => {
     const fixture = createFixture(putModelCardSchema)
+    modelServiceMock.updateModelCard.mockResolvedValueOnce(
+      new ModelCardRevisionModel({
+        _id: new ObjectId('6776901b879d08e34b599d7e'),
+        id: new ObjectId('6776901b879d08e34b599d7e'),
+        modelId: 'test',
+      }),
+    )
     const res = await testPut(`/api/v2/model/${fixture.params.modelId}/model-cards`, fixture)
     expect(res.statusCode).toBe(200)
     expect(audit.onUpdateModelCard).toBeCalled()
