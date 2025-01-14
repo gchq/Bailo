7775793dd084df3603406c5db5fe355b55abc024
PE39806 <185931318+PE39806@users.noreply.github.com>
2025-01-14 10:17:19 +0000

    BAI-1570 add migration script to create deletedAt field in mongo

---
+++ backend/src/migrations/0013_add_missing_deletedat_field_to_mongo.ts
@@ -0,0 +1,29 @@
+import AccessRequestModel from '../models/AccessRequest.js'
+import FileModel from '../models/File.js'
+import ModelModel from '../models/Model.js'
+import ReleaseModel from '../models/Release.js'
+import ResponseModel from '../models/Response.js'
+import ReviewModel from '../models/Review.js'
+import TokenModel from '../models/Token.js'
+import WebhookModel from '../models/Webhook.js'
+
+export async function up() {
+  const modelTypesToUpdate = [
+    AccessRequestModel,
+    FileModel,
+    ModelModel,
+    ReleaseModel,
+    ResponseModel,
+    ReviewModel,
+    TokenModel,
+    WebhookModel,
+  ]
+
+  for (const modelTypeToUpdate of modelTypesToUpdate) {
+    await (modelTypeToUpdate as any).updateManyDeleted({ deletedAt: { $exists: false } }, { $set: { deletedAt: null } })
+  }
+}
+
+export async function down() {
+  /* NOOP */
+}
