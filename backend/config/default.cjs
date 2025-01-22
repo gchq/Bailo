fb574df2e8a53e0a04efa4c1fe925470516adbce
araddcc002 <araddcc002@euro.ngc.com>
2025-01-22 13:14:04 +0000

    fixed an issue with the submission of mirrored models

--- backend/config/default.cjs
+++ backend/config/default.cjs
@@ -191,7 +191,7 @@ module.exports = {
         enabled: false,
       },
       export: {
-        enabled: true,
+        enabled: false,
         disclaimer: '## Example Agreement \n I agree that this model is suitable for exporting',
       },
     },
