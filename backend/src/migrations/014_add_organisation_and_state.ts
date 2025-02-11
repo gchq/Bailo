15b2deee525eb39f2cde311770de5ff2a60a3486
ARADDCC002 <89992537+ARADDCC002@users.noreply.github.com>
2025-02-11 12:38:34 +0000

    Removed commented out section of migration script

--- backend/src/migrations/014_add_organisation_and_state.ts
+++ backend/src/migrations/014_add_organisation_and_state.ts
@@ -3,18 +3,6 @@ import ModelModel from '../models/Model.js'
 export async function up() {
   await ModelModel.updateMany({ organisation: { $exists: false } }, { $set: { organisation: '' } })
   await ModelModel.updateMany({ state: { $exists: false } }, { $set: { state: '' } })
-  //   for (const model of models) {
-  //     const organisation = model.get('organisation')
-  //     if (organisation === undefined) {
-  //       model.set('organisation', '')
-  //       await model.save()
-  //     }
-  //     const state = model.get('state')
-  //     if (state === undefined) {
-  //       model.set('state', '')
-  //       await model.save()
-  //     }
-  //   }
 }
 
 export async function down() {
