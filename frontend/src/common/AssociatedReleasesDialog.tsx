e87741b92149a1074bd542b78574d6edfac71836
IR96334 <175131971+IR96334@users.noreply.github.com>
2025-02-24 12:29:54 +0000

    Added check for diff

--- frontend/src/common/AssociatedReleasesDialog.tsx
+++ frontend/src/common/AssociatedReleasesDialog.tsx
@@ -1,5 +1,6 @@
 import { Dialog, DialogContent, DialogTitle } from '@mui/material'
 import { useGetReleasesForModelId } from 'actions/release'
+import { useMemo } from 'react'
 import { FileInterface, isFileInterface, ReleaseInterface } from 'types/types'
 
 type AssociatedReleasesDialogProps = {
@@ -12,14 +13,15 @@ type AssociatedReleasesDialogProps = {
 export default function AssociatedReleasesDialog({ modelId, file, open, onClose }: AssociatedReleasesDialogProps) {
   const { releases } = useGetReleasesForModelId(modelId)
 
-  const associatedReleases: Array<ReleaseInterface> = releases.filter(
-    (release) => isFileInterface(file) && release.fileIds.includes(file._id),
+  const associatedReleases: Array<ReleaseInterface> = useMemo(
+    () => releases.filter((release) => isFileInterface(file) && release.fileIds.includes(file._id)),
+    [file, releases],
   )
 
   return (
     <Dialog open={open} onClose={onClose} fullWidth maxWidth='md'>
       <DialogTitle>Releases associated to {file.name}</DialogTitle>
-      <DialogContent>{associatedReleases[0].semver}</DialogContent>
+      <DialogContent>{associatedReleases ? associatedReleases[0].semver : <></>}</DialogContent>
     </Dialog>
   )
 }
