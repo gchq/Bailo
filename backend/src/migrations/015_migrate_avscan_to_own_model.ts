9baadbe864f512d03c3b0390241350382a12b745
PE39806 <185931318+PE39806@users.noreply.github.com>
2025-03-13 08:26:08 +0000

    change 015 migration script strategy

--- backend/src/migrations/015_migrate_avscan_to_own_model.ts
+++ backend/src/migrations/015_migrate_avscan_to_own_model.ts
@@ -2,26 +2,15 @@ import FileModel from '../models/File.js'
 import ScanModel, { ArtefactKind } from '../models/Scan.js'
 
 export async function up() {
-  // toolName was originally not a required field so may not exist
-  const filesMissingToolName = await FileModel.find({
-    avScan: { $exists: true },
-    'avScan.toolName': { $exists: false },
-  })
-  for (const file of filesMissingToolName) {
-    if (file.get('avScan') !== undefined) {
-      for (const avResult of file.get('avScan')) {
-        if (!('toolName' in avResult)) {
-          avResult.toolName = 'Unknown Scanner'
-        }
-      }
-      await file.save()
-    }
-  }
   // convert avScan from being stored in File to a new Scan Document
   const files = await FileModel.find({})
   for (const file of files) {
     if (file.get('avScan') !== undefined) {
       for (const avResult of file.get('avScan')) {
+        // toolName was originally not a required field so may not exist
+        if (!Object.prototype.hasOwnProperty.call(avResult, 'toolName')) {
+          avResult.toolName = 'Unknown Scanner'
+        }
         // create new Scan Document
         const newScan = new ScanModel({
           artefactKind: ArtefactKind.File,
@@ -35,7 +24,7 @@ export async function up() {
     }
   }
   // remove all old avScan fields
-  await FileModel.updateMany({ avScan: { $exists: true } }, { $unset: { avScan: 1 } })
+  await FileModel.updateMany({ avScan: { $exists: true } }, { $unset: { avScan: 1 } }, { strict: false })
 }
 
 export async function down() {
