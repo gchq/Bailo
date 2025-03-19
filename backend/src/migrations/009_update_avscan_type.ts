398bdd0e7dd557a4365d4d61ab51169fe346b275
PE39806 <185931318+PE39806@users.noreply.github.com>
2025-03-19 12:49:45 +0000

    change migration 009 to use hydrated documents and `for ... of` loop

--- backend/src/migrations/009_update_avscan_type.ts
+++ backend/src/migrations/009_update_avscan_type.ts
@@ -1,16 +1,15 @@
 import FileModel from '../models/File.js'
 
 export async function up() {
-  const results = await FileModel.find({ avScan: { $type: 'object' } }, null, { strict: false, lean: true })
-  results.forEach(async (result) => {
-    // because of `forEach`, we cannot use `.get(<property>)`
-    const avScan = result['avScan']
+  const results = await FileModel.find({ avScan: { $type: 'object' } }, null, { strict: false })
+  for (const result of results) {
+    const avScan = result.get('avScan')
     if (avScan !== undefined) {
       if (!Array.isArray(avScan)) {
         await FileModel.findOneAndUpdate({ _id: result._id }, { $set: { avScan: [avScan] } })
       }
     }
-  })
+  }
 }
 
 export async function down() {
