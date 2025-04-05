dba8eb257d764bb1f09b2bded1ff83eba07cf799
GCHQDeveloper36 <73173408+GCHQDeveloper36@users.noreply.github.com>
2025-04-05 14:08:01 +0000

    Add basic system status endpoint and initial federation configuration

---
+++ backend/src/connectors/peer/index.ts
@@ -0,0 +1,80 @@
+import { RemoteFederationConfig } from '../../types/types.js'
+import config from '../../utils/config.js'
+import { ConfigurationError, InternalError } from '../../utils/error.js'
+import { BailoPeerConnector } from './bailo.js'
+import { BasePeerConnector } from './base.js'
+
+export const PeerKind = {
+  Bailo: 'bailo',
+} as const
+export type PeerKindKeys = (typeof PeerKind)[keyof typeof PeerKind]
+
+let peersConfigured = false
+const peerIds = new Array<string>()
+const peers = new Map<string, BasePeerConnector>()
+
+function validate(cfg: RemoteFederationConfig): void {
+  const keys = new Array<string>()
+
+  if (!cfg.kind) {
+    keys.push('kind')
+  }
+  if (!cfg.baseUrl) {
+    keys.push('baseUrl')
+  }
+  if (!cfg.state) {
+    keys.push('state')
+  }
+  if (keys.length > 0) {
+    throw ConfigurationError(`Missing configuration for peer`, {
+      missingConfig: keys,
+    })
+  }
+}
+
+function setupPeers(): void {
+  if (peersConfigured) {
+    // Early return
+    return
+  }
+  for (const [id, cfg] of Object.entries(config.federation.peers)) {
+    peers.set(id, configurePeerConnector(id, cfg))
+    peerIds.push(id)
+  }
+  peersConfigured = true
+}
+
+function configurePeerConnector(id: string, cfg: RemoteFederationConfig) {
+  validate(cfg)
+  switch (cfg.kind) {
+    case PeerKind.Bailo:
+      return new BailoPeerConnector(id, cfg)
+    default:
+      throw ConfigurationError(`'${cfg.kind}' not supported.`, {
+        validKinds: Object.values(PeerKind),
+      })
+  }
+}
+
+export function getPeerIds(): Array<string> {
+  if (!peersConfigured) {
+    setupPeers()
+  }
+  return peerIds
+}
+
+export function getPeerConnectors(): Map<string, BasePeerConnector> {
+  if (!peersConfigured) {
+    setupPeers()
+  }
+  return peers
+}
+
+export default function getPeerConnector(id: string): BasePeerConnector {
+  if (!peersConfigured) {
+    setupPeers()
+  }
+  const peer = peers.get(id)
+  if (!peer) throw InternalError('Peer not configured')
+  return peer
+}
