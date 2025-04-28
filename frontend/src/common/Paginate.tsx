f0cfc12e29f275335949a97d468627f902e7aaaf
ARADDCC002 <89992537+ARADDCC002@users.noreply.github.com>
2025-04-28 16:22:25 +0000

    improvements and customisations added to new paginate component

--- frontend/src/common/Paginate.tsx
+++ frontend/src/common/Paginate.tsx
@@ -1,9 +1,8 @@
 import { CalendarMonth, Check, ExpandLess, ExpandMore, Sort, SortByAlpha } from '@mui/icons-material'
 import { Button, Divider, Grid2, ListItemIcon, ListItemText, Menu, MenuItem, Pagination, Stack } from '@mui/material'
 import { isArray } from 'lodash-es'
-import { ReactElement, useCallback, useMemo, useState, MouseEvent, ReactNode, Children } from 'react'
+import { MouseEvent, ReactElement, useCallback, useMemo, useState } from 'react'
 import EmptyBlob from 'src/common/EmptyBlob'
-import { SortingDirection } from 'src/entry/model/Files'
 import { FileInterface } from 'types/types'
 import { sortByCreatedAtDescending } from 'utils/arrayUtils'
 
@@ -11,19 +10,38 @@ interface PaginateProps {
   list: any[]
   pageSize?: number
   emptyListText?: string
+  sortingProperties?: SortingProperty[]
   children: ({ data, index }: { data: any; index: any }) => ReactElement
 }
 
+export const SortingDirection = {
+  ASC: 'Ascending',
+  DESC: 'Descending',
+} as const
+
 export type SortingDirectionKeys = (typeof SortingDirection)[keyof typeof SortingDirection]
 
-export default function Paginate({ list, pageSize = 10, emptyListText = 'No items found', children }: PaginateProps) {
+export interface SortingProperty {
+  value: string
+  title: string
+  iconKind: 'text' | 'date'
+}
+
+export default function Paginate({
+  list,
+  pageSize = 10,
+  emptyListText = 'No items found',
+  children,
+  sortingProperties = [{ value: 'name', title: 'Name', iconKind: 'text' }],
+}: PaginateProps) {
   const [page, setPage] = useState(1)
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
   const menuOpen = Boolean(anchorEl)
   const [orderByValue, setOrderByValue] = useState('createdAt')
   const [orderByButtonTitle, setOrderByButtonTitle] = useState('Order by')
   const [ascOrDesc, setAscOrDesc] = useState<SortingDirectionKeys>(SortingDirection.DESC)
-  const pageCount = useMemo(() => (isArray(list) ? Math.ceil(list.length / pageSize) : 10), [list])
+
+  const pageCount = useMemo(() => (isArray(list) ? Math.ceil(list.length / pageSize) : 10), [list, pageSize])
 
   const handlePageOnChange = (_event: React.ChangeEvent<unknown>, value: number) => {
     setPage(value)
@@ -58,33 +76,47 @@ export default function Paginate({ list, pageSize = 10, emptyListText = 'No item
     setAnchorEl(null)
   }
 
-  const orderByMenuListItems = (value: string, title: string) => (
-    <MenuItem
-      onClick={() => {
-        setOrderByValue(value)
-        setOrderByButtonTitle(title)
-      }}
-      sx={{ px: 2.5 }}
-      selected={checkMenuOption(value)}
-    >
-      <Grid2 container sx={{ minWidth: '200px' }}>
-        <Grid2 size={2}>
-          {checkMenuOption(value) ? (
-            <Check sx={{ width: '100%' }} color='primary' />
-          ) : (
-            <Check sx={{ width: '100%' }} color='primary' opacity={0} />
-          )}
-        </Grid2>
-        <Grid2 size={2}>
-          <ListItemIcon>
-            {value === 'name' ? <SortByAlpha color='primary' /> : <CalendarMonth color='primary' />}
-          </ListItemIcon>
-        </Grid2>
-        <Grid2 size={8}>
-          <ListItemText>{title}</ListItemText>
-        </Grid2>
-      </Grid2>
-    </MenuItem>
+  const displaySortingKindIcon = (sortingKind: SortingProperty['iconKind']) => {
+    switch (sortingKind) {
+      case 'text':
+        return <SortByAlpha color='primary' />
+      case 'date':
+        return <CalendarMonth color='primary' />
+      default:
+        ;<SortByAlpha color='primary' />
+    }
+  }
+
+  const orderByMenuListItems = useCallback(
+    (sortingProperty: SortingProperty) => {
+      return (
+        <MenuItem
+          onClick={() => {
+            setOrderByValue(sortingProperty.value)
+            setOrderByButtonTitle(sortingProperty.title)
+          }}
+          sx={{ px: 2.5 }}
+          selected={checkMenuOption(sortingProperty.value)}
+        >
+          <Grid2 container sx={{ minWidth: '200px' }}>
+            <Grid2 size={2}>
+              {checkMenuOption(sortingProperty.value) ? (
+                <Check sx={{ width: '100%' }} color='primary' />
+              ) : (
+                <Check sx={{ width: '100%' }} color='primary' opacity={0} />
+              )}
+            </Grid2>
+            <Grid2 size={2}>
+              <ListItemIcon>{displaySortingKindIcon(sortingProperty.iconKind)}</ListItemIcon>
+            </Grid2>
+            <Grid2 size={8}>
+              <ListItemText>{sortingProperty.title}</ListItemText>
+            </Grid2>
+          </Grid2>
+        </MenuItem>
+      )
+    },
+    [checkMenuOption],
   )
 
   const ascOrDescMenuListItems = (direction: SortingDirectionKeys) => (
@@ -123,22 +155,24 @@ export default function Paginate({ list, pageSize = 10, emptyListText = 'No item
     return value === ascOrDesc
   }
 
-  const sortedList = useMemo(() => [...list].sort(sortByCreatedAtDescending), [list])
+  const sortingPropertyMenuItems = useMemo(() => {
+    return sortingProperties.map((sortingProperty) => {
+      return orderByMenuListItems(sortingProperty)
+    })
+  }, [sortingProperties, orderByMenuListItems])
 
-  // TODO
-
-  // Make it so that this list is sorted and sliced based on the UI ordering
-  // Remove the sorting code from Files.tsx
+  const defaultSortedList = useMemo(() => [...list].sort(sortByCreatedAtDescending), [list])
 
   const listDisplay = useMemo(() => {
-    console.log(children({ data: list, index: 0 }))
+    const sortedList = defaultSortedList.sort(sortFilesByValue).slice((page - 1) * pageSize, page * pageSize)
     if (isArray(list)) {
-      return sortedList
-        .sort(sortFilesByValue)
-        .slice((page - 1) * pageSize, page * pageSize)
-        .map((_item, index) => <div>{children({ data: list, index })}</div>)
+      return sortedList.map((item, index) => (
+        <div key={item} style={{ width: '100%' }}>
+          {children({ data: sortedList, index })}
+        </div>
+      ))
     }
-  }, [pageSize, page, list, sortFilesByValue])
+  }, [pageSize, page, list, sortFilesByValue, children, defaultSortedList])
 
   if (list.length === 0) {
     return <EmptyBlob text={emptyListText} />
@@ -169,9 +203,7 @@ export default function Paginate({ list, pageSize = 10, emptyListText = 'No item
           onClose={handleMenuButtonClose}
           sx={{ minWidth: '200px' }}
         >
-          {orderByMenuListItems('name', 'Name')}
-          {orderByMenuListItems('createdAt', 'Date uploaded')}
-          {orderByMenuListItems('updatedAt', 'Date updated')}
+          {sortingPropertyMenuItems}
           <Divider />
           {ascOrDescMenuListItems(SortingDirection.ASC)}
           {ascOrDescMenuListItems(SortingDirection.DESC)}
--- frontend/src/entry/model/Files.tsx
+++ frontend/src/entry/model/Files.tsx
@@ -1,48 +1,21 @@
-import { CalendarMonth, Check, ExpandLess, ExpandMore, Sort, SortByAlpha } from '@mui/icons-material'
 import { LoadingButton } from '@mui/lab'
-import {
-  Box,
-  Button,
-  Card,
-  Container,
-  Divider,
-  LinearProgress,
-  ListItemIcon,
-  ListItemText,
-  Menu,
-  MenuItem,
-  Pagination,
-  Stack,
-  Typography,
-} from '@mui/material'
-import Grid2 from '@mui/material/Grid2'
+import { Box, Button, Card, Container, LinearProgress, Stack, Typography } from '@mui/material'
 import { styled } from '@mui/material/styles'
 import { postFileForModelId } from 'actions/file'
 import { useGetModelFiles } from 'actions/model'
 import { AxiosProgressEvent } from 'axios'
-import { ChangeEvent, MouseEvent, useCallback, useMemo, useState } from 'react'
-import EmptyBlob from 'src/common/EmptyBlob'
+import { ChangeEvent, useCallback, useMemo, useState } from 'react'
 import FileUploadProgressDisplay, { FailedFileUpload, FileUploadProgress } from 'src/common/FileUploadProgressDisplay'
 import Loading from 'src/common/Loading'
 import Paginate from 'src/common/Paginate'
 import Restricted from 'src/common/Restricted'
 import FileDownload from 'src/entry/model/files/FileDownload'
 import MessageAlert from 'src/MessageAlert'
-import { EntryInterface, FileInterface } from 'types/types'
-import { sortByCreatedAtDescending } from 'utils/arrayUtils'
+import { EntryInterface } from 'types/types'
 
 type FilesProps = {
   model: EntryInterface
 }
-
-export const SortingDirection = {
-  ASC: 'Ascending',
-  DESC: 'Descending',
-} as const
-
-export type SortingDirectionKeys = (typeof SortingDirection)[keyof typeof SortingDirection]
-
-export const menuItemDetails = {}
 
 const Input = styled('input')({
   display: 'none',
@@ -50,129 +23,11 @@ const Input = styled('input')({
 
 export default function Files({ model }: FilesProps) {
   const { entryFiles, isEntryFilesLoading, isEntryFilesError, mutateEntryFiles } = useGetModelFiles(model.id)
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
-  const menuOpen = Boolean(anchorEl)
-  const [orderByValue, setOrderByValue] = useState('createdAt')
-  const [orderByButtonTitle, setOrderByButtonTitle] = useState('Order by')
-  const [ascOrDesc, setAscOrDesc] = useState<SortingDirectionKeys>(SortingDirection.DESC)
   const [currentFileUploadProgress, setCurrentFileUploadProgress] = useState<FileUploadProgress | undefined>(undefined)
   const [uploadedFiles, setUploadedFiles] = useState<string[]>([])
   const [totalFilesToUpload, setTotalFilesToUpload] = useState(0)
   const [isFilesUploading, setIsFilesUploading] = useState(false)
   const [failedFileUploads, setFailedFileUploads] = useState<FailedFileUpload[]>([])
-
-  const sortFilesByValue = useMemo(
-    () => (a: FileInterface, b: FileInterface) => {
-      if (ascOrDesc === SortingDirection.DESC) {
-        return a[orderByValue] < b[orderByValue] ? -1 : 1
-      }
-      return a[orderByValue] > b[orderByValue] ? -1 : 1
-    },
-    [ascOrDesc, orderByValue],
-  )
-
-  const checkMenuOption = useCallback(
-    (menuOption: string) => {
-      if (menuOption === orderByValue) {
-        return true
-      } else {
-        return false
-      }
-    },
-    [orderByValue],
-  )
-
-  function handleMenuButtonClick(event: MouseEvent<HTMLButtonElement>) {
-    setAnchorEl(event.currentTarget)
-  }
-
-  const handleMenuButtonClose = () => {
-    setAnchorEl(null)
-  }
-
-  const orderByMenuListItems = (value: string, title: string) => (
-    <MenuItem
-      onClick={() => {
-        setOrderByValue(value)
-        setOrderByButtonTitle(title)
-      }}
-      sx={{ px: 2.5 }}
-      selected={checkMenuOption(value)}
-    >
-      <Grid2 container sx={{ minWidth: '200px' }}>
-        <Grid2 size={2}>
-          {checkMenuOption(value) ? (
-            <Check sx={{ width: '100%' }} color='primary' />
-          ) : (
-            <Check sx={{ width: '100%' }} color='primary' opacity={0} />
-          )}
-        </Grid2>
-        <Grid2 size={2}>
-          <ListItemIcon>
-            {value === 'name' ? <SortByAlpha color='primary' /> : <CalendarMonth color='primary' />}
-          </ListItemIcon>
-        </Grid2>
-        <Grid2 size={8}>
-          <ListItemText>{title}</ListItemText>
-        </Grid2>
-      </Grid2>
-    </MenuItem>
-  )
-
-  const ascOrDescMenuListItems = (direction: SortingDirectionKeys) => (
-    <MenuItem
-      onClick={() => {
-        setAscOrDesc(direction)
-      }}
-      sx={{ px: 2.5 }}
-      selected={checkAscOrDesc(direction)}
-    >
-      <Grid2 container sx={{ minWidth: '200px' }}>
-        <Grid2 size={2}>
-          {checkAscOrDesc(direction) ? (
-            <Check sx={{ width: '100%' }} color='primary' />
-          ) : (
-            <Check sx={{ width: '100%' }} color='primary' opacity={0} />
-          )}
-        </Grid2>
-        <Grid2 size={2}>
-          <ListItemIcon>
-            {direction === SortingDirection.ASC ? (
-              <Sort color='primary' />
-            ) : (
-              <Sort sx={{ transform: 'scaleY(-1)' }} color='primary' />
-            )}
-          </ListItemIcon>
-        </Grid2>
-        <Grid2 size={8}>
-          <ListItemText>{direction}</ListItemText>
-        </Grid2>
-      </Grid2>
-    </MenuItem>
-  )
-
-  const checkAscOrDesc = (value: string) => {
-    return value === ascOrDesc
-  }
-
-  const sortedEntryFiles = useMemo(() => [...entryFiles].sort(sortByCreatedAtDescending), [entryFiles])
-
-  const entryFilesList = useMemo(
-    () =>
-      sortedEntryFiles.sort(sortFilesByValue).map((file) => (
-        <Card key={file._id} sx={{ width: '100%' }}>
-          <Stack spacing={1} p={2}>
-            <FileDownload
-              showMenuItems={{ associatedReleases: true, deleteFile: true, rescanFile: true }}
-              file={file}
-              modelId={model.id}
-              mutator={mutateEntryFiles}
-            />
-          </Stack>
-        </Card>
-      )),
-    [entryFiles.length, sortedEntryFiles, sortFilesByValue, model.name, model.id, mutateEntryFiles],
-  )
 
   const EntryListItem = ({ data, index }) => (
     <Card key={data[index]._id} sx={{ width: '100%' }}>
@@ -257,7 +112,7 @@ export default function Files({ model }: FilesProps) {
             that are no longer needed, and also manually retrigger anti-virus scanning (if anti-virus scanning is
             enabled).
           </Typography>
-          <Stack width='100%' direction='row' justifyContent='space-between' sx={{ px: 0.5 }}>
+          <Stack width='100%' direction='row' justifyContent='flex-end' sx={{ px: 0.5 }}>
             <Restricted action='createRelease' fallback={<Button disabled>Add new files</Button>}>
               <>
                 <label htmlFor='add-files-button'>
@@ -274,35 +129,7 @@ export default function Files({ model }: FilesProps) {
                 />
               </>
             </Restricted>
-            <Button
-              onClick={handleMenuButtonClick}
-              endIcon={anchorEl ? <ExpandLess /> : <ExpandMore />}
-              sx={{ width: '170px' }}
-            >
-              <Stack sx={{ minWidth: '150px' }} direction='row' spacing={2} justifyContent='space-evenly'>
-                {checkAscOrDesc(SortingDirection.ASC) ? (
-                  <Sort color='primary' />
-                ) : (
-                  <Sort sx={{ transform: 'scaleY(-1)' }} color='primary' />
-                )}
-                {orderByButtonTitle}
-              </Stack>
-            </Button>
           </Stack>
-          <Menu
-            open={menuOpen}
-            slotProps={{ list: { dense: true } }}
-            anchorEl={anchorEl}
-            onClose={handleMenuButtonClose}
-            sx={{ minWidth: '200px' }}
-          >
-            {orderByMenuListItems('name', 'Name')}
-            {orderByMenuListItems('createdAt', 'Date uploaded')}
-            {orderByMenuListItems('updatedAt', 'Date updated')}
-            <Divider />
-            {ascOrDescMenuListItems(SortingDirection.ASC)}
-            {ascOrDescMenuListItems(SortingDirection.DESC)}
-          </Menu>
           {currentFileUploadProgress && (
             <>
               <LinearProgress
@@ -317,7 +144,15 @@ export default function Files({ model }: FilesProps) {
             </>
           )}
           {failedFileList}
-          <Paginate list={entryFiles} emptyListText={`No files found for model ${model.name}`}>
+          <Paginate
+            list={entryFiles}
+            emptyListText={`No files found for model ${model.name}`}
+            sortingProperties={[
+              { value: 'name', title: 'Name', iconKind: 'text' },
+              { value: 'createdAt', title: 'Date uploaded', iconKind: 'date' },
+              { value: 'updatedAt', title: 'Date updated', iconKind: 'date' },
+            ]}
+          >
             {EntryListItem}
           </Paginate>
         </Stack>
