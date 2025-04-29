c71a8656499d04abcee3c5bf0b72fde40635863b
PE39806 <185931318+PE39806@users.noreply.github.com>
2025-04-29 13:44:33 +0000

    Upload helper docker registry export script

---
+++ backend/src/scripts/streamDockerRegistryToS3.ts
@@ -0,0 +1,30 @@
+import { ensureBucketExists } from '../clients/s3.js'
+import { exportCompressedRegistryImage } from '../services/exportRegistry.js'
+import log from '../services/log.js'
+import config from '../utils/config.js'
+import { connectToMongoose, disconnectFromMongoose } from '../utils/database.js'
+
+async function script() {
+  // process args
+  const args = process.argv.slice(2)[0].split(',')
+  if (args.length != 3) {
+    log.error('Please use format "npm run script -- streamDockerRegistryToS3 <model-id> <image-name> <image-tag>"')
+    return
+  }
+  const imageModel = args[0]
+  const imageName = args[1]
+  const imageTag = args[2]
+  log.info({ imageModel, imageName, imageTag })
+
+  // setup
+  await connectToMongoose()
+  ensureBucketExists(config.modelMirror.export.bucket)
+
+  // main functionality
+  await exportCompressedRegistryImage(imageModel, imageName, imageTag, {})
+
+  // cleanup
+  setTimeout(disconnectFromMongoose, 50)
+}
+
+script()
