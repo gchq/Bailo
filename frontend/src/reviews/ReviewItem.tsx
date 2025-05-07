4dc8d7eff823771d0dd5d960411f1a13d90b2865
IR96334 <175131971+IR96334@users.noreply.github.com>
2025-05-07 15:24:53 +0000

    Fixed reviewitem overflow

--- frontend/src/reviews/ReviewItem.tsx
+++ frontend/src/reviews/ReviewItem.tsx
@@ -1,4 +1,4 @@
-import { ListItem, ListItemButton, Stack, Typography } from '@mui/material'
+import { Divider, ListItem, ListItemButton, Stack, Typography } from '@mui/material'
 import { useGetResponses } from 'actions/response'
 import { useGetCurrentUser } from 'actions/user'
 import { useRouter } from 'next/router'
@@ -50,34 +50,16 @@ export default function ReviewItem({ review }: ReviewItemProps) {
       <ListItem disablePadding>
         <ListItemButton onClick={handleListItemClick} aria-label={`Review model ${review.model} ${review.semver}`}>
           <Stack>
-            <Stack
-              spacing={1}
-              direction='column'
-              justifyContent='flex-start'
-              alignItems='flex-start'
-              overflow='hidden'
-              sx={{ maxWidth: '100vw', paddingRight: '200px' }}
-            >
-              <Typography
-                overflow='hidden'
-                textOverflow='ellipsis'
-                color='primary'
-                variant='h6'
-                component='h2'
-                fontWeight='bold'
-              >
+            <Stack spacing={1} direction='column' justifyContent='flex-start' divider={<Divider flexItem />}>
+              <Typography sx={{ wordBreak: 'break-all' }} color='primary' variant='h6' component='h2' fontWeight='bold'>
                 {review.model.name}
               </Typography>
               {review.accessRequestId && (
-                <Typography overflow='hidden' textOverflow='ellipsis'>
+                <Typography sx={{ wordBreak: 'break-all' }}>
                   {toTitleCase(review.accessRequestId.substring(0, review.accessRequestId.lastIndexOf('-')))}
                 </Typography>
               )}
-              {review.semver && (
-                <Typography overflow='hidden' textOverflow='ellipsis'>
-                  {review.semver}
-                </Typography>
-              )}
+              {review.semver && <Typography sx={{ wordBreak: 'break-all' }}>{review.semver}</Typography>}
             </Stack>
             <Stack spacing={1} direction='row' justifyContent='flex-start' alignItems='center'>
               <Typography variant='caption'>{`Created ${timeDifference(
