276e45fb4a5dc2ce4be0235332ef5d72a42404f2
ARADDCC002 <89992537+ARADDCC002@users.noreply.github.com>
2025-05-14 14:19:20 +0000

    added organisation search filter

--- frontend/src/marketplace/EntryListRow.tsx
+++ frontend/src/marketplace/EntryListRow.tsx
@@ -1,3 +1,4 @@
+import { CorporateFare } from '@mui/icons-material'
 import { Box, Stack, Typography } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
 import { EntrySearchResult } from 'actions/model'
@@ -8,12 +9,22 @@ import Link from 'src/Link'
 interface EntryListRowProps {
   selectedChips: string[]
   onSelectedChipsChange: (chips: string[]) => void
+  selectedOrganisations: string[]
+  onSelectedOrganisationsChange: (organisations: string[]) => void
   data: EntrySearchResult[]
   index: number
   style: CSSProperties
 }
 
-export default function EntryListRow({ selectedChips, onSelectedChipsChange, data, index, style }: EntryListRowProps) {
+export default function EntryListRow({
+  selectedChips,
+  onSelectedChipsChange,
+  selectedOrganisations,
+  onSelectedOrganisationsChange,
+  data,
+  index,
+  style,
+}: EntryListRowProps) {
   const theme = useTheme()
   const entry = data[index]
 
@@ -32,32 +43,44 @@ export default function EntryListRow({ selectedChips, onSelectedChipsChange, dat
       }}
       key={entry.id}
     >
-      <Stack>
-        <Stack direction='row'>
-          <Link
-            sx={{ textDecoration: 'none', whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden' }}
-            href={`${entry.kind}/${entry.id}`}
-          >
-            <Typography
-              variant='h5'
-              sx={{
-                fontWeight: '500',
-                textDecoration: 'none',
-                color: theme.palette.primary.main,
-                textOverflow: 'ellipsis',
-                overflow: 'hidden',
-              }}
-            >
-              {entry.name}
-            </Typography>
-          </Link>
-        </Stack>
-        <Typography
-          variant='body1'
-          sx={{ marginBottom: 2, whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden' }}
+      <Stack spacing={1}>
+        <Link
+          sx={{ textDecoration: 'none', whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden' }}
+          href={`${entry.kind}/${entry.id}`}
         >
+          <Typography
+            variant='h5'
+            sx={{
+              fontWeight: '500',
+              textDecoration: 'none',
+              color: theme.palette.primary.main,
+              textOverflow: 'ellipsis',
+              overflow: 'hidden',
+            }}
+          >
+            {entry.name}
+          </Typography>
+        </Link>
+        <Typography variant='body1' sx={{ whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden' }}>
           {entry.description}
         </Typography>
+        {entry.organisation && (
+          <div>
+            <ChipSelector
+              chipTooltipTitle={'Filter by organisation'}
+              options={[entry.organisation]}
+              expandThreshold={10}
+              variant='outlined'
+              multiple
+              selectedChips={selectedOrganisations}
+              onChange={onSelectedOrganisationsChange}
+              size='small'
+              ariaLabel='add tag to search filter'
+              icon={<CorporateFare />}
+              style={{ padding: 1 }}
+            />
+          </div>
+        )}
         <div>
           <ChipSelector
             chipTooltipTitle={'Filter by tag'}
