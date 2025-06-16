b0ae50bab598e3b76d5bce30a07c1a9d3eac928b
JR40159 <126243293+JR40159@users.noreply.github.com>
2025-06-16 15:40:21 +0000

    Use single query to remove bucket property

--- backend/src/migrations/018_remove_bucket_property_from_files.ts
+++ backend/src/migrations/018_remove_bucket_property_from_files.ts
@@ -1,11 +1,5 @@
 import FileModel from '../models/File.js'
 
 export async function up() {
-  const files = await FileModel.find({})
-  for (const file of files) {
-    if (file.get('bucket') !== undefined) {
-      file.set('bucket', undefined, { strict: false })
-      await file.save()
-    }
-  }
+  await FileModel.updateMany({ tags: { $exists: true } }, { $unset: { tags: 1 } }, { strict: false })
 }
