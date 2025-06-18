596804c8556bfbe8122f49b845977775709960d2
JR40159 <126243293+JR40159@users.noreply.github.com>
2025-06-18 15:24:06 +0000

    Unset bucket property

--- backend/src/migrations/018_remove_bucket_property_from_files.ts
+++ backend/src/migrations/018_remove_bucket_property_from_files.ts
@@ -1,5 +1,5 @@
 import FileModel from '../models/File.js'
 
 export async function up() {
-  await FileModel.updateMany({ tags: { $exists: true } }, { $unset: { tags: 1 } }, { strict: false })
+  await FileModel.updateMany({ bucket: { $exists: true } }, { $unset: { bucket: 1 } }, { strict: false })
 }
