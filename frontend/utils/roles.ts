06ccc5fe27fcead2df26047d3cfcb7534f0c2f6d
IR96334 <175131971+IR96334@users.noreply.github.com>
2025-07-16 16:27:55 +0000

    Reverting fix 1

--- frontend/utils/roles.ts
+++ frontend/utils/roles.ts
@@ -5,7 +5,7 @@ export function getRoleDisplayName(roleId: string, entryRoles: SystemRole[]) {
   if (role) {
     return role.name
   }
-  return 'Unknown Role'
+  return roleId
 }
 
 export const hasRole = (userRoles: string[], validRoles: string[]) => {
