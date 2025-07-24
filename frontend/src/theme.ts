61752f3a0ad64500a842a81dc33ce2cc26435372
IR96334 <175131971+IR96334@users.noreply.github.com>
2025-07-24 09:28:58 +0000

    Removed incorrect change

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -104,7 +104,7 @@ export const lightTheme = createTheme({
       light: '#e5f6fd',
     },
     warning: {
-      main: '#C55302',
+      main: '#ed6c02',
       light: '#ffd08b',
     },
     markdownBorder: {
