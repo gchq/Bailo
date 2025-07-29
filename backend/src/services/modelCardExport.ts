6a7152bfa96d0afdf5de5198dcd777fcb44d0a2e
JRB66955 <214850388+JRB66955@users.noreply.github.com>
2025-07-29 11:16:08 +0000

    move await out of for...of loop and into getResponsesByParentIds function, as originally intended

--- backend/src/services/modelCardExport.ts
+++ backend/src/services/modelCardExport.ts
@@ -111,9 +111,9 @@ async function createReleaseReviewExports(user: UserInterface, modelId: string) 
   const modelReviews = await ReviewModel.find({ modelId: modelId, kind: 'release', deleted: false })
 
   for (const modelReview of modelReviews) {
-    const modelResponses = getResponsesByParentIds(user, [modelReview._id as string])
+    const modelResponses = await getResponsesByParentIds(user, [modelReview._id as string])
 
-    for (const modelResponse of await modelResponses) {
+    for (const modelResponse of modelResponses) {
       const reviewExport: ReviewExport = {
         semver: modelReview.semver,
         collaborator: modelResponse.entity,
