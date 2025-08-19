f5959fc38a4f36b1266f130f574c06ed39e98dca
ARADDCC002 <89992537+ARADDCC002@users.noreply.github.com>
2025-08-19 10:34:37 +0000

    wip

---
+++ frontend/pages/schemas/migration.tsx
@@ -0,0 +1,82 @@
+import { Container, MenuItem, Select, SelectChangeEvent, Stack } from '@mui/material'
+import { useGetSchemas } from 'actions/schema'
+import { useMemo, useState } from 'react'
+import Loading from 'src/common/Loading'
+import MultipleErrorWrapper from 'src/errors/MultipleErrorWrapper'
+import SchemaMigrator from 'src/schemas/SchemaMigrator'
+
+export default function SchemaMigrationSelector() {
+  const { schemas, isSchemasLoading, isSchemasError } = useGetSchemas()
+
+  const [sourceSchema, setSourceSchema] = useState<string>('')
+  const [targetSchema, setTargetSchema] = useState<string>('')
+
+  const schemaOptions = useMemo(() => {
+    if (schemas) {
+      return schemas.map((filteredSchema) => (
+        <MenuItem
+          key={filteredSchema.id}
+          value={filteredSchema.id}
+          disabled={filteredSchema.id === sourceSchema || filteredSchema.id === targetSchema}
+        >
+          {filteredSchema.name}
+        </MenuItem>
+      ))
+    } else {
+      return (
+        <MenuItem>
+          <em>No schemas found</em>
+        </MenuItem>
+      )
+    }
+  }, [schemas, sourceSchema, targetSchema])
+
+  if (isSchemasLoading) {
+    return <Loading />
+  }
+
+  const handleSourceSchemaChange = (event: SelectChangeEvent) => {
+    const schemaFromInput = schemas.find((schema) => schema.id === event.target.value)
+    console.log(schemaFromInput)
+    setSourceSchema(schemaFromInput ? schemaFromInput.id : 'test')
+  }
+
+  const handleTargetSchemaChange = (event: SelectChangeEvent) => {
+    const schemaFromInput = schemas.find((schema) => schema.id === event.target.value)
+    setTargetSchema(schemaFromInput ? schemaFromInput.id : '')
+  }
+
+  const error = MultipleErrorWrapper(`Unable to load schema page`, {
+    isSchemasError,
+  })
+  if (error) return error
+
+  return (
+    <Container>
+      <Stack spacing={2} direction='row' sx={{ width: '100%' }}>
+        <Select
+          aria-label='toggle entry state menu'
+          size='small'
+          value={sourceSchema}
+          onChange={handleSourceSchemaChange}
+          id='source-schema-select'
+        >
+          {schemaOptions}
+        </Select>
+        <Select
+          aria-label='toggle entry state menu'
+          size='small'
+          value={targetSchema}
+          onChange={handleTargetSchemaChange}
+          id='target-schema-select'
+        >
+          {schemaOptions}
+        </Select>
+      </Stack>
+      <SchemaMigrator
+        sourceSchema={schemas.find((schema) => schema.id === sourceSchema)}
+        targetSchema={schemas.find((schema) => schema.id === targetSchema)}
+      ></SchemaMigrator>
+    </Container>
+  )
+}
---
+++ frontend/src/schemas/SchemaMigrator.tsx
@@ -0,0 +1,34 @@
+import { Stack, Typography } from '@mui/material'
+import schema from 'pages/data-card/[dataCardId]/schema'
+import { useEffect, useState } from 'react'
+import JsonSchemaForm from 'src/Form/JsonSchemaForm'
+import { SchemaInterface, SplitSchemaNoRender } from 'types/types'
+import { getStepsFromSchema } from 'utils/formUtils'
+
+interface SchemaMigratorProps {
+  sourceSchema: SchemaInterface | undefined
+  targetSchema: SchemaInterface | undefined
+}
+export default function SchemaMigrator({ sourceSchema, targetSchema }: SchemaMigratorProps) {
+  const [splitSourceSchema, setSplitSourceSchema] = useState<SplitSchemaNoRender>({ reference: '', steps: [] })
+  const [splitTargetSchema, setSplitTargetSchema] = useState<SplitSchemaNoRender>({ reference: '', steps: [] })
+
+  useEffect(() => {
+    if (!sourceSchema || !targetSchema) return
+    const sourceSteps = getStepsFromSchema(sourceSchema, {}, ['properties.contacts'])
+    const targetSteps = getStepsFromSchema(targetSchema, {}, ['properties.contacts'])
+
+    for (const step of sourceSteps) {
+      step.steps = sourceSteps
+    }
+    for (const step of targetSteps) {
+      step.steps = targetSteps
+    }
+
+    setSplitSchema({ reference: schema.id, steps })
+  }, [schema, sourceSchema, targetSchema])
+
+  return (
+    <Stack direction='row'>{sourceSchema ? <JsonSchemaForm /> : <Typography>No valid source schema</Typography>}</Stack>
+  )
+}
