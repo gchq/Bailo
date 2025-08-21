f6073b202e4b0483c85456ef7548aa6e55318eee
JRB66955 <214850388+JRB66955@users.noreply.github.com>
2025-08-21 11:48:11 +0000

    add minimal test for (some) coverage of new code bloc within review.ts

--- backend/test/services/review.spec.ts
+++ backend/test/services/review.spec.ts
@@ -154,6 +154,32 @@ const modelModelMock = vi.hoisted(() => {
   return model
 })
 
+const responseModelMock = vi.hoisted(() => {
+  const obj: any = {}
+
+  obj.aggregate = vi.fn(() => obj)
+  obj.match = vi.fn(() => obj)
+  obj.sort = vi.fn(() => obj)
+  obj.lookup = vi.fn(() => obj)
+  obj.append = vi.fn(() => obj)
+  obj.find = vi.fn(() => obj)
+  obj.findOne = vi.fn(() => obj)
+  obj.findOneAndUpdate = vi.fn(() => obj)
+  obj.findByIdAndUpdate = vi.fn(() => obj)
+  obj.updateOne = vi.fn(() => obj)
+  obj.save = vi.fn(() => obj)
+  obj.limit = vi.fn(() => obj)
+  obj.unwind = vi.fn(() => obj)
+  obj.at = vi.fn(() => obj)
+  obj.map = vi.fn(() => [])
+  obj.filter = vi.fn(() => [])
+
+  const model: any = vi.fn(() => obj)
+  Object.assign(model, obj)
+
+  return model
+})
+
 vi.mock('../../src/models/Model.js', async () => ({
   ...((await vi.importActual('../../src/models/Model.js')) as object),
   default: modelModelMock,
@@ -184,6 +210,11 @@ const arrayUtilMock = vi.hoisted(() => ({
   asyncFilter: vi.fn(),
 }))
 vi.mock('../../src/utils/array.js', async () => arrayUtilMock)
+
+vi.mock('../../src/models/Response.js', async () => ({
+  ...((await vi.importActual('../../src/models/Response.js')) as object),
+  default: responseModelMock,
+}))
 
 const configMock = vi.hoisted(() => ({
   defaultReviewRoles: [
@@ -219,6 +250,15 @@ describe('services > review', () => {
 
   test('findReviews > all reviews for user', async () => {
     await findReviews(user, true, false)
+
+    expect(reviewModelMock.match.mock.calls.at(0)).toMatchSnapshot()
+    expect(reviewModelMock.match.mock.calls.at(1)).toMatchSnapshot()
+  })
+
+  test('findReviews > all open reviews for user', async () => {
+    const mockResponses = [{ _id: 'response' }]
+    responseModelMock.find.mockResolvedValueOnce(mockResponses)
+    await findReviews(user, true, true)
 
     expect(reviewModelMock.match.mock.calls.at(0)).toMatchSnapshot()
     expect(reviewModelMock.match.mock.calls.at(1)).toMatchSnapshot()
