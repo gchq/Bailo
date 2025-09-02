8d19f4d6d6131a6686e62496aa665bac7577a023
ARADDCC002 <89992537+ARADDCC002@users.noreply.github.com>
2025-09-02 13:49:20 +0000

    tidy up to migration selection page

--- frontend/src/schemas/SchemaMigrationSelector.tsx
+++ frontend/src/schemas/SchemaMigrationSelector.tsx
@@ -1,5 +1,5 @@
-import { Forward } from '@mui/icons-material'
-import { Autocomplete, Box, Stack, TextField, Typography } from '@mui/material'
+import { ArrowBack, Forward } from '@mui/icons-material'
+import { Autocomplete, Box, Button, Stack, TextField, Typography } from '@mui/material'
 import { useGetSchemas } from 'actions/schema'
 import { SyntheticEvent, useCallback, useState } from 'react'
 import Loading from 'src/common/Loading'
@@ -10,16 +10,23 @@ import { SchemaInterface } from 'types/types'
 export default function SchemaMigrationSelector() {
   const { schemas, isSchemasLoading, isSchemasError } = useGetSchemas()
 
-  const [beforeSchema, setBeforeSchema] = useState<SchemaInterface | null>()
-  const [afterSchema, setAfterSchema] = useState<SchemaInterface | null>()
+  const [sourceSchema, setSourceSchema] = useState<SchemaInterface | null>()
+  const [targetSchema, setTargetSchema] = useState<SchemaInterface | null>()
+  const [isMigrationPlannerActive, setIsMigrationPlannerActive] = useState(false)
 
-  const handleBeforeSchemaChange = useCallback((_event: SyntheticEvent, newValue: SchemaInterface | null) => {
-    setBeforeSchema(newValue)
+  const handleSourceSchemaChange = useCallback((_event: SyntheticEvent, newValue: SchemaInterface | null) => {
+    setSourceSchema(newValue)
   }, [])
 
-  const handleAfterSchemaChange = useCallback((_event: SyntheticEvent, newValue: SchemaInterface | null) => {
-    setAfterSchema(newValue)
+  const handleTargetSchemaChange = useCallback((_event: SyntheticEvent, newValue: SchemaInterface | null) => {
+    setTargetSchema(newValue)
   }, [])
+
+  const handleReturnToSelection = () => {
+    setIsMigrationPlannerActive(false)
+    setTargetSchema(null)
+    setSourceSchema(null)
+  }
 
   if (isSchemasError) {
     return <MessageAlert message={isSchemasError.info.message} severity='error' />
@@ -31,41 +38,61 @@ export default function SchemaMigrationSelector() {
 
   return (
     <Box sx={{ p: 4 }}>
-      <Stack spacing={4}>
-        <Stack direction='row' spacing={6} justifyContent='center' alignItems='center'>
-          <Autocomplete
-            disablePortal
-            options={schemas}
-            fullWidth
+      {isMigrationPlannerActive ? (
+        <Stack spacing={1}>
+          <Button
             size='small'
-            getOptionDisabled={(option) => option.id === afterSchema?.id}
-            getOptionLabel={(option: SchemaInterface) => option.name}
-            sx={{ width: 300 }}
-            renderInput={(params) => <TextField {...params} label='Source schema ' />}
-            onChange={handleBeforeSchemaChange}
-          />
-          <Forward color='primary' fontSize='large' />
-          <Autocomplete
-            disablePortal
-            options={schemas}
-            fullWidth
-            size='small'
-            getOptionDisabled={(option) => option.id === beforeSchema?.id}
-            getOptionLabel={(option: SchemaInterface) => option.name}
-            sx={{ width: 300 }}
-            renderInput={(params) => <TextField {...params} label='Target schema' />}
-            onChange={handleAfterSchemaChange}
-          />
-        </Stack>
-        {beforeSchema && afterSchema ? (
+            sx={{ width: 'fit-content', pb: 2 }}
+            startIcon={<ArrowBack />}
+            onClick={handleReturnToSelection}
+          >
+            Back to schema selection
+          </Button>
           <SchemaMigrator
-            sourceSchema={schemas.find((schema) => schema.id === beforeSchema.id)}
-            targetSchema={schemas.find((schema) => schema.id === afterSchema.id)}
+            sourceSchema={schemas.find((schema) => sourceSchema && schema.id === sourceSchema.id)}
+            targetSchema={schemas.find((schema) => targetSchema && schema.id === targetSchema.id)}
           ></SchemaMigrator>
-        ) : (
+        </Stack>
+      ) : (
+        <Stack spacing={4}>
+          <Stack direction='row' spacing={6} justifyContent='center' alignItems='center'>
+            <Autocomplete
+              disablePortal
+              options={schemas}
+              fullWidth
+              size='small'
+              getOptionDisabled={(option) => option.id === targetSchema?.id}
+              getOptionLabel={(option: SchemaInterface) => option.name}
+              sx={{ width: 300 }}
+              renderInput={(params) => <TextField {...params} label='Source schema ' />}
+              onChange={handleSourceSchemaChange}
+            />
+            <Forward color='primary' fontSize='large' />
+            <Autocomplete
+              disablePortal
+              options={schemas}
+              fullWidth
+              size='small'
+              getOptionDisabled={(option) => option.id === sourceSchema?.id}
+              getOptionLabel={(option: SchemaInterface) => option.name}
+              sx={{ width: 300 }}
+              renderInput={(params) => <TextField {...params} label='Target schema' />}
+              onChange={handleTargetSchemaChange}
+            />
+          </Stack>
           <Typography sx={{ textAlign: 'center' }}>Please select two schemas to migrate</Typography>
-        )}
-      </Stack>
+          <Box textAlign='center'>
+            <Button
+              variant='contained'
+              sx={{ width: 'max-content' }}
+              onClick={() => setIsMigrationPlannerActive(true)}
+              disabled={!sourceSchema || !targetSchema}
+            >
+              Begin migration
+            </Button>
+          </Box>
+        </Stack>
+      )}
     </Box>
   )
 }
