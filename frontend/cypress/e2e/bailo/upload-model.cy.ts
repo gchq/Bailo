a6930316ef6b7bc735182ffafbef3faf3d918549
ARADDCC002 <89992537+ARADDCC002@users.noreply.github.com>
2025-09-30 06:33:43 +0000

    added a timeout for model upload e2e test

--- frontend/cypress/e2e/bailo/upload-model.cy.ts
+++ frontend/cypress/e2e/bailo/upload-model.cy.ts
@@ -57,7 +57,7 @@ describe('Create new model', () => {
       force: true,
     })
     cy.url().should('not.contain', '/schema')
-    cy.contains('Edit model card')
+    cy.contains('Edit model card', { timeout: 5000 })
   })
 
   it('can edit an existing model', () => {
