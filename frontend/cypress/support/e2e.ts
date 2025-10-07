032e4a4c3278df2888502d442e3989f9b8860608
JRB66955 <214850388+JRB66955@users.noreply.github.com>
2025-10-07 14:27:09 +0100

    Revert "Add initial automated accessibility testing"

--- frontend/cypress/support/e2e.ts
+++ frontend/cypress/support/e2e.ts
@@ -15,8 +15,6 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
-import 'cypress-axe'
-import 'axe-core'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
