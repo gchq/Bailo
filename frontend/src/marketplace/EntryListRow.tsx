61e4a8dea8bf71bd95ac0492e41174fef2ad6c40
IR96334 <175131971+IR96334@users.noreply.github.com>
2025-10-09 11:53:32 +0000

    Fixed overflow

--- frontend/src/marketplace/EntryListRow.tsx
+++ frontend/src/marketplace/EntryListRow.tsx
@@ -70,7 +70,7 @@ export default function EntryListRow({
         <Typography variant='body1' sx={{ whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden' }}>
           {entry.description}
         </Typography>
-        <Stack direction='row' spacing={1} alignItems='center'>
+        <Stack direction='row' spacing={1} alignItems='center' sx={{ flexWrap: 'wrap', rowGap: 1 }}>
           <Stack direction='row' spacing={1}>
             {displayOrganisation && entry.organisation && (
               <ChipSelector
