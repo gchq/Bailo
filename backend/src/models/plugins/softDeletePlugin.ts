bdd542fcf71cb820fadd1aa870a49f4e7ac0d23a
ARADDCC002 <89992537+ARADDCC002@users.noreply.github.com>
2025-11-06 13:12:55 +0000

    Update backend/src/models/plugins/softDeletePlugin.ts
    Co-authored-by: PE39806 <185931318+PE39806@users.noreply.github.com>

--- backend/src/models/plugins/softDeletePlugin.ts
+++ backend/src/models/plugins/softDeletePlugin.ts
@@ -45,7 +45,7 @@ export function softDeletionPlugin(schema: Schema) {
       this.where('deleted').equals(this['_conditions'].deleted)
       next()
     } else {
-      this.or([{ deleted: null }, { deleted: false }])
+      this.or([{ deleted: { $exists: false } }, { deleted: false }])
       next()
     }
   })
