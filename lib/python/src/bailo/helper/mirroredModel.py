d66128719ee71ecbbab6b8797c88e856bc3b1c4a
JR40159 <126243293+JR40159@users.noreply.github.com>
2025-11-07 15:25:15 +0000

    Use source ID when creating a mirrored model

--- lib/python/src/bailo/helper/mirroredModel.py
+++ lib/python/src/bailo/helper/mirroredModel.py
@@ -176,6 +176,7 @@ class MirroredModel(Entry):
                 model_id=model["id"],
                 name=model["name"],
                 description=model["description"],
+                sourceModelId=res_model["settings"]["mirror"]["sourceModelId"],
                 collaborators=model["collaborators"],
                 organisation=model.get("organisation"),
                 state=model.get("state"),
