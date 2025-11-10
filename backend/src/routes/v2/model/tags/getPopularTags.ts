e2367f28fd328612370614732e4a4c27904eb3bb
ARADDCC002 <89992537+ARADDCC002@users.noreply.github.com>
2025-11-10 07:36:30 +0000

    committed missing files

---
+++ backend/src/routes/v2/model/tags/getPopularTags.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from 'express'
+import { z } from 'zod'
+
+import { popularTagsForEntries } from '../../../../services/model.js'
+import { registerPath } from '../../../../services/specification.js'
+import { parse } from '../../../../utils/validate.js'
+
+export const getPopularTagsSchema = z.object({})
+
+registerPath({
+  method: 'get',
+  path: '/api/v2/model',
+  tags: ['tags'],
+  description: 'Get a list of the most commonly used tags',
+  schema: getPopularTagsSchema,
+  responses: {
+    200: {
+      description: 'A list of commonly used tags in string format',
+      content: {
+        'application/json': {
+          schema: z.object({ tags: z.array(z.string()) }),
+        },
+      },
+    },
+  },
+})
+
+interface GetPopularTagsResponse {
+  tags: string[]
+}
+
+export const getPopularTags = [
+  async (req: Request, res: Response<GetPopularTagsResponse>): Promise<void> => {
+    const _ = parse(req, getPopularTagsSchema)
+    const tags = await popularTagsForEntries()
+    res.json({ tags })
+  },
+]
