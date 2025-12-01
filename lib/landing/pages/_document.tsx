9c0980fc440bc14dc39b1ee70944fc3886c21155
PE39806 <185931318+PE39806@users.noreply.github.com>
2025-12-01 13:48:54 +0000

    Fix build issues

--- lib/landing/pages/_document.tsx
+++ lib/landing/pages/_document.tsx
@@ -34,9 +34,9 @@ MyDocument.getInitialProps = async (ctx) => {
   ctx.renderPage = () =>
     originalRenderPage({
       enhanceApp: (App: any) =>
-        function EnhanceApp(props) {
+        (function EnhanceApp(props) {
           return <App emotionCache={cache} {...props} />
-        },
+        }),
     })
 
   const initialProps = await Document.getInitialProps(ctx)
