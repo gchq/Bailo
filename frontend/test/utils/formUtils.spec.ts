c8f8df9bb9b9f1db85d5b2036373f504f8194b4c
ARADDCC002 <89992537+ARADDCC002@users.noreply.github.com>
2026-01-29 16:20:09 +0000

    committed missing test file

---
+++ frontend/test/utils/formUtils.spec.ts
@@ -0,0 +1,20 @@
+import { setFormDataPropertiesToUndefined } from 'utils/formUtils'
+import { describe, expect, it } from 'vitest'
+
+describe('Form utils', () => {
+  it('setFormDataPropertiesToUndefined > successfully sets all properties to be undefined whilst retaining original structure', async () => {
+    const sourceObject = {
+      parent: {
+        array: [{ question1: 'Test 1' }, { question2: 'Test 2' }],
+        question3: 'Test 3',
+      },
+    }
+    const expectedResult = {
+      parent: {
+        array: [{ question1: undefined }, { question2: undefined }],
+        question3: undefined,
+      },
+    }
+    expect(JSON.stringify(setFormDataPropertiesToUndefined(sourceObject))).toBe(JSON.stringify(expectedResult))
+  })
+})
