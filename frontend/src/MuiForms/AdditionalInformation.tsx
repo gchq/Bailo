5458400db6d77943740e816ccf52edc71fc78213
JR40159 <126243293+JR40159@users.noreply.github.com>
2026-04-07 21:53:09 +0000

    Update AdditionalInformation to have expandable descriptions

--- frontend/src/MuiForms/AdditionalInformation.tsx
+++ frontend/src/MuiForms/AdditionalInformation.tsx
@@ -1,7 +1,7 @@
-import { Box, Divider, Stack, Typography } from '@mui/material'
+import { Box, Button, Collapse, Divider, Stack, Typography } from '@mui/material'
 import { SxProps, useTheme } from '@mui/material/styles'
 import { useGetUiConfig } from 'actions/uiConfig'
-import { ReactNode } from 'react'
+import { ReactNode, useEffect, useRef, useState } from 'react'
 import Loading from 'src/common/Loading'
 import MarkdownDisplay from 'src/common/MarkdownDisplay'
 import MessageAlert from 'src/MessageAlert'
@@ -33,6 +33,18 @@ export default function AdditionalInformation({
 }: AdditionalInformationProps) {
   const { uiConfig, isUiConfigLoading, isUiConfigError } = useGetUiConfig()
   const theme = useTheme()
+  const [expanded, setExpanded] = useState(false)
+  const descriptionBoxRef = useRef<HTMLDivElement>(null)
+  const [isOverflowing, setIsOverflowing] = useState(false)
+
+  useEffect(() => {
+    const descriptionBox = descriptionBoxRef.current
+    if (!descriptionBox || !description) {
+      return
+    }
+
+    setIsOverflowing(descriptionBox.scrollHeight > 40)
+  }, [description, editMode])
 
   if (children === undefined || (Array.isArray(children) && children.length === 0)) {
     return <></>
@@ -60,9 +72,24 @@ export default function AdditionalInformation({
           {required && <span style={{ color: theme.palette.error.main }}>{' *'}</span>}
         </Typography>
         {description && editMode && (
-          <Typography variant='caption' color='textSecondary' fontWeight='bold'>
-            {description}
-          </Typography>
+          <Box sx={{ mb: 3 }}>
+            {description && (
+              <Box sx={{ mb: 1 }}>
+                <Collapse in={expanded} collapsedSize={40}>
+                  <Box ref={descriptionBoxRef}>
+                    <Typography variant='caption' color='textSecondary' fontWeight='bold'>
+                      {description}
+                    </Typography>
+                  </Box>
+                </Collapse>
+                {isOverflowing && (
+                  <Button size='small' onClick={() => setExpanded(!expanded)}>
+                    {expanded ? 'Show less' : 'Show more'}
+                  </Button>
+                )}
+              </Box>
+            )}
+          </Box>
         )}
         {children}
       </Stack>
