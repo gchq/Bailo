4ae801777bcc02e21a038a3855c00396ef8a6902
GB907762 <241953074+GB907762@users.noreply.github.com>
2026-04-20 12:00:48 +0000

    Split endpoints

--- backend/src/routes/v2/model/images/getImage.ts
+++ backend/src/routes/v2/model/images/getImage.ts
@@ -8,25 +8,31 @@ import { imageTagWithScanResultsSchema, PathConfig, registerPath } from '../../.
 import { ImageTagResult } from '../../../../types/types.js'
 import { parse } from '../../../../utils/validate.js'
 
-export const getImageSchema = z.object({
-  params: z.object({
-    modelId: z.string({
-      required_error: 'Must specify model id as param',
-    }),
-    name: z.string({
-      required_error: 'Must specify image name as param',
-    }),
-    tag: z.string({
-      required_error: 'Must specify image tag as param',
-    }),
-    digest: z.string().optional(),
+const getImageParams = z.object({
+  modelId: z.string({
+    required_error: 'Must specify model id as param',
+  }),
+  name: z.string({
+    required_error: 'Must specify image name as param',
+  }),
+  tag: z.string({
+    required_error: 'Must specify image tag as param',
   }),
 })
 
-const pathSpec: Omit<PathConfig, 'path' | 'description'> = {
+const getImageByDigestParams = getImageParams.extend({
+  digest: z.string({
+    required_error: 'Must specify image digest as param',
+  }),
+})
+
+export const getImageSchema = z.object({ params: getImageParams })
+
+export const getImageByDigestSchema = z.object({ params: getImageByDigestParams })
+
+const pathSpec: Omit<PathConfig, 'path' | 'description' | 'schema'> = {
   method: 'get',
   tags: ['image'],
-  schema: getImageSchema,
   responses: {
     200: {
       description: 'An image associated with the model.',
@@ -43,16 +49,18 @@ const pathSpec: Omit<PathConfig, 'path' | 'description'> = {
 
 registerPath({
   ...pathSpec,
-  description: 'Get information associated with a specific tagged image and digest for a model.',
-  path: '/api/v2/model/{modelId}/image/{name}/{tag}/{digest}',
-})
-
-registerPath({
-  ...pathSpec,
+  deprecated: true,
+  schema: getImageSchema,
   description: 'Get information associated with a specific tagged image for a model.',
   path: '/api/v2/model/{modelId}/image/{name}/{tag}',
 })
 
+registerPath({
+  ...pathSpec,
+  schema: getImageByDigestSchema,
+  description: 'Get information associated with a specific tagged image and digest for a model.',
+  path: '/api/v3/model/{modelId}/image/{name}/{tag}/{digest}',
+})
 interface GetImagesResponse {
   imageBreakdown: ImageTagResult
 }
@@ -61,8 +69,23 @@ export const getImage = [
   async (req: Request, res: Response<GetImagesResponse>): Promise<void> => {
     req.audit = AuditInfo.ViewModelImage
     const {
-      params: { modelId, name, tag, digest },
+      params: { modelId, name, tag },
     } = parse(req, getImageSchema)
+    const imageBreakdown = await getModelImageWithScanResults(req.user, { repository: modelId, name, tag }, digest)
+    await audit.onViewModelImage(req, modelId, name, tag)
+
+    res.json({
+      imageBreakdown,
+    })
+  },
+]
+
+export const getImageByDigest = [
+  async (req: Request, res: Response<GetImagesResponse>): Promise<void> => {
+    req.audit = AuditInfo.ViewModelImage
+    const {
+      params: { modelId, name, tag, digest },
+    } = parse(req, getImageByDigestSchema)
     const imageBreakdown = await getModelImageWithScanResults(req.user, { repository: modelId, name, tag }, digest)
     await audit.onViewModelImage(req, modelId, name, tag)
 
