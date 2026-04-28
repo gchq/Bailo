0014f22bc781daa06ef6ab79b5b5abb6fbd2478f
JR40159 <126243293+JR40159@users.noreply.github.com>
2026-04-28 09:01:55 +0000

    Refactor migration script

--- backend/src/migrations/032_lowercase_tags.ts
+++ backend/src/migrations/032_lowercase_tags.ts
@@ -2,53 +2,26 @@ import FileModel from '../models/File.js'
 import Model from '../models/Model.js'
 
 export async function up() {
-  await Model.updateMany(
+  const query = { tags: { $exists: true, $type: 'array' }, 'tags.0': { $exists: true } }
+  const lowercaseTagsPipeline = [
     {
-      tags: { $exists: true, $type: 'array' },
-      'tags.0': { $exists: true },
-    },
-    [
-      {
-        $set: {
-          tags: {
-            $setUnion: [
-              {
-                $map: {
-                  input: '$tags',
-                  as: 'tag',
-                  in: { $toLower: '$$tag' },
-                },
+      $set: {
+        tags: {
+          $setUnion: [
+            {
+              $map: {
+                input: '$tags',
+                as: 'tag',
+                in: { $toLower: '$$tag' },
               },
-            ],
-          },
+            },
+          ],
         },
       },
-    ],
-  )
-
-  await FileModel.updateMany(
-    {
-      tags: { $exists: true, $type: 'array' },
-      'tags.0': { $exists: true },
     },
-    [
-      {
-        $set: {
-          tags: {
-            $setUnion: [
-              {
-                $map: {
-                  input: '$tags',
-                  as: 'tag',
-                  in: { $toLower: '$$tag' },
-                },
-              },
-            ],
-          },
-        },
-      },
-    ],
-  )
+  ]
+  await Model.updateMany(query, lowercaseTagsPipeline)
+  await FileModel.updateMany(query, lowercaseTagsPipeline)
 }
 
 export async function down() {}
