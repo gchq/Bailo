c4e154b353c2ad542a8fe79e6262243e09fc1816
PE39806 <185931318+PE39806@users.noreply.github.com>
2026-04-29 14:27:54 +0000

    Add vitest 4.1.x support

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -1,9 +1,40 @@
-import tsconfigPaths from 'vite-tsconfig-paths'
+import babel from '@rolldown/plugin-babel'
 import { defineConfig } from 'vitest/config'
 
+// Required for https://github.com/gchq/Bailo/issues/3511
+// may be removed once sub-dependency oxc supports ecma decorators
+function decoratorPreset(options: Record<string, unknown>) {
+  return {
+    preset: () => ({
+      plugins: [['@babel/plugin-proposal-decorators', options]],
+    }),
+    rolldown: {
+      // Only run Babel on files containing decorators
+      filter: {
+        code: '@',
+      },
+    },
+  }
+}
+
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  resolve: {
+    tsconfigPaths: true,
+  },
+  plugins: [
+    babel({
+      presets: [
+        decoratorPreset({
+          version: '2023-11',
+        }),
+      ],
+    }),
+  ],
   test: {
+    typecheck: {
+      enabled: true,
+      tsconfig: 'backend/test/tsconfig.json',
+    },
     restoreMocks: true,
     setupFiles: [
       './test/testUtils/zod.ts',
