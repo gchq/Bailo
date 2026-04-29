e98da87f43c0f6fedcdfcded184065441cc400ed
PE39806 <185931318+PE39806@users.noreply.github.com>
2026-04-29 15:23:36 +0000

    Possible fix for mock persistence behaviour

--- backend/test/testUtils/clearMocks.ts
+++ backend/test/testUtils/clearMocks.ts
@@ -3,7 +3,6 @@ import { beforeEach, vi } from 'vitest'
 import { resetAuthMock } from '../../src/connectors/authorisation/__mocks__/index.js'
 
 beforeEach(() => {
-  vi.clearAllMocks()
   vi.resetAllMocks()
   resetAuthMock()
 })
