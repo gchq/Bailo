edfd63001c38b9b2b21d2ff933c6f1b9f07c7602
PE39806 <185931318+PE39806@users.noreply.github.com>
2026-05-05 08:29:11 +0000

    Drop jsrsasign package ahead of deprecation

--- backend/src/scripts/generateJWKS.ts
+++ backend/src/scripts/generateJWKS.ts
@@ -1,13 +1,13 @@
+import { createPublicKey } from 'crypto'
 import { readFile, writeFile } from 'fs/promises'
-import r from 'jsrsasign'
 
 import config from '../utils/config.js'
 import { getKid } from '../utils/registryUtils.js'
 
 async function script() {
   const pem = await readFile(config.app.publicKey, { encoding: 'utf-8' })
-  const key = r.KEYUTIL.getKey(pem)
-  const jwk = r.KEYUTIL.getJWKFromKey(key)
+  const key = createPublicKey(pem)
+  const jwk = key.export({ format: 'jwk' })
   jwk.kid = await getKid()
   await writeFile(config.app.jwks, JSON.stringify({ keys: [jwk] }))
 }
