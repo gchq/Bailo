b65868ff7fb47a92f55ecd93ecdc4db9903615a7
JR40159 <126243293+JR40159@users.noreply.github.com>
2026-05-28 13:00:15 +0000

    Deduce role check duplication in the frontend

--- frontend/pages/metrics.tsx
+++ frontend/pages/metrics.tsx
@@ -1,30 +1,9 @@
-import { useContext } from 'react'
-import Forbidden from 'src/common/Forbidden'
 import PageWithTabs from 'src/common/PageWithTabs'
 import Title from 'src/common/Title'
-import CurrentUserContext from 'src/contexts/currentUserContext'
-import Link from 'src/Link'
 import OverviewMetrics from 'src/metrics/OverviewMetrics'
 import PolicyMetrics from 'src/metrics/PolicyMetrics'
-import { Roles } from 'types/types'
 
 export default function Metrics() {
-  const currentUser = useContext(CurrentUserContext)
-
-  if (!currentUser.systemRoles.includes(Roles.Compliance)) {
-    return (
-      <Forbidden
-        errorMessage={
-          <span>
-            If you think this is in error please {<Link href={'/help'}>contact</Link>} the Bailo administrators.
-          </span>
-        }
-        noMargin
-        hideNavButton
-      />
-    )
-  }
-
   const tabs = [
     { title: 'Overview', path: 'overview', view: <OverviewMetrics /> },
     {
--- frontend/pages/schemas/list.tsx
+++ frontend/pages/schemas/list.tsx
@@ -1,40 +1,13 @@
 import { FileUpload } from '@mui/icons-material'
-import { useGetCurrentUser } from 'actions/user'
 import { useRouter } from 'next/router'
 import { useMemo } from 'react'
-import Forbidden from 'src/common/Forbidden'
-import Loading from 'src/common/Loading'
 import PageWithTabs from 'src/common/PageWithTabs'
 import Title from 'src/common/Title'
-import MultipleErrorWrapper from 'src/errors/MultipleErrorWrapper'
 import SchemaCompare from 'src/schemas/SchemaCompare'
 import SchemaMigrationList from 'src/schemas/SchemaMigrationList'
 import SchemaTab from 'src/schemas/SchemaTab'
 
 export default function SchemasPage() {
-  const { currentUser, isCurrentUserLoading, isCurrentUserError } = useGetCurrentUser()
-
-  if (isCurrentUserLoading) {
-    return <Loading />
-  }
-
-  if (!currentUser || !currentUser.isAdmin) {
-    return (
-      <Forbidden
-        errorMessage='If you think this is in error please contact the Bailo administrators.'
-        noMargin
-        hideNavButton
-      />
-    )
-  }
-
-  const error = MultipleErrorWrapper(`Unable to load schema page`, {
-    isCurrentUserError,
-  })
-  if (error) {
-    return error
-  }
-
   return (
     <>
       <Title text='Schemas' />
