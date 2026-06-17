88d39db78d8064c112a061c843a9f5875837223a
PE39806 <185931318+PE39806@users.noreply.github.com>
2026-06-17 09:50:18 +0000

    Fix empty layer bug for container images and add regression test

--- frontend/cypress/e2e/bailo/push-image-registry.cy.ts
+++ frontend/cypress/e2e/bailo/push-image-registry.cy.ts
@@ -3,6 +3,9 @@ let modelUuidForRegistry = ''
 const modelNameForRegistry = 'Test Model'
 let registryUrl = ''
 const testModelImage = 'testmodelimage'
+const testModelImageTag = '1'
+const testModelMultiplatformImageTag = 'multiplatform'
+const platforms = 'linux/amd64,linux/arm64'
 
 describe('Make and approve an access request', () => {
   before(() => {
@@ -40,14 +43,17 @@ describe('Make and approve an access request', () => {
     cy.get('[data-test=secretKeyText]').invoke('text').as('secretKey')
   })
 
-  it('can push and pull to the registry', function () {
+  it('can push a single-platform image to the registry', function () {
+    const registryImage = `${registryUrl}/${modelUuidForRegistry}/${testModelImage}:${testModelImageTag}`
     cy.log('Running all the docker commands to push an image')
     cy.exec(`docker login ${registryUrl} -u ${this.accessKey} -p ${this.secretKey}`, { timeout: 60000 })
     cy.exec(`docker build --tag ${testModelImage} cypress/fixtures/docker-image`, { timeout: 60000 })
-    cy.exec(`docker tag ${testModelImage} ${registryUrl}/${modelUuidForRegistry}/${testModelImage}:1`, {
+    cy.exec(`docker tag ${testModelImage} ${registryImage}`, {
       timeout: 60000,
     })
-    cy.exec(`docker push ${registryUrl}/${modelUuidForRegistry}/${testModelImage}:1`, { timeout: 60000 })
+    cy.exec(`docker push ${registryImage}`, {
+      timeout: 60000,
+    })
   })
 
   it('can select the image when drafting a release', () => {
@@ -58,8 +64,39 @@ describe('Make and approve an access request', () => {
     cy.log('Opening the draft release page to see if we can see our image in the drop down list')
     cy.contains('Draft new release')
     cy.get('[data-test=draftNewReleaseButton').click({ force: true })
-    cy.get('[data-test=imageListAutocomplete').type('1')
-    cy.contains('1')
+    cy.get('[data-test=imageListAutocomplete').type(testModelImageTag)
+    cy.contains(testModelImageTag)
+    cy.contains(`${testModelImage}`)
+  })
+
+  it('can push a multiplatform image to the registry', function () {
+    const registryImage = `${registryUrl}/${modelUuidForRegistry}/${testModelImage}:${testModelMultiplatformImageTag}`
+    cy.log('Running all the docker commands to build and push a multi-platform image')
+    cy.exec(`docker login ${registryUrl} -u ${this.accessKey} -p ${this.secretKey}`, { timeout: 60000 })
+    cy.exec(
+      `docker buildx build --platform ${platforms} --tag ${registryImage} --output=type=registry,registry.insecure=true --push cypress/fixtures/docker-image`,
+      {
+        timeout: 180000,
+      },
+    )
+
+    cy.log('Verifying the pushed image contains both target platforms')
+    cy.exec(`docker buildx imagetools inspect ${registryImage}`, {
+      timeout: 60000,
+    }).then(({ stdout }) => {
+      expect(stdout).to.contain('linux/amd64')
+      expect(stdout).to.contain('linux/arm64')
+    })
+  })
+
+  it('can select the multiplatform image when drafting a release', () => {
+    cy.log('Navigating to the model page and then to the releases tab')
+    cy.visit(`/model/${modelUuidForRegistry}?tab=releases`)
+    cy.log('Opening the draft release page to see if we can see our image in the drop down list')
+    cy.contains('Draft new release')
+    cy.get('[data-test=draftNewReleaseButton').click({ force: true })
+    cy.get('[data-test=imageListAutocomplete').type(testModelMultiplatformImageTag)
+    cy.contains(testModelMultiplatformImageTag)
     cy.contains(`${testModelImage}`)
   })
 })
