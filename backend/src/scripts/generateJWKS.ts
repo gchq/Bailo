78f9874d1238489301853b793bd0968cd943403f
jason-d337 <291811752+jason-d337@users.noreply.github.com>
2026-06-24 11:27:31 +0100

    Address PR comments
    Updated lockfile for lib/landing and reverted change in test

--- backend/src/scripts/generateJWKS.ts
+++ backend/src/scripts/generateJWKS.ts
@@ -7,10 +7,8 @@ import { getKid } from '../utils/registryUtils.js'
 async function script() {
   const pem = await readFile(config.app.publicKey, { encoding: 'utf-8' })
   const key = createPublicKey(pem)
-
   const jwk = key.export({ format: 'jwk' })
   const jwksKey = { ...jwk, kid: await getKid() }
-
   await writeFile(config.app.jwks, JSON.stringify({ keys: [jwksKey] }))
 }
 script()
