d09feb35a8fbe789858f88ec417c260df0cfc0fc
jason-d337 <291811752+jason-d337@users.noreply.github.com>
2026-06-24 11:04:15 +0100

    Resolve JSONWebKey type error
    crypto.d.ts JSONWebKey type is more restrictive:
    
    '[key: string]: unknown;' has been removed. Updated generateJWKS.ts
    to align with this change.

--- backend/src/scripts/generateJWKS.ts
+++ backend/src/scripts/generateJWKS.ts
@@ -7,8 +7,10 @@ import { getKid } from '../utils/registryUtils.js'
 async function script() {
   const pem = await readFile(config.app.publicKey, { encoding: 'utf-8' })
   const key = createPublicKey(pem)
+
   const jwk = key.export({ format: 'jwk' })
-  jwk.kid = await getKid()
-  await writeFile(config.app.jwks, JSON.stringify({ keys: [jwk] }))
+  const jwksKey = { ...jwk, kid: await getKid() }
+
+  await writeFile(config.app.jwks, JSON.stringify({ keys: [jwksKey] }))
 }
 script()
