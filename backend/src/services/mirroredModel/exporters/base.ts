d0c9f74072f6541934b94e14f724f61b871756e3
ARADDCC002 <89992537+ARADDCC002@users.noreply.github.com>
2026-07-01 10:12:43 +0000

    reverted change

--- backend/src/services/mirroredModel/exporters/base.ts
+++ backend/src/services/mirroredModel/exporters/base.ts
@@ -178,7 +178,8 @@ export abstract class BaseExporter {
    * Provides parameters for `initialiseTarGzUpload()`.
    */
   protected abstract getInitialiseTarGzUploadParams():
-    Promise<Parameters<typeof initialiseTarGzUpload>> | Parameters<typeof initialiseTarGzUpload>
+    | Promise<Parameters<typeof initialiseTarGzUpload>>
+    | Parameters<typeof initialiseTarGzUpload>
 
   protected readonly logData: MirrorExportLogData
   protected readonly user: UserInterface
