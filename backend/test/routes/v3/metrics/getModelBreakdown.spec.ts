35e1c0159b970735e3784665bd3be313089906e8
on93754 <244511272+ON93754@users.noreply.github.com>
2026-07-06 09:31:28 +0000

    Fixed route test

--- backend/test/routes/v3/metrics/getModelBreakdown.spec.ts
+++ backend/test/routes/v3/metrics/getModelBreakdown.spec.ts
@@ -1,29 +1,22 @@
 import { describe, expect, test, vi } from 'vitest'
 
-import audit from '../../../../src/connectors/audit/__mocks__/index.js'
 import { testGet } from '../../../testUtils/routes.js'
 
 vi.mock('../../../../src/connectors/audit/index.js')
 
-const mockAuth = vi.hoisted(() => ({
-  authenticationMiddleware: vi.fn(() => [
+const mockMetricsConnector = vi.hoisted(() => ({
+  calculateModelBreakdown: vi.fn(() => [
     {
-      path: '/',
-      middleware: (req: any, _res: any, next: any) => {
-        req.user = { dn: 'test-user' }
-        next()
-      },
+      entryId: 'print-ocr-model-g9yuo2',
+      entryName: 'Print OCR model',
+      collaborators: [
+        {
+          entity: 'user:user',
+          roles: ['owner'],
+        },
+      ],
     },
   ]),
-  hasRole: vi.fn(),
-}))
-
-vi.mock('../../../../src/connectors/authentication/index.js', () => ({
-  default: mockAuth,
-}))
-
-const mockMetricsConnector = vi.hoisted(() => ({
-  getModelBreakdown: vi.fn(),
 }))
 
 vi.mock('../../../../src/connectors/metrics/index.js', () => ({
@@ -31,37 +24,14 @@ vi.mock('../../../../src/connectors/metrics/index.js', () => ({
 }))
 
 describe('routes > metrics > getModelBreakdown', () => {
-  test('200 > returns breakdown when user is Admin', async () => {
-    mockMetricsConnector.getModelBreakdown.mockResolvedValue([
-      {
-        entryId: 'print-ocr-model-g9yuo2',
-        entryName: 'Print OCR model',
-        collaborators: [
-          {
-            entity: 'user:user',
-            roles: ['owner'],
-          },
-        ],
-      },
-    ])
-
-    const res = await testGet('/api/v3/metrics/breakdown')
+  test('200 > ok', async () => {
+    const queryParams = {
+      organisation: 'All',
+      state: 'Development',
+    }
+    const res = await testGet(`/api/v3/metrics/breakdown?${queryParams}`)
 
     expect(res.statusCode).toBe(200)
-    expect(res.body).toEqual([
-      {
-        entryId: 'print-ocr-model-g9yuo2',
-        entryName: 'Print OCR model',
-        collaborators: [
-          {
-            entity: 'user:user',
-            roles: ['owner'],
-          },
-        ],
-      },
-    ])
-
-    expect(mockMetricsConnector.getModelBreakdown).toHaveBeenCalled()
-    expect(audit.onViewMetric).toHaveBeenCalled()
+    expect(res.body).matchSnapshot()
   })
 })
