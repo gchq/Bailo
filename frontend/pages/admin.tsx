8355fa079a4394bd8f70301fd219653b4f235369
ARADDCC002 <89992537+ARADDCC002@users.noreply.github.com>
2026-07-14 15:13:16 +0000

    fixed zoom accessibility on admin panel

--- frontend/pages/admin.tsx
+++ frontend/pages/admin.tsx
@@ -2,7 +2,7 @@ import CompareArrows from '@mui/icons-material/CompareArrows'
 import PublishedWithChanges from '@mui/icons-material/PublishedWithChanges'
 import Schema from '@mui/icons-material/Schema'
 import SupervisorAccount from '@mui/icons-material/SupervisorAccount'
-import { Container, Divider, List, Paper, Stack, Typography } from '@mui/material'
+import { Container, Divider, Grid, List, Paper, Typography } from '@mui/material'
 import { useState } from 'react'
 import SimpleListItemButton from 'src/common/SimpleListItemButton'
 import Title from 'src/common/Title'
@@ -36,53 +36,55 @@ export default function Admin() {
             Admin panel
           </Typography>
           <Divider />
-          <Stack
-            direction={{ xs: 'column', sm: 'row' }}
-            spacing={{ sm: 2 }}
-            divider={<Divider orientation='vertical' flexItem />}
-          >
-            <List sx={{ width: '280px' }}>
-              <Typography variant='caption'>Schemas</Typography>
-              <SimpleListItemButton
-                selected={selectedSection === AdminSection.SCHEMA_LIST}
-                onClick={() => handleListItemClick(AdminSection.SCHEMA_LIST)}
-                icon={<Schema color={selectedSection === AdminSection.SCHEMA_LIST ? 'secondary' : 'inherit'} />}
-              >
-                Schema list
-              </SimpleListItemButton>
-              <SimpleListItemButton
-                selected={selectedSection === AdminSection.COMPARE}
-                onClick={() => handleListItemClick(AdminSection.COMPARE)}
-                icon={<CompareArrows color={selectedSection === AdminSection.COMPARE ? 'secondary' : 'inherit'} />}
-              >
-                Schema compare
-              </SimpleListItemButton>
-              <SimpleListItemButton
-                selected={selectedSection === AdminSection.MIGRATION}
-                onClick={() => handleListItemClick(AdminSection.MIGRATION)}
-                icon={
-                  <PublishedWithChanges color={selectedSection === AdminSection.MIGRATION ? 'secondary' : 'inherit'} />
-                }
-              >
-                Schema migration
-              </SimpleListItemButton>
-              <Divider sx={{ mb: 2, mt: 1 }} />
-              <Typography variant='caption'>Review</Typography>
-              <SimpleListItemButton
-                selected={selectedSection === AdminSection.ROLES}
-                onClick={() => handleListItemClick(AdminSection.ROLES)}
-                icon={<SupervisorAccount color={selectedSection === AdminSection.ROLES ? 'secondary' : 'inherit'} />}
-              >
-                Roles
-              </SimpleListItemButton>
-            </List>
-            <Container sx={{ my: 2 }}>
-              {selectedSection === AdminSection.SCHEMA_LIST && <SchemaTab />}
-              {selectedSection === AdminSection.COMPARE && <SchemaCompare />}
-              {selectedSection === AdminSection.MIGRATION && <SchemaMigrationList />}
-              {selectedSection === AdminSection.ROLES && <ReviewRoleList />}
-            </Container>
-          </Stack>
+          <Grid container spacing={{ sm: 2 }}>
+            <Grid size='auto'>
+              <List sx={{ width: '280px' }}>
+                <Typography variant='caption'>Schemas</Typography>
+                <SimpleListItemButton
+                  selected={selectedSection === AdminSection.SCHEMA_LIST}
+                  onClick={() => handleListItemClick(AdminSection.SCHEMA_LIST)}
+                  icon={<Schema color={selectedSection === AdminSection.SCHEMA_LIST ? 'secondary' : 'inherit'} />}
+                >
+                  Schema list
+                </SimpleListItemButton>
+                <SimpleListItemButton
+                  selected={selectedSection === AdminSection.COMPARE}
+                  onClick={() => handleListItemClick(AdminSection.COMPARE)}
+                  icon={<CompareArrows color={selectedSection === AdminSection.COMPARE ? 'secondary' : 'inherit'} />}
+                >
+                  Schema compare
+                </SimpleListItemButton>
+                <SimpleListItemButton
+                  selected={selectedSection === AdminSection.MIGRATION}
+                  onClick={() => handleListItemClick(AdminSection.MIGRATION)}
+                  icon={
+                    <PublishedWithChanges
+                      color={selectedSection === AdminSection.MIGRATION ? 'secondary' : 'inherit'}
+                    />
+                  }
+                >
+                  Schema migration
+                </SimpleListItemButton>
+                <Divider sx={{ mb: 2, mt: 1 }} />
+                <Typography variant='caption'>Review</Typography>
+                <SimpleListItemButton
+                  selected={selectedSection === AdminSection.ROLES}
+                  onClick={() => handleListItemClick(AdminSection.ROLES)}
+                  icon={<SupervisorAccount color={selectedSection === AdminSection.ROLES ? 'secondary' : 'inherit'} />}
+                >
+                  Roles
+                </SimpleListItemButton>
+              </List>
+            </Grid>
+            <Grid size={{ md: 12, lg: 9 }}>
+              <Container sx={{ my: 2, width: '100%' }}>
+                {selectedSection === AdminSection.SCHEMA_LIST && <SchemaTab />}
+                {selectedSection === AdminSection.COMPARE && <SchemaCompare />}
+                {selectedSection === AdminSection.MIGRATION && <SchemaMigrationList />}
+                {selectedSection === AdminSection.ROLES && <ReviewRoleList />}
+              </Container>
+            </Grid>
+          </Grid>
         </Paper>
       </Container>
     </>
