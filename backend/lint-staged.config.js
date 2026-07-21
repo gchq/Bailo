5b3ea014a6d1a1e56607272db7c9c9dff5a85c92
PE39806 <185931318+PE39806@users.noreply.github.com>
2026-07-21 14:39:32 +0000

    style

--- backend/lint-staged.config.js
+++ backend/lint-staged.config.js
@@ -1,5 +1,8 @@
 const eslintBin = `${process.cwd()}/backend/node_modules/.bin/eslint`
 
 export default {
-  '*.ts': [`${eslintBin} --cache --cache-location backend/node_modules/.cache/eslint/ --max-warnings=0`, 'prettier --check'],
+  '*.ts': [
+    `${eslintBin} --cache --cache-location backend/node_modules/.cache/eslint/ --max-warnings=0`,
+    'prettier --check',
+  ],
 }
