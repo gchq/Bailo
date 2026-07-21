79a91a35cab39d6a6a8ca149b103445770436c94
PE39806 <185931318+PE39806@users.noreply.github.com>
2026-07-21 15:02:08 +0000

    Better fix

--- backend/lint-staged.config.js
+++ backend/lint-staged.config.js
@@ -1,8 +1,3 @@
-const eslintBin = `${process.cwd()}/backend/node_modules/.bin/eslint`
-
 export default {
-  '*.ts': [
-    `${eslintBin} --cache --cache-location backend/node_modules/.cache/eslint/ --max-warnings=0`,
-    'prettier --check',
-  ],
+  '*.ts': ['npx eslint --cache --cache-location node_modules/.cache/eslint/ --max-warnings=0', 'prettier --check'],
 }
--- frontend/lint-staged.config.mjs
+++ frontend/lint-staged.config.mjs
@@ -1,11 +1,4 @@
-const eslintBin = `${process.cwd()}/frontend/node_modules/.bin/eslint`
-
-const buildEslintCommand = (filenames) =>
-  `${eslintBin} --cache --cache-location frontend/node_modules/.cache/eslint/ --max-warnings 0 ${filenames.join(' ')}`
-
-const config = {
-  '**/*.{ts,tsx,md}': [buildEslintCommand],
+export default {
+  '**/*.{ts,tsx,md}': ['npx eslint --cache --cache-location node_modules/.cache/eslint/ --max-warnings 0'],
   '**/*.{ts,tsx,md,mdx}': ['prettier --check'],
 }
-
-export default config
