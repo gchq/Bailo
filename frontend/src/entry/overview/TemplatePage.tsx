988d85a20c352a4d2ba1794951407396d3349a8c
ARADDCC002 <89992537+ARADDCC002@users.noreply.github.com>
2026-07-28 14:07:06 +0000

    Hidden the template page from non model owners

--- frontend/src/entry/overview/TemplatePage.tsx
+++ frontend/src/entry/overview/TemplatePage.tsx
@@ -2,6 +2,8 @@ import Add from '@mui/icons-material/Add'
 import PostAdd from '@mui/icons-material/PostAdd'
 import { Box, Button, Card, Divider, Stack, Typography } from '@mui/material'
 import { useMemo } from 'react'
+import EmptyBlob from 'src/common/EmptyBlob'
+import Restricted from 'src/common/Restricted'
 import Link from 'src/Link'
 import { EntryCardKindLabel, EntryInterface, EntryKind } from 'types/types'
 import { entryKindForRedirect } from 'utils/routerUtils'
@@ -25,55 +27,36 @@ export default function TemplatePage({ entry }: TemplatePageProps) {
   }, [entry.kind])
 
   return (
-    <Box sx={{ maxWidth: 'md', mx: 'auto', my: 4 }}>
-      <Stack
-        spacing={4}
-        sx={{
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Typography component='h2' variant='h6' color='primary' data-test='createEntryCardOverview'>
-          {`Create ${toTitleCase(EntryCardKindLabel[entry.kind])}`}
-        </Typography>
-        <PostAdd fontSize='large' color='primary' />
-        <Typography variant='body1'>{entryCardDescription}</Typography>
+    <Restricted
+      action='editEntry'
+      fallback={
+        <Box sx={{ mt: 4 }}>
+          <EmptyBlob text={`No schema has been set for this ${toTitleCase(EntryCardKindLabel[entry.kind])}`} />
+        </Box>
+      }
+    >
+      <Box sx={{ maxWidth: 'md', mx: 'auto', my: 4 }}>
         <Stack
-          direction={{ sm: 'column', md: 'row' }}
           spacing={4}
-          divider={<Divider orientation='vertical' flexItem />}
           sx={{
             justifyContent: 'center',
             alignItems: 'center',
           }}
         >
-          <Card
+          <Typography component='h2' variant='h6' color='primary' data-test='createEntryCardOverview'>
+            {`Create ${toTitleCase(EntryCardKindLabel[entry.kind])}`}
+          </Typography>
+          <PostAdd fontSize='large' color='primary' />
+          <Typography variant='body1'>{entryCardDescription}</Typography>
+          <Stack
+            direction={{ sm: 'column', md: 'row' }}
+            spacing={4}
+            divider={<Divider orientation='vertical' flexItem />}
             sx={{
-              width: '300px',
-              p: 2,
+              justifyContent: 'center',
+              alignItems: 'center',
             }}
           >
-            <Stack spacing={2}>
-              <Typography component='h3' variant='h6' color='primary'>
-                Create from schema
-              </Typography>
-              <Typography>
-                {`Create ${EntryCardKindLabel[entry.kind]} from scratch using a predefined schema.`}
-              </Typography>
-              <Button
-                href={`/${entryKindForRedirect(entry.kind)}/${entry.id}/schema`}
-                LinkComponent={Link}
-                variant='contained'
-                sx={{ width: '100%' }}
-                data-test='createSchemaFromScratchButton'
-                disabled={!!entry.settings.mirror?.sourceModelId}
-                startIcon={<Add />}
-              >
-                Create
-              </Button>
-            </Stack>
-          </Card>
-          {entry.kind === EntryKind.MODEL && (
             <Card
               sx={{
                 width: '300px',
@@ -82,14 +65,17 @@ export default function TemplatePage({ entry }: TemplatePageProps) {
             >
               <Stack spacing={2}>
                 <Typography component='h3' variant='h6' color='primary'>
-                  Create from a template
+                  Create from schema
                 </Typography>
-                <Typography>Create a model card using an existing model as a template.</Typography>
+                <Typography>
+                  {`Create ${EntryCardKindLabel[entry.kind]} from scratch using a predefined schema.`}
+                </Typography>
                 <Button
-                  sx={{ width: '100%' }}
-                  variant='contained'
-                  href={`/${entryKindForRedirect(entry.kind)}/${entry.id}/template`}
+                  href={`/${entryKindForRedirect(entry.kind)}/${entry.id}/schema`}
                   LinkComponent={Link}
+                  variant='contained'
+                  sx={{ width: '100%' }}
+                  data-test='createSchemaFromScratchButton'
                   disabled={!!entry.settings.mirror?.sourceModelId}
                   startIcon={<Add />}
                 >
@@ -97,9 +83,34 @@ export default function TemplatePage({ entry }: TemplatePageProps) {
                 </Button>
               </Stack>
             </Card>
-          )}
+            {entry.kind === EntryKind.MODEL && (
+              <Card
+                sx={{
+                  width: '300px',
+                  p: 2,
+                }}
+              >
+                <Stack spacing={2}>
+                  <Typography component='h3' variant='h6' color='primary'>
+                    Create from a template
+                  </Typography>
+                  <Typography>Create a model card using an existing model as a template.</Typography>
+                  <Button
+                    sx={{ width: '100%' }}
+                    variant='contained'
+                    href={`/${entryKindForRedirect(entry.kind)}/${entry.id}/template`}
+                    LinkComponent={Link}
+                    disabled={!!entry.settings.mirror?.sourceModelId}
+                    startIcon={<Add />}
+                  >
+                    Create
+                  </Button>
+                </Stack>
+              </Card>
+            )}
+          </Stack>
         </Stack>
-      </Stack>
-    </Box>
+      </Box>
+    </Restricted>
   )
 }
