a2da673fd9e0e7f7c7137e468578459d50b72aab
ARADDCC002 <89992537+ARADDCC002@users.noreply.github.com>
2026-07-28 10:39:23 +0000

    removed dark empty blob in favour of media query styles

--- frontend/src/common/EmptyBlob.tsx
+++ frontend/src/common/EmptyBlob.tsx
@@ -1,5 +1,4 @@
 import { Stack } from '@mui/material'
-import { useTheme } from '@mui/material/styles'
 import Typography from '@mui/material/Typography'
 import Image from 'next/image'
 import { CSSProperties } from 'react'
@@ -10,15 +9,6 @@ type EmptyBlobProps = {
 }
 
 export default function EmptyBlob({ text, style }: EmptyBlobProps) {
-  const theme = useTheme()
-
-  const blobToDisplay = () => {
-    if (theme.palette.mode === 'light') {
-      return <Image src='/emptyBlob.svg' alt='Empty blob' width={120} height={120} data-test='emptyBlobImage' />
-    } else {
-      return <Image src='/emptyBlobDark.svg' alt='Empty blob' width={120} height={120} data-test='emptyBlobImage' />
-    }
-  }
   return (
     <Stack
       spacing={1}
@@ -27,7 +17,7 @@ export default function EmptyBlob({ text, style }: EmptyBlobProps) {
         alignItems: 'center',
       }}
     >
-      {blobToDisplay()}
+      <Image src='/emptyBlob.svg' alt='Empty blob' width={120} height={120} data-test='emptyBlobImage' />
       <Typography
         sx={{
           color: 'text.secondary',
