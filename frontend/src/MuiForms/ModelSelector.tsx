253a5c20927f73c32c8ef95f1fd3711aee295334
ARADDCC002 <89992537+ARADDCC002@users.noreply.github.com>
2026-08-12 15:19:19 +0000

    test

--- frontend/src/MuiForms/ModelSelector.tsx
+++ frontend/src/MuiForms/ModelSelector.tsx
@@ -164,7 +164,7 @@ export default function ModelSelector({
         </Box>
       ) : (
         <Typography component='span' sx={{ fontStyle: 'italic', color: theme.palette.customTextInput.main }}>
-          Unansweredtest
+          Unanswered
         </Typography>
       )}
     </CompareField>
