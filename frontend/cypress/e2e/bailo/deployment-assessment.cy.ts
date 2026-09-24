5051ea7c6ef3a550ae13b55bbbba7b1d5ef80608
IR96334 <175131971+IR96334@users.noreply.github.com>
2026-09-24 08:42:15 +0000

    try within

--- frontend/cypress/e2e/bailo/deployment-assessment.cy.ts
+++ frontend/cypress/e2e/bailo/deployment-assessment.cy.ts
@@ -93,7 +93,9 @@ describe('Deployment Assessment Suite', () => {
     cy.get('[role=presentation] ul li:first').click()
 
     cy.get('[data-test=signOffButton]').click()
-    cy.get('[data-test=entitySelector] input', { timeout: 20000 }).type('user').wait(500)
+    cy.get('[data-test=entitySelector]', { timeout: 20000 }).within(() => {
+      cy.get('input').type('user').wait(500)
+    })
     cy.get('[role=presentation] ul li:first').click().wait(100)
 
     cy.get('[data-test=submitDeploymentAssessmentButton]').click()
